9970fe926f8b0d51a97240a7f00c2d66a19d8401
Bill DeRusha <bill@edx.org>
2016-03-29 11:49:29 -0400

    Add Currency to core app

--- course_discovery/apps/core/tests/test_models.py
+++ course_discovery/apps/core/tests/test_models.py
@@ -4,7 +4,7 @@ from django.test import TestCase
 from django_dynamic_fixture import G
 from social.apps.django_app.default.models import UserSocialAuth
 
-from course_discovery.apps.core.models import User
+from course_discovery.apps.core.models import User, Currency
 
 
 # pylint: disable=no-member
@@ -38,3 +38,15 @@ class UserTests(TestCase):
 
         user = G(User, full_name=full_name, first_name=first_name, last_name=last_name)
         self.assertEqual(user.get_full_name(), full_name)
+
+
+class CurrencyTests(TestCase):
+    """ Tests for the Currency class. """
+
+    def test_str(self):
+        """ Verify casting an instance to a string returns a string containing the ID and name of the currency. """
+
+        code = 'USD',
+        name = 'U.S. Dollar'
+        instance = Currency(code=code, name=name)
+        self.assertEqual(str(instance), '{code} - {name}'.format(code=code, name=name))
