a27d10432ceda22d01516a023a5b774509696565
Bill DeRusha <bill@edx.org>
2016-04-19 16:49:31 -0400

    Use edx-drf-extentsions 0.5.0 for JWT authentication support

--- course_discovery/settings/base.py
+++ course_discovery/settings/base.py
@@ -261,7 +261,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.SessionAuthentication',
         'edx_rest_framework_extensions.authentication.BearerAuthentication',
-        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'edx_rest_framework_extensions.authentication.JwtAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'DEFAULT_PERMISSION_CLASSES': (
