747c28a035c1c24a0f4c7007731cc33d637633b9
Peter Fogg <pfogg@edx.org>
2016-04-26 09:03:57 -0400

    Include courses with no end date in active course runs.

--- course_discovery/apps/course_metadata/query.py
+++ course_discovery/apps/course_metadata/query.py
@@ -2,6 +2,7 @@ import datetime
 
 import pytz
 from django.db import models
+from django.db.models.query_utils import Q
 
 
 class CourseQuerySet(models.QuerySet):
@@ -9,4 +10,7 @@ class CourseQuerySet(models.QuerySet):
         """ Filters Courses to those with CourseRuns that are either currently open for enrollment,
         or will be open for enrollment in the future. """
 
-        return self.filter(course_runs__enrollment_end__gt=datetime.datetime.now(pytz.UTC))
+        return self.filter(
+            Q(course_runs__enrollment_end__gt=datetime.datetime.now(pytz.UTC)) |
+            Q(course_runs__enrollment_end__isnull=True)
+        )
