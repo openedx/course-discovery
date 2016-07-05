e540ca8fba1973e400fd76afe0d48bc7ebb7e2cc
Awais <awais.qureshi@arbisoft.com>
2016-07-06 01:31:38 +0500

    ECOM-4892 Basic infrastructure for course workflow.

--- course_discovery/apps/course_builder/tests/test_views.py
+++ course_discovery/apps/course_builder/tests/test_views.py
@@ -4,6 +4,8 @@ Tests for course builder views.
 from django.core.urlresolvers import reverse
 from django.test import TestCase
 
+from course_discovery.apps.course_builder.tests import toggle_switch
+
 
 class CourseListing(TestCase):
     """ Course builder list view tests. """
@@ -18,5 +20,12 @@ class CourseListing(TestCase):
 
     def test_courses_list_page(self):
         """ Verify that the course listing view renders a page. """
+
+        # Method should return a 404 if the switch is inactive
+        toggle_switch('enable_course_builder', False)
+        response = self.client.get(self.listing_path)
+        self.assertEqual(response.status_code, 404)
+
+        toggle_switch('enable_course_builder', True)
         response = self.client.get(self.listing_path)
         self.assertEqual(response.status_code, 200)
--- course_discovery/apps/course_builder/views.py
+++ course_discovery/apps/course_builder/views.py
@@ -1,7 +1,11 @@
 """
 Course Builder views.
 """
+from django.http import Http404
 from django.views.generic import TemplateView
+import waffle
+
+from course_discovery.apps.course_metadata.models import Course
 
 
 class CourseListing(TemplateView):
@@ -11,4 +15,18 @@ class CourseListing(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(CourseListing, self).get_context_data(**kwargs)
 
+        context.update({
+            'courses': self._get_courses(),
+        })
+
         return context
+
+    def get(self, request, *args, **kwargs):
+        """ Get method for list page."""
+        if not waffle.switch_is_active('enable_course_builder'):
+            raise Http404
+        return super(CourseListing, self).get(request, args, **kwargs)
+
+    def _get_courses(self):
+        """ Helper method to retrieve all courses."""
+        return Course.objects.all()
