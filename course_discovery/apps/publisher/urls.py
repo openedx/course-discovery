39c04dec02a4ea06229cfc768962ef51d7af58b7
Bill DeRusha <bill@edx.org>
2016-07-15 17:46:21 -0400

    Add prepublish page

--- course_discovery/apps/publisher/urls.py
+++ course_discovery/apps/publisher/urls.py
@@ -2,8 +2,14 @@
 URLs for the course publisher views.
 """
 from django.conf.urls import url
-from course_discovery.apps.publisher.views import UnpublishedCourseListing
+from course_discovery.apps.course_metadata.constants import COURSE_RUN_ID_REGEX
+from course_discovery.apps.publisher.views import UnpublishedCourseListing, CourseDetailPrepublish
 
 urlpatterns = [
     url(r'^courses/$', UnpublishedCourseListing.as_view(), name='unpublished_courses'),
+    url(
+        r'^course_runs/{course_run_key}/prepublish$'.format(course_run_key=r'(.*?)'),
+        CourseDetailPrepublish.as_view(),
+        name='course_run_prepublish'
+    ),
 ]
