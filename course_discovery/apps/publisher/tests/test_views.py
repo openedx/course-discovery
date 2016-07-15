5fa013e2a92a98ae917f76f81942397dc4321d15
Awais <awais.qureshi@arbisoft.com>
2016-07-16 02:37:36 +0500

    ECOM-4892 Adding tests for the list page.

--- course_discovery/apps/publisher/tests/test_views.py
+++ course_discovery/apps/publisher/tests/test_views.py
@@ -1,31 +1,65 @@
 """
 Tests for publisher views.
 """
+import ddt
+
 from django.core.urlresolvers import reverse
 from django.test import TestCase
 
+from course_discovery.apps.publisher.models import Status
 from course_discovery.apps.publisher.tests import toggle_switch
+from course_discovery.apps.publisher.tests import factories
 
 
+@ddt.ddt
 class UnpublishedCourseRunListing(TestCase):
     """ Tests cases for non-published course run lists view. """
 
     def setUp(self):
         super(UnpublishedCourseRunListing, self).setUp()
+        toggle_switch('enable_publisher', True)
+        self.course_run = factories.CourseRunFactory()
+        self.course_run_status = factories.StatusFactory(
+            course_run=self.course_run, name=Status.PUBLISHED
+        )
+        self.course_detail = factories.CourseRunDetailFactory(
+            course_run=self.course_run, target_content=True, priority=False
+        )
         self.listing_path = self._course_listing_url()
 
     def _course_listing_url(self):
         """ Helper method to generate the url for a course listing page."""
         return reverse('publisher:unpublished_courses')
 
-    def test_courses_list_page(self):
-        """ Verify that the course listing view renders a page. """
-
-        # Method should return a 404 if the switch is inactive
+    def test_page_without_enabling_switch(self):
+        """ Verify that the course listing view returns 404 if switch
+        is not enable. """
         toggle_switch('enable_publisher', False)
         response = self.client.get(self.listing_path)
         self.assertEqual(response.status_code, 404)
 
-        toggle_switch('enable_publisher', True)
+    def test_courses_list_with_published_status(self):
+        """ Verify that no data will appear on page is status is published. """
         response = self.client.get(self.listing_path)
+        self.assertContains(response, 'No Data available')
+
+    @ddt.data(Status.REVIEW, Status.DRAFT)
+    def test_courses_list_page(self, status_name):
+        """ Verify that the course listing page shows unpublished data. """
+        self.course_run_status.name = status_name
+        self.course_run_status.save()
+        response = self.client.get(self.listing_path)
+        self._assert_un_published_data(response)
+
+    def _assert_un_published_data(self, response):
         self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.course_run.course.key)
+
+        self.assertContains(response, self.course_run.status.name)
+        # TODO need to fix the date format in templates
+        # self.assertContains(response, self.course_run.status.created)
+        # self.assertContains(response, self.course_run.status.modified)
+
+        self.assertContains(response, self.course_run.detail.priority)
+        self.assertContains(response, self.course_run.detail.target_content)
+        self.assertContains(response, self.course_run.detail.priority)
