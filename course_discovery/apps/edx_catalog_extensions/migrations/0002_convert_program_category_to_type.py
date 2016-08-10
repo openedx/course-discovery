e20b7a9c9804db1faddf207b242643fb63e65791
Clinton Blackburn <cblackburn@edx.org>
2016-08-10 15:54:36 -0400

    Added migration to convert program categories for XSeries
    ECOM-5173

---
+++ course_discovery/apps/edx_catalog_extensions/migrations/0002_convert_program_category_to_type.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations
+
+
+def set_program_types(apps, schema_editor):
+    Program = apps.get_model('course_metadata', 'Program')
+    ProgramType = apps.get_model('course_metadata', 'ProgramType')
+    xseries_type = ProgramType.objects.get(name='XSeries')
+
+    Program.objects.filter(category__iexact='xseries').update(type=xseries_type)
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('edx_catalog_extensions', '0001_create_program_types'),
+    ]
+
+    operations = [
+        migrations.RunPython(set_program_types, lambda *args: None)
+    ]
