78cf3ae75af5a39e471387fecbb721df9b5fecb9
Clinton Blackburn <cblackburn@edx.org>
2016-08-13 17:43:06 -0400

    Set Program.excluded_course_runs to be an optional field
    ECOM-5097

---
+++ course_discovery/apps/course_metadata/migrations/0015_auto_20160813_2142.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('course_metadata', '0014_auto_20160811_0436'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='program',
+            name='excluded_course_runs',
+            field=models.ManyToManyField(blank=True, null=True, to='course_metadata.CourseRun'),
+        ),
+    ]
