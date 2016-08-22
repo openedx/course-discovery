809973ac11fe6b450aaa6f079870ac0e6775d412
Clinton Blackburn <cblackburn@edx.org>
2016-08-22 14:21:33 -0400

    Post-merge fixes for Course model and data loader
    - Corrected data loader URL
    - Added migration to remove duplicated courses
    
    ECOM-5195

---
+++ course_discovery/apps/course_metadata/migrations/0022_remove_duplicate_courses.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations
+
+
+def remove_duplicate_courses(apps, schema_editor):
+    Course = apps.get_model('course_metadata', 'Course')
+    duplicates = Course.objects.raw('SELECT'
+                                    '   c.* '
+                                    'FROM '
+                                    '  course_metadata_course AS c'
+                                    '  JOIN ('
+                                    '    SELECT'
+                                    '      LOWER(key) AS key,'
+                                    '      COUNT(1)'
+                                    '    FROM'
+                                    '      course_metadata_course'
+                                    '    GROUP BY'
+                                    '      LOWER(key)'
+                                    '    HAVING'
+                                    '      COUNT(1) > 1'
+                                    '  ) AS dupes ON dupes.key = LOWER(c.key)')
+
+    for course in duplicates:
+        course.delete()
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('course_metadata', '0021_auto_20160819_2005'),
+    ]
+
+    operations = [
+        migrations.RunPython(remove_duplicate_courses, reverse_code=migrations.RunPython.noop),
+    ]
