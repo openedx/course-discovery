b4b76423feadf085822c20d719a7d15aa5de5784
Renzo Lucioni <renzo@renzolucioni.com>
2016-09-19 11:27:22 -0400

    Add hidden field to CourseRun model and index
    Also add a filter to the list view of CourseRun admin so that users can determine which course runs are hidden. ECOM-5687.

---
+++ course_discovery/apps/course_metadata/migrations/0028_courserun_hidden.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('course_metadata', '0027_auto_20160915_2038'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='courserun',
+            name='hidden',
+            field=models.BooleanField(default=False),
+        ),
+    ]
