a43fe29d9990e9707bd159c1b39aa193cdea0d85
Awais <awais.qureshi@arbisoft.com>
2016-09-28 13:00:19 +0500

    Integrating django-ses for sending emails
    Installing the django-ses from base.py.
    Updating the required authentication variable for sending emails ( using discovery configuration ).
    Add an example of required credentials in private.py.example.
    
    Testing email function

--- course_discovery/apps/publisher_comments/models.py
+++ course_discovery/apps/publisher_comments/models.py
@@ -1,8 +1,25 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.utils.translation import ugettext_lazy as _
-
 from django_comments.models import CommentAbstractModel
 from django_extensions.db.fields import ModificationDateTimeField
+
+import waffle
 
 
 class Comments(CommentAbstractModel):
     modified = ModificationDateTimeField(_('modified'))
+
+
+@receiver(post_save, sender=Comments)
+def send_email(sender, instance, **kwargs):    # pylint: disable=unused-argument
+    """ Send email on new comment. """
+    if waffle.switch_is_active('enable_emails'):
+        from django.core.mail import send_mail
+        send_mail(
+            'Subject here',
+            'Here is the message.',
+            'awais@edx.org',
+            ['awais@edx.org'],
+            fail_silently=False,
+        )
