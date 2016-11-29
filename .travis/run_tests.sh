db55469236f7391b1981651c035bad6984925e7d
Feanil Patel <feanil@edx.org>
2016-11-29 13:47:40 -0500

    Get tests working. This still needs to be re-worked.

--- .travis/run_tests.sh
+++ .travis/run_tests.sh
@@ -3,15 +3,22 @@
 . /edx/app/discovery/nodeenvs/discovery/bin/activate
 
 apt update
-apt install -y xvfb firefox gettext
+apt install -y xvfb firefox gettext wget
+
+cd
+wget https://github.com/mozilla/geckodriver/releases/download/v0.11.1/geckodriver-v0.11.1-linux64.tar.gz
+tar xvzf geckodriver-v0.11.1-linux64.tar.gz
+mv geckodriver /usr/bin/
+
+pip install --upgrade selenium
 
 cd /edx/app/discovery/discovery
-pip install -U pip wheel
 # Make it so bower can run without sudo.
 # https://github.com/GeoNode/geonode/pull/1070
 echo '{ "allow_root": true }' > /root/.bowerrc
 
 make requirements
+make requirements.js
 # Ensure documentation can be compiled
 cd docs && make html
 cd ..
@@ -22,7 +29,6 @@ export DJANGO_SETTINGS_MODULE=course_discovery.settings.test
 make validate_translations
 
 # Compile assets and run validation
-xvfb-run make clean_static
-xvfb-run make static
-xvfb-run make validate_python
-xvfb-run make validate_js
+make clean_static
+make static
+xvfb-run make validate
