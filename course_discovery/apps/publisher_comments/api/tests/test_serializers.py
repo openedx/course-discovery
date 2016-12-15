9d00857445c9e4c7d15b0a82048c2e17f2ef145e
tasawernawaz <tasawer.nawaz@arbisoft.com>
2016-12-15 10:12:52 +0500

    Front end  & JS for comments edit ECOM-6525

--- course_discovery/apps/publisher_comments/api/tests/test_serializers.py
+++ course_discovery/apps/publisher_comments/api/tests/test_serializers.py
@@ -5,10 +5,12 @@ from course_discovery.apps.publisher_comments.tests.factories import CommentFact
 
 
 class CommentSerializerTests(TestCase):
-
     def test_data(self):
         """ Verify that CommentsSerializer serialize the comment object. """
+
         comment = CommentFactory.create(comment='test comment')
         serializer = CommentSerializer(comment)
-        expected = {'comment': 'test comment'}
+        expected = {'comment': 'test comment',
+                    'modified': comment.modified.strftime("%Y-%m-%dT%H:%M:%S.%fZ")}
+
         self.assertDictEqual(serializer.data, expected)
