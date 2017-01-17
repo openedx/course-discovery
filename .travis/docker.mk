3ba1a0ee0a069ca9ed39abf47e3c659b0efc208d
Renzo Lucioni <renzo@lucioni.xyz>
2017-01-17 14:36:10 -0500

    Clean up Makefile
    Move target descriptions inline and add targets for running Travis builds to a separate, included Makefile.

---
+++ .travis/docker.mk
@@ -0,0 +1,16 @@
+.PHONY: travis_down travis_start travis_stop travis_test travis_up
+
+travis_up: ## Create containers used to run tests on Travis CI
+	docker-compose -f .travis/docker-compose-travis.yml up -d
+
+travis_start: ## Start containers stopped by `travis_stop`
+	docker-compose -f .travis/docker-compose-travis.yml start
+
+travis_test: ## Run tests on Docker containers, as on Travis CI
+	docker exec -it course-discovery env TERM=$(TERM) /edx/app/discovery/discovery/.travis/run_tests.sh
+
+travis_stop: ## Stop running containers created by `travis_up` without removing them
+	docker-compose -f .travis/docker-compose-travis.yml stop
+
+travis_down: ## Stop and remove containers and other resources created by `travis_up`
+	docker-compose -f .travis/docker-compose-travis.yml down
