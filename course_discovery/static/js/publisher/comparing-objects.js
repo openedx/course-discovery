4a92d72771ea2ce8f0275a979a7eda47f27e3a53
Awais <awais.qureshi@arbisoft.com>
2017-02-01 16:39:43 +0500

    Adding google-diff in bower.json.
    ECOM-6990

--- course_discovery/static/js/publisher/comparing-objects.js
+++ course_discovery/static/js/publisher/comparing-objects.js
@@ -1,8 +1,13 @@
-    var dmp =new diff_match_patch();
-    function launch() {
-      var text1 = $("#title").text();
-      var text2 = $("#history-title").text();
-      var d = dmp.diff_main(text1, text2);
-      $("#difference-title").html(dmp.diff_prettyHtml(d));
+$(document).ready(function() {
+
+    var dmp = new diff_match_patch();
+
+    function compare() {
+        var current = $("#current-title").text();
+        var historical = $("#history-title").text();
+        var d = dmp.diff_main(current, historical);
+        $("#difference-title").html(dmp.diff_prettyHtml(d));
     }
-launch();
+
+    compare();
+});
