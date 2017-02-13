0ab5d4209f85e793b8bb23ed3ede22954d4088bd
Waheed Ahmed <waheed.ahmed@arbisoft.com>
2017-02-13 17:44:19 +0500

    Register CourseState and CourseRunState in django admin.

--- course_discovery/apps/publisher/admin.py
+++ course_discovery/apps/publisher/admin.py
@@ -2,14 +2,16 @@ from django.contrib import admin
 from guardian.admin import GuardedModelAdmin
 
 from course_discovery.apps.publisher.forms import CourseUserRoleForm, OrganizationUserRoleForm, UserAttributesAdminForm
-from course_discovery.apps.publisher.models import (
-    Course, CourseRun, CourseUserRole, OrganizationExtension, OrganizationUserRole, Seat, State, UserAttributes
-)
+from course_discovery.apps.publisher.models import (Course, CourseRun, CourseRunState, CourseState, CourseUserRole,
+                                                    OrganizationExtension, OrganizationUserRole, Seat, State,
+                                                    UserAttributes)
 
 admin.site.register(Course)
 admin.site.register(CourseRun)
 admin.site.register(Seat)
 admin.site.register(State)
+admin.site.register(CourseState)
+admin.site.register(CourseRunState)
 
 
 @admin.register(CourseUserRole)
