410c54d20e6e73f664656c1db5e2790ae5884266
Awais <awais.qureshi@arbisoft.com>
2017-02-14 20:35:05 +0500

    Fixing admin issue with large data in changed-by id.
    ECOM-7222

--- course_discovery/apps/publisher/admin.py
+++ course_discovery/apps/publisher/admin.py
@@ -2,21 +2,17 @@ from django.contrib import admin
 from guardian.admin import GuardedModelAdmin
 
 from course_discovery.apps.publisher.forms import CourseUserRoleForm, OrganizationUserRoleForm, UserAttributesAdminForm
-from course_discovery.apps.publisher.models import (Course, CourseRun, CourseRunState, CourseState, CourseUserRole,
-                                                    OrganizationExtension, OrganizationUserRole, Seat, State,
-                                                    UserAttributes)
-
-admin.site.register(Course)
-admin.site.register(CourseRun)
-admin.site.register(Seat)
-admin.site.register(State)
-admin.site.register(CourseState)
-admin.site.register(CourseRunState)
+from course_discovery.apps.publisher.models import (
+    Course, CourseRun, CourseRunState, CourseState, CourseUserRole,
+    OrganizationExtension, OrganizationUserRole, Seat, State,
+    UserAttributes
+)
 
 
 @admin.register(CourseUserRole)
 class CourseUserRoleAdmin(admin.ModelAdmin):
     form = CourseUserRoleForm
+    raw_id_fields = ('changed_by',)
 
 
 @admin.register(OrganizationExtension)
@@ -32,3 +28,33 @@ class UserAttributesAdmin(admin.ModelAdmin):
 @admin.register(OrganizationUserRole)
 class OrganizationUserRoleAdmin(admin.ModelAdmin):
     form = OrganizationUserRoleForm
+
+
+@admin.register(CourseState)
+class CourseStateAdmin(admin.ModelAdmin):
+    raw_id_fields = ('changed_by',)
+
+
+@admin.register(CourseRunState)
+class CourseRunStateAdmin(admin.ModelAdmin):
+    raw_id_fields = ('changed_by',)
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    raw_id_fields = ('changed_by',)
+
+
+@admin.register(CourseRun)
+class CourseRunAdmin(admin.ModelAdmin):
+    raw_id_fields = ('changed_by',)
+
+
+@admin.register(Seat)
+class SeatAdmin(admin.ModelAdmin):
+    raw_id_fields = ('changed_by',)
+
+
+@admin.register(State)
+class StateAdmin(admin.ModelAdmin):
+    raw_id_fields = ('changed_by',)
