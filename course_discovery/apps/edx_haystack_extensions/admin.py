53837a51e9c541aac890cccef41e86c3d159c3a0
Matthew Piatetsky <mpiatetsky@edx.org>
2017-02-15 14:05:42 -0500

    Remove ElasticsearchBoostConfig migrations and config
    ECOM-7077

--- course_discovery/apps/edx_haystack_extensions/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from solo.admin import SingletonModelAdmin
-
-from course_discovery.apps.edx_haystack_extensions.models import ElasticsearchBoostConfig
-
-admin.site.register(ElasticsearchBoostConfig, SingletonModelAdmin)
