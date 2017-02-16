e86548a91240c8f7fd8e98768c03276df785673b
tasawernawaz <tasawer.nawaz@arbisoft.com>
2017-02-16 16:39:45 +0500

    fix google diff configuration ECOM-7147

--- course_discovery/static/js/publisher/comparing-objects.js
+++ course_discovery/static/js/publisher/comparing-objects.js
@@ -16,8 +16,10 @@ $(document).on('click', '.btn-show-changes', function (e) {
 });
 
 var dmp = new diff_match_patch();
+dmp.Diff_EditCost = 8;
 function showDiff($object, $historyObject, $outputDiv) {
     var d = dmp.diff_main($historyObject.text(), $object.text());
+    dmp.diff_cleanupEfficiency(d);
     $outputDiv.html(dmp.diff_prettyHtml(d));
     $historyObject.hide();
     $outputDiv.show();
@@ -25,6 +27,7 @@ function showDiff($object, $historyObject, $outputDiv) {
 
 function showDiffCourseDetails(currentObject, historyObject, $outputDiv) {
     var d = dmp.diff_main(currentObject, historyObject);
+    dmp.diff_cleanupEfficiency(d);
     $outputDiv.html(dmp.diff_prettyHtml(d));
     $outputDiv.show();
 }
