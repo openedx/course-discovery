029a24ccdce121f942939c209750427a97747cd3
Awais <awais.qureshi@arbisoft.com>
2017-03-15 17:58:11 +0500

    Implement person drupal image. Now on adding instructor it will first add on drupal and then in discovery using the same UUID.
    ECOM-7367

--- course_discovery/apps/course_metadata/exceptions.py
+++ course_discovery/apps/course_metadata/exceptions.py
@@ -10,3 +10,12 @@ class ProgramPublisherException(Exception):
         super(ProgramPublisherException, self).__init__(message)
         suffix = 'The program data has not been saved. Please check your marketing site configuration'
         self.message = '{exception_msg} {suffix}'.format(exception_msg=message, suffix=suffix)
+
+
+class PersonToMarketingException(Exception):
+    """ The exception thrown during the person adding process to marketing site """
+
+    def __init__(self, message):
+        super(PersonToMarketingException, self).__init__(message)
+        suffix = 'The person data has not been saved. Please check your marketing site configuration'
+        self.message = '{exception_msg} {suffix}'.format(exception_msg=message, suffix=suffix)
