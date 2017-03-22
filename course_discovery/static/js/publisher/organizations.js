2fe5555989d541a94a0329456819990be65c675b
tasawernawaz <tasawer.nawaz@arbisoft.com>
2017-03-22 12:59:33 +0500

    make current user default course team admin ECOM-7496

--- course_discovery/static/js/publisher/organizations.js
+++ course_discovery/static/js/publisher/organizations.js
@@ -21,7 +21,8 @@ function loadAdminUsers(org_id) {
         success: function (data) {
             var teamAdminDropDown = $('#id_team_admin'),
                 selectedTeamAdmin = $('#id_team_admin option:selected').val(),
-                organizationInputType = $('#id_organization').attr('type');
+                organizationInputType = $('#id_organization').attr('type'),
+                currentUser = teamAdminDropDown.data('user');
             teamAdminDropDown.empty();
 
             if (organizationInputType == 'hidden' ) {
@@ -32,7 +33,7 @@ function loadAdminUsers(org_id) {
             }
 
             $.each(data.results, function (i, user) {
-                if (selectedTeamAdmin == user.id && organizationInputType === 'hidden' ) {
+                if ((selectedTeamAdmin == user.id && organizationInputType === 'hidden') || currentUser == user.id) {
                     teamAdminDropDown.append(
                         $('<option selected="selected"> </option>').val(user.id).html(user.full_name)
                     );
