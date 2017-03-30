b3cdda94282462e201df94c464d3c4c3108789ee
Waheed Ahmed <waheed.ahmed@arbisoft.com>
2017-03-30 15:26:28 +0500

    Added FAQ, Video link and learner testimonial fields in course edit form. ECOM-7618

--- course_discovery/apps/publisher/forms.py
+++ course_discovery/apps/publisher/forms.py
@@ -141,6 +141,7 @@ class CustomCourseForm(CourseForm):
             'expected_learnings', 'primary_subject', 'secondary_subject',
             'tertiary_subject', 'prerequisites', 'image', 'team_admin',
             'level_type', 'organization', 'is_seo_review', 'syllabus',
+            'learner_testimonial', 'faq', 'video_link',
         )
 
     def __init__(self, *args, **kwargs):
