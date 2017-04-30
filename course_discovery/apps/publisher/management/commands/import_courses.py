5e95f19fc9037eac086e314eb255f24d3754ecf6
Awais <awais.qureshi@arbisoft.com>
2017-05-01 01:09:22 +0500

    Proto-type code for import course-metadata courses into publisher app.
    ECOM-7712

--- course_discovery/apps/publisher/management/commands/import_courses.py
+++ course_discovery/apps/publisher/management/commands/import_courses.py
@@ -1,10 +1,9 @@
 import logging
 
-from django.core.management import BaseCommand, CommandError
-from django.db import connection
+from django.core.management import BaseCommand
 from django.shortcuts import get_object_or_404
 
-from course_discovery.apps.core.models import Partner, User
+from course_discovery.apps.core.models import Partner
 from course_discovery.apps.course_metadata.models import Course as Metadata_Course
 from course_discovery.apps.course_metadata.models import Organization
 from course_discovery.apps.publisher.choices import CourseRunStateChoices, CourseStateChoices, PublisherUserRole
@@ -57,11 +56,11 @@ class Command(BaseCommand):
             OrganizationExtension, organization=organization
         )
 
-        if organization.organization_user_roles.all().count() != 4:
-            raise CommandError('Organization User Roles are missing.')
+        # if organization.organization_user_roles.all().count() != 4:
+        #     raise CommandError('Organization User Roles are missing.')
 
-        courses = Metadata_Course.objects.filter(partner=partner, authoring_organizations=organization)
-        for course in courses:
+        # courses = Metadata_Course.objects.filter(partner=partner, authoring_organizations=organization)
+        for course in Metadata_Course.objects.all():
             self.get_or_create_course(course)
 
     def get_or_create_course(self, meta_data_course):
@@ -74,7 +73,7 @@ class Command(BaseCommand):
             'card_image_url': meta_data_course.card_image_url
         }
 
-        course, created = Publisher_Course.objects.update_or_create(
+        publisher_course, created = Publisher_Course.objects.update_or_create(
             number__iexact=meta_data_course.number,
             title__iexact=meta_data_course.title,
             defaults=defaults
@@ -85,55 +84,36 @@ class Command(BaseCommand):
             subject_count = subjects.count()
 
             if subject_count == 1:
-                course.primary_subject = subjects[0]
+                publisher_course.primary_subject = subjects[0]
 
             if subject_count == 2:
-                course.primary_subject = subjects[0]
-                course.secondary_subject = subjects[1]
+                publisher_course.primary_subject = subjects[0]
+                publisher_course.secondary_subject = subjects[1]
 
             if subject_count == 3:
-                course.primary_subject = subjects[0]
-                course.secondary_subject = subjects[1]
-                course.tertiary_subject = subjects[2]
+                publisher_course.primary_subject = subjects[0]
+                publisher_course.secondary_subject = subjects[1]
+                publisher_course.tertiary_subject = subjects[2]
 
-        course.save()
-
-        organization = meta_data_course.authoring_organizations.all().first()
+        publisher_course.save()
 
         if created:
-            self.assign_roles(course, organization)
+            for organization in meta_data_course.authoring_organizations.all():
+                self.assign_course_user_roles(publisher_course, organization)
 
-        for metadata_course_run in meta_data_course.course_runs.all():
-            defaults = {
-                'start': metadata_course_run.start, 'end': metadata_course_run.end,
-                'enrollment_start': metadata_course_run.enrollment_start, 'enrollment_end': metadata_course_run.enrollment_end,
-                'min_effort': metadata_course_run.min_effort, 'max_effort': metadata_course_run.max_effort,
-                'language': metadata_course_run.language, 'pacing_type': metadata_course_run.pacing_type,
-
-            }
-            course_run, created = Publisher_CourseRun.objects.update_or_create(
-                course=course, lms_course_id=metadata_course_run.key, defaults=defaults
+            # marked course as approved with related fields.
+            CourseState.objects.create(
+                course=publisher_course, approved_by_role=PublisherUserRole.MarketingReviewer,
+                owner_role=PublisherUserRole.MarketingReviewer, marketing_reviewed=True,
+                name=CourseStateChoices.Approved
             )
 
-            # # # Initialize workflow for Course-run.
-            if created:
-                CourseRunState.objects.create(
-                    course_run=course_run, approved_by_role=PublisherUserRole.ProjectCoordinator,
-                    owner_role=PublisherUserRole.Publisher, preview_accepted=True,
-                    name=CourseRunStateChoices.Published
-                )
+        self.create_course_runs(meta_data_course, publisher_course)
 
-            for metadata_seat in metadata_course_run.seats.all():
-                defaults = {'price': metadata_seat.price, 'currency': metadata_seat.currency}
-                seat, created = Publisher_Seat.objects.update_or_create(
-                    course_run=course_run,
-                    type=metadata_seat.type,
-                    defaults=defaults
-                )
+        return (publisher_course, created)
 
-        return (course, created)
-
-    def assign_roles(self, course, organization):
+    def assign_course_user_roles(self, course, organization):
+        # Assign the course user roles against each organization users.
 
         course.organizations.add(organization)
 
@@ -141,8 +121,46 @@ class Command(BaseCommand):
         for user_role in organization.organization_user_roles.all():
             CourseUserRole.add_course_roles(course, user_role.role, user_role.user)
 
-        CourseState.objects.create(
-            course=course, approved_by_role=PublisherUserRole.MarketingReviewer,
-            owner_role=PublisherUserRole.MarketingReviewer, marketing_reviewed=True,
-            name=CourseStateChoices.Approved
-        )
+    def create_course_runs(self, meta_data_course, publisher_course):
+        # create or update all metadata course runs.
+        for metadata_course_run in meta_data_course.course_runs.all():
+            defaults = {
+                'start': metadata_course_run.start, 'end': metadata_course_run.end,
+                'enrollment_start': metadata_course_run.enrollment_start,
+                'enrollment_end': metadata_course_run.enrollment_end,
+                'min_effort': metadata_course_run.min_effort, 'max_effort': metadata_course_run.max_effort,
+                'language': metadata_course_run.language, 'pacing_type': metadata_course_run.pacing_type,
+                'length': metadata_course_run.weeks_to_complete
+            }
+            publisher_course_run, created = Publisher_CourseRun.objects.update_or_create(
+                course=publisher_course, lms_course_id=metadata_course_run.key, defaults=defaults
+            )
+
+            # add many to many fields.
+            publisher_course_run.transcript_languages.add(*metadata_course_run.transcript_languages.all())
+            publisher_course_run.staff.add(*metadata_course_run.staff.all())
+
+            # Initialize workflow for Course-run.
+            if created:
+                CourseRunState.objects.create(
+                    course_run=publisher_course_run, approved_by_role=PublisherUserRole.ProjectCoordinator,
+                    owner_role=PublisherUserRole.Publisher, preview_accepted=True,
+                    name=CourseRunStateChoices.Published
+                )
+
+            self.create_seats(metadata_course_run, publisher_course_run)
+
+    def create_seats(self, metadata_course_run, publisher_course_run):
+        # create or update all metadata course runs.
+
+        for metadata_seat in metadata_course_run.seats.all():
+            defaults = {
+                'price': metadata_seat.price, 'currency': metadata_seat.currency,
+                'credit_provider': metadata_seat.credit_provider, 'credit_hours': metadata_seat.credit_hours,
+                'upgrade_deadline': metadata_seat.upgrade_deadline
+            }
+            publisher_seat, created = Publisher_Seat.objects.update_or_create(
+                course_run=publisher_course_run,
+                type=metadata_seat.type,
+                defaults=defaults
+            )
