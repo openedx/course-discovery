6d1b800bee6e1ddc80156a1c5221ff11ab79dad2
Waheed Ahmed <waheed.ahmed@arbisoft.com>
2017-05-11 16:00:12 +0500

    Removed CourseTeam from organization role dropdown choices. LEARNER-810

--- course_discovery/apps/publisher/choices.py
+++ course_discovery/apps/publisher/choices.py
@@ -2,11 +2,14 @@ from django.utils.translation import ugettext_lazy as _
 from djchoices import ChoiceItem, DjangoChoices
 
 
-class PublisherUserRole(DjangoChoices):
+class InternalUserRole(DjangoChoices):
     PartnerManager = ChoiceItem('partner_manager', _('Partner Manager'))
     ProjectCoordinator = ChoiceItem('project_coordinator', _('Project Coordinator'))
     MarketingReviewer = ChoiceItem('marketing_reviewer', _('Marketing Reviewer'))
     Publisher = ChoiceItem('publisher', _('Publisher'))
+
+
+class PublisherUserRole(InternalUserRole):
     CourseTeam = ChoiceItem('course_team', _('Course Team'))
 
 
