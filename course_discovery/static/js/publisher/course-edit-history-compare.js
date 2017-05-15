df178e03a0d6dfca85c8c0e3f8428001643bf52e
Awais <awais.qureshi@arbisoft.com>
2017-05-15 18:13:10 +0500

    Fixing bug.
    ECOM-7778

--- course_discovery/static/js/publisher/course-edit-history-compare.js
+++ course_discovery/static/js/publisher/course-edit-history-compare.js
@@ -1,15 +1,16 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('.history').each(function () {
-        var dmp = new diff_match_patch();
-        var element_id = this.id.split('_revision')[0];
+        if ($('#id_history_revision').val() > 0) {
+            var dmp = new diff_match_patch();
+            var element_id = this.id.split('_revision')[0];
 
-        var current_course_object = $('#' + element_id).val();
-        var history_object_value = $(this).val().trim();
+            var current_course_object = $('#' + element_id).val();
+            var history_object_value = $(this).val().trim();
 
-        var d = dmp.diff_main(history_object_value, current_course_object);
-        dmp.diff_cleanupEfficiency(d)
-        tinymce.get(element_id).setContent(decodeEntities(dmp.diff_prettyHtml(d)))
-
+            var d = dmp.diff_main(history_object_value, current_course_object);
+            dmp.diff_cleanupEfficiency(d)
+            tinymce.get(element_id).setContent(decodeEntities(dmp.diff_prettyHtml(d)))
+        }
     });
 });
 
@@ -18,4 +19,3 @@ function decodeEntities(encodedString) {
     textArea.innerHTML = encodedString;
     return textArea.value;
 }
-
