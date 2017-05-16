e04a1b304eba1aa0085a9a0f8e5965d9853cfb92
Awais <awais.qureshi@arbisoft.com>
2017-05-16 16:16:52 +0500

    Implementing accept/reject changes button.
    ECOM-7779

--- course_discovery/static/js/publisher/course-edit-history-compare.js
+++ course_discovery/static/js/publisher/course-edit-history-compare.js
@@ -19,3 +19,19 @@ function decodeEntities(encodedString) {
     textArea.innerHTML = encodedString;
     return textArea.value;
 }
+
+function hasValidData(){
+    for (var i = 0; i < tinyMCE.editors.length; i++) {
+        var editor = tinyMCE.editors[i];
+        if (editor.dom.select('ins').length > 0) {
+            editor.focus();
+            return false;
+        }
+
+        if (editor.dom.select('del').length > 0) {
+            editor.focus();
+            return false;
+        }
+    }
+    return true;
+}
