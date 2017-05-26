1592dc4a587f3968d3dacaef78f0a94062d8088c
Awais <awais.qureshi@arbisoft.com>
2017-05-26 15:37:23 +0500

    Fixing issue related with tinymce.
    ECOM-7830

--- course_discovery/static/js/publisher/diff_match_patching.js
+++ course_discovery/static/js/publisher/diff_match_patching.js
@@ -14,6 +14,26 @@ function diffMatchBugFixHack(decodedString) {
     fixedString = fixedString.replace(pattern_li_ins_fix, "<li>$1$2$3</li>");
     fixedString = fixedString.replace(pattern_li_del_fix, "<li>$1$2$3</li>");
 
+    fixedString = diffMatchBugFixHack_for_bullets(fixedString)
+    return fixedString;
+}
+
+function diffMatchBugFixHack_for_bullets(decodedString) {
+
+    //This function fixes a critical bug in diffmatch while diffing ul
+
+    var pattern_ul_cleanup = /(<ol>.*?)¶<br>(.*?<\/ol>)/g;
+    var pattern_li_ins_fix = /(<ins.*?>)<ol>(.*)?<\/ol>(<\/ins>)/g;
+    var pattern_li_del_fix = /(<del.*?>)<ol>(.*)?<\/ol>(<\/del>)/g;
+    var fixedString = decodedString.replace(pattern_ul_cleanup, "$1$2");
+
+    while (fixedString.length < decodedString.length) {
+        decodedString = fixedString;
+        fixedString = decodedString.replace(pattern_ul_cleanup, "$1$2");
+    }
+    fixedString = fixedString.replace(pattern_li_ins_fix, "<ol>$1$2$3</ol>");
+    fixedString = fixedString.replace(pattern_li_del_fix, "<ol>$1$2$3</ol>");
+
     return fixedString;
 }
 
