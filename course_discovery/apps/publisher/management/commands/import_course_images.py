acd2bfb5e79617fbf500bb016e94fa588f0a57c0
Awais <awais.qureshi@arbisoft.com>
2017-05-28 16:39:04 +0500

    Adding management command to import images.
    ECOM-7842

---
+++ course_discovery/apps/publisher/management/commands/import_course_images.py
@@ -0,0 +1,68 @@
+import logging
+
+from django.core.management import BaseCommand
+
+from course_discovery.apps.publisher.models import Course
+from io import BytesIO
+
+import requests
+from django.core.files import File
+
+logger = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+    help = 'Import courses into publisher app.'
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--start_id',
+            action='store',
+            dest='start_id',
+            default=None,
+            required=True,
+            help='The Primary key value starting id to import the courses.'
+
+        )
+
+        parser.add_argument(
+            '--end_id',
+            action='store',
+            dest='end_id',
+            default=None,
+            required=True,
+            help='To this id courses will be imported.'
+        )
+
+    def handle(self, *args, **options):
+        """ Import the course according to the given range. But in prod for multiple runs there are multiple
+        courses. During import just pick the latest course and do not import the old ones.
+        """
+        start_id = options.get('start_id')
+        end_id = options.get('end_id')
+
+        for course in Course.objects.filter(id__range=(start_id, end_id)):
+            self._download_image(course)
+
+    def _download_image(self, course):
+
+        # if image exists don't download it again.
+        if course.image:
+            return
+
+        course_run = course.course_runs.first()
+        if course_run and not course_run.card_image_url:
+            return
+
+        r = requests.get(course_run.card_image_url)
+        if r.status_code == 200:
+            image_data = File(BytesIO(r.content))
+            course_run.course.image.save('image.jpg', content=image_data)
+            course_run.course.save()
+            logger.info('Import')
+        else:
+            logger.exception(
+                'Loading the image for course [%s] for course-run [%s] failed',
+                course.id,
+                course_run.id
+            )
