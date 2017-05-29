37c15497e9a885dcff709cc2680cb0b1bbd75e93
Awais <awais.qureshi@arbisoft.com>
2017-05-29 17:23:17 +0500

    Adding management command to import images.
    ECOM-7842

--- course_discovery/apps/publisher/management/commands/tests/test_import_images.py
+++ course_discovery/apps/publisher/management/commands/tests/test_import_images.py
@@ -1,21 +1,20 @@
+from io import BytesIO
+
 import ddt
 import responses
 from django.contrib.auth.models import Group
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.core.management import CommandError, call_command
 from django.test import TestCase
 from guardian.shortcuts import get_group_perms
 from mock import Mock, patch
+from PIL import Image
 from testfixtures import LogCapture
+
 from course_discovery.apps.core.tests.utils import mock_jpeg_callback
 from course_discovery.apps.course_metadata.data_loaders.tests import JPEG
-
 from course_discovery.apps.publisher.models import Course
 from course_discovery.apps.publisher.tests.factories import CourseFactory, CourseRunFactory
-
-from io import BytesIO
-
-from django.core.files.uploadedfile import SimpleUploadedFile
-from PIL import Image
 
 
 # @ddt.ddt
@@ -71,11 +70,11 @@ class ImportCourseImageTests(TestCase):
     def setUp(self):
         super(ImportCourseImageTests, self).setUp()
 
-        self.course_run = CourseRunFactory(card_image_url='http://testing.jpg')
+        self.course_run = CourseRunFactory(card_image_url='https://example.com/testing.jpg')
         self.course = self.course_run.course
 
         self.command_name = 'import_course_images'
-        self.command_args = ['--start_id={}'.format(self.course.id), '--end_id={}'.format(format(self.course.id))]
+        self.command_args = ['--start_id={}'.format(self.course.id), '--end_id={}'.format(self.course.id)]
 
     @responses.activate
     def test_course_import_image_successfully(self):
@@ -86,5 +85,33 @@ class ImportCourseImageTests(TestCase):
             callback=mock_jpeg_callback(),
             content_type=JPEG
         )
-
         call_command(self.command_name, *self.command_args)
+        course = Course.objects.first()
+        self.assert_program_banner_image_loaded(course)
+
+    @responses.activate
+    def test_course_import_image_successfully(self):
+        """ Verify that course-run image successfully imported."""
+        responses.add_callback(
+            responses.GET,
+            self.course_run.card_image_url,
+            callback=mock_jpeg_callback(),
+            content_type=JPEG
+        )
+        call_command(self.command_name, *self.command_args)
+        course = Course.objects.first()
+        self.assert_program_banner_image_loaded(course)
+
+
+    def assert_program_banner_image_loaded(self, course):
+        """ Assert a program corresponding to the specified data body has banner image loaded into DB """
+        for size_key in course.image.field.variations:
+            # Get different sizes specs from the model field
+            # Then get the file path from the available files
+            sized_image = getattr(course.image, size_key, None)
+            self.assertIsNotNone(sized_image)
+            if sized_image:
+                path = getattr(course.image, size_key).url
+                self.assertIsNotNone(path)
+                self.assertIsNotNone(course.image.field.variations[size_key]['width'])
+                self.assertIsNotNone(course.image.field.variations[size_key]['height'])
