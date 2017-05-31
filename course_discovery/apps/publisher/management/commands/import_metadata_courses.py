ec7a14e59decec75e86abf9aaff24f35235dacc3
Awais <awais.qureshi@arbisoft.com>
2017-05-31 15:36:59 +0500

    Fixing import issue. Adding missing video field Optimize the query..
    ECOM-7844

--- course_discovery/apps/publisher/management/commands/import_metadata_courses.py
+++ course_discovery/apps/publisher/management/commands/import_metadata_courses.py
@@ -2,8 +2,7 @@ import logging
 
 from django.core.management import BaseCommand
 
-from course_discovery.apps.course_metadata.models import Course
-from course_discovery.apps.publisher.dataloader.create_courses import process_course
+from course_discovery.apps.publisher.dataloader.create_courses import execute_query
 
 logger = logging.getLogger(__name__)
 
@@ -32,11 +31,8 @@ class Command(BaseCommand):
         )
 
     def handle(self, *args, **options):
-        """ Import the course according to the given range. But in prod for multiple runs there are multiple
-        courses. During import just pick the latest course and do not import the old ones.
-        """
+        """ Import the course according to the given range."""
         start_id = options.get('start_id')
         end_id = options.get('end_id')
 
-        for course in Course.objects.filter(id__range=(start_id, end_id)):
-            process_course(course)
+        execute_query(start_id, end_id)
