b0305f7b590d7297220ebe6b9fb039fc9e0df0c7
Clinton Blackburn <clinton.blackburn@gmail.com>
2017-06-02 11:39:40 -0400

    Added Dockerfile
    - Added simplified Dockerfile that does not rely on Ansible
    - Updated devstack settings to read from environment variables

--- course_discovery/settings/shared/test.py
+++ course_discovery/settings/shared/test.py
@@ -3,7 +3,7 @@ import os
 HAYSTACK_CONNECTIONS = {
     'default': {
         'ENGINE': 'course_discovery.apps.edx_haystack_extensions.backends.EdxElasticsearchSearchEngine',
-        'URL': os.environ.get('TEST_ELASTICSEARCH_URL', 'http://localhost:9200/'),
+        'URL': os.environ.get('ELASTICSEARCH_URL', 'http://localhost:9200/'),
         'INDEX_NAME': 'catalog_test',
     },
 }
--- course_discovery/settings/test.py
+++ course_discovery/settings/test.py
@@ -1,4 +1,5 @@
 from course_discovery.settings.base import *
+# noinspection PyUnresolvedReferences
 from course_discovery.settings.shared.test import *
 
 INSTALLED_APPS += [
@@ -20,25 +21,6 @@ TEST_NON_SERIALIZED_APPS = [
     # at https://code.djangoproject.com/ticket/10827.
     'django.contrib.auth',
 ]
-
-CACHES = {
-    'default': {
-        'BACKEND': os.environ.get('CACHE_BACKEND', 'django.core.cache.backends.locmem.LocMemCache'),
-        'LOCATION': os.environ.get('CACHE_LOCATION', ''),
-    }
-}
-
-DATABASES = {
-    'default': {
-        'ENGINE': os.environ.get('DB_ENGINE', 'django.db.backends.sqlite3'),
-        'NAME': os.environ.get('DB_NAME', ':memory:'),
-        'USER': os.environ.get('DB_USER', ''),
-        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
-        'HOST': os.environ.get('DB_HOST', ''),
-        'PORT': os.environ.get('DB_PORT', ''),
-        'CONN_MAX_AGE': int(os.environ.get('CONN_MAX_AGE', 0)),
-    },
-}
 
 JWT_AUTH['JWT_SECRET_KEY'] = 'course-discovery-jwt-secret-key'
 
