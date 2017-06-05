bf44109b8d985078a2890565e40f47cd1b861871
Awais <awais.qureshi@arbisoft.com>
2017-06-05 15:04:39 +0500

    Adding new groups for publisher app. Assign permissions also.
    ECOM-7857

--- course_discovery/apps/publisher/constants.py
+++ course_discovery/apps/publisher/constants.py
@@ -9,5 +9,8 @@ REVIEWER_GROUP_NAME = 'Marketing Reviewers'
 PUBLISHER_GROUP_NAME = 'Publishers'
 LEGAL_TEAM_GROUP_NAME = 'Legal Team Members'
 
+GENERAL_STAFF_GROUP_NAME = 'General Staff'
+PARTNER_SUPPORT_GROUP_NAME = 'Partner Support Members'
+
 # Being used in old migration `0019_create_user_groups`.
 PARTNER_COORDINATOR_GROUP_NAME = 'Partner Coordinators'
