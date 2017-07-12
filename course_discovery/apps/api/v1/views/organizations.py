3ff708de4bc4c6833db8e17543b1918a72c2bff0
Renzo Lucioni <renzo@lucioni.xyz>
2017-07-12 17:40:02 -0400

    Upgrade to DRF 3.5.4
    3.5.4 features improved schema generation, including the ability to exclude views from generated schemas. 3.5.4 deprecates DjangoFilterBackend in favor of the implementation provided by the django-filter package.
    
    LEARNER-1590

--- course_discovery/apps/api/v1/views/organizations.py
+++ course_discovery/apps/api/v1/views/organizations.py
@@ -1,5 +1,5 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
-from rest_framework.filters import DjangoFilterBackend
 from rest_framework.permissions import IsAuthenticated
 
 from course_discovery.apps.api import filters, serializers
