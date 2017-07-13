50a0e1ab1ae128eae10de41c0fecf2aebde77f4f
Awais Jibran <awaisdar001@gmail.com>
2017-07-13 15:28:06 +0500

    Publisher admin CourseState table changes EDUCATOR-894

--- course_discovery/apps/publisher/admin.py
+++ course_discovery/apps/publisher/admin.py
@@ -71,8 +71,9 @@ class OrganizationUserRoleAdmin(admin.ModelAdmin):
 @admin.register(CourseState)
 class CourseStateAdmin(admin.ModelAdmin):
     raw_id_fields = ('changed_by',)
-    list_display = ['name', 'approved_by_role', 'owner_role', 'course', 'marketing_reviewed']
-    search_fields = ['name']
+    list_display = ['id', 'name', 'approved_by_role', 'owner_role', 'course', 'marketing_reviewed']
+    search_fields = ['id', 'course__title']
+    list_filter = ('name',)
 
 
 @admin.register(CourseRunState)
