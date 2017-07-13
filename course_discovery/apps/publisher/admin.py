ee7bb249e5158835fe5585651ebbf604d6d6c1a7
Awais Jibran <awaisdar001@gmail.com>
2017-07-13 13:52:17 +0500

    Add search by name capability in publisher admin

--- course_discovery/apps/publisher/admin.py
+++ course_discovery/apps/publisher/admin.py
@@ -80,7 +80,8 @@ class CourseRunStateAdmin(admin.ModelAdmin):
     raw_id_fields = ('changed_by',)
     list_display = ['id', 'name', 'approved_by_role', 'owner_role',
                     'course_run', 'owner_role_modified', 'preview_accepted']
-    search_fields = ['id', 'name']
+    list_filter = ('name',)
+    search_fields = ['id', 'course_run__course__title']
     ordering = ['id']
 
 
