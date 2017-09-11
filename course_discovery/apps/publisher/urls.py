281199c1a2b89c0345664e30cd3d8c33f825387d
Clinton Blackburn <cblackburn@edx.org>
2017-09-11 16:56:18 -0400

    Added view for developers to preview emails in browser
    This is a quick-and-dirty view for developers to paste code and preview
    email content. It is not intended for public consumption of any form.
    
    EDUCATOR-815

--- course_discovery/apps/publisher/urls.py
+++ course_discovery/apps/publisher/urls.py
@@ -1,12 +1,10 @@
-"""
-URLs for the course publisher views.
-"""
 from django.conf.urls import include, url
 
 from course_discovery.apps.publisher import views
 
 urlpatterns = [
     url(r'^$', views.Dashboard.as_view(), name='publisher_dashboard'),
+    url(r'^admin/importcourses/$', views.AdminImportCourse.as_view(), name='publisher_admin_import_course'),
     url(r'^api/', include('course_discovery.apps.publisher.api.urls', namespace='api')),
     url(r'^courses/$', views.CourseListView.as_view(), name='publisher_courses'),
     url(r'^courses/new/$', views.CreateCourseView.as_view(), name='publisher_courses_new'),
@@ -24,7 +22,6 @@ urlpatterns = [
         views.CreateRunFromDashboardView.as_view(),
         name='publisher_create_run_from_dashboard'
     ),
-
     url(
         r'^user/toggle/email_settings/$',
         views.ToggleEmailNotification.as_view(),
@@ -32,6 +29,5 @@ urlpatterns = [
     ),
     url(r'^courses/(?P<pk>\d+)/revisions/(?P<revision_id>\d+)/$', views.CourseRevisionView.as_view(),
         name='publisher_course_revision'),
-
-    url(r'^admin/importcourses/$', views.AdminImportCourse.as_view(), name='publisher_admin_import_course'),
+    url(r'^email_preview/$', views.EmailPreviewView.as_view(), name='email_preview'),
 ]
