060c174e35665a9341382edbecf1532fe3a15783
Clinton Blackburn <cblackburn@edx.org>
2017-09-20 14:25:53 -0400

    Removed Course.verification_deadline from Publisher
    This field is not used, and makes no sense. Only course runs have verification deadlines.
    
    LEARNER-2621

--- course_discovery/apps/publisher/models.py
+++ course_discovery/apps/publisher/models.py
@@ -56,13 +56,6 @@ class Course(TimeStampedModel, ChangedByMixin):
     syllabus = models.TextField(default=None, null=True, blank=True)
     prerequisites = models.TextField(default=None, null=True, blank=True, verbose_name=_('Prerequisites'))
     learner_testimonial = models.TextField(default=None, null=True, blank=True, verbose_name=_('Learner Testimonials'))
-    verification_deadline = models.DateTimeField(
-        null=True,
-        blank=True,
-        verbose_name=_("Verification deadline"),
-        help_text=_('Last date/time on which verification for this product can be submitted.')
-    )
-
     primary_subject = models.ForeignKey(
         Subject, default=None, null=True, blank=True, related_name='publisher_courses_primary'
     )
