50fd61d4a42cd0e1693e32fab1beb88ee4de657d
rabia23 <rabiaiftikhar2392@gmail.com>
2018-02-27 17:02:29 +0000

    EDUCATOR-2231 remove course run enrollment dates references

--- course_discovery/apps/publisher/management/commands/tests/test_import_courses.py
+++ course_discovery/apps/publisher/management/commands/tests/test_import_courses.py
@@ -349,13 +349,6 @@ class CreateCoursesTests(TestCase):
 
         self.assertEqual(publisher_course_run.start, metadata_course_run.start)
         self.assertEqual(publisher_course_run.end, metadata_course_run.end)
-        self.assertEqual(
-            publisher_course_run.enrollment_start, metadata_course_run.enrollment_start
-        )
-        self.assertEqual(
-            publisher_course_run.enrollment_end, metadata_course_run.enrollment_end
-        )
-
         self.assertEqual(publisher_course_run.min_effort, metadata_course_run.min_effort)
         self.assertEqual(publisher_course_run.max_effort, metadata_course_run.max_effort)
         self.assertEqual(publisher_course_run.length, metadata_course_run.weeks_to_complete)
