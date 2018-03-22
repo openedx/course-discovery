9f7226cbeb4871b4c26fed52f6a3d5b73de8a6c2
Bessie Steinberg <bsteinberg@edx.org>
2018-03-22 17:16:43 -0400

    Journal Bundle Get API

--- course_discovery/apps/journal/api/v1/views.py
+++ course_discovery/apps/journal/api/v1/views.py
@@ -1,8 +1,12 @@
 '''JournalViewSet'''
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
+from rest_framework_extensions.cache.mixins import CacheResponseMixin
+from rest_framework.permissions import IsAuthenticated
 
-from course_discovery.apps.journal.models import Journal
-from course_discovery.apps.journal.api.serializers import JournalSerializer
+from course_discovery.apps.journal.models import Journal, JournalBundle
+from course_discovery.apps.journal.api.serializers import JournalSerializer, JournalBundleSerializer
+from course_discovery.apps.journal import constants as journal_constants
 
 
 class JournalViewSet(viewsets.ModelViewSet):
@@ -11,3 +15,14 @@ class JournalViewSet(viewsets.ModelViewSet):
     """
     queryset = Journal.objects.all()
     serializer_class = JournalSerializer
+
+
+class JournalBundleViewSet(CacheResponseMixin, viewsets.ReadOnlyModelViewSet):
+    """ Journal Bundle"""
+    lookup_field = 'uuid'
+    lookup_value_regex = journal_constants.UUID_PATTERN
+    permission_classes = (IsAuthenticated,)
+    filter_backends = (DjangoFilterBackend,)
+
+    queryset = JournalBundle.objects.all()
+    serializer_class = JournalBundleSerializer
