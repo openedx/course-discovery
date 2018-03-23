2575fc1670cebdb7ae83fc5d0f842e9b2143e82c
Bill Filler <bfiller@edx.org>
2018-03-23 14:57:52 -0400

    fixed serializer for Journal

--- course_discovery/apps/journal/api/serializers.py
+++ course_discovery/apps/journal/api/serializers.py
@@ -3,29 +3,29 @@ from rest_framework import serializers
 
 from course_discovery.apps.api.serializers import MinimalCourseSerializer
 from course_discovery.apps.journal.models import Journal, JournalBundle
+from course_discovery.apps.core.models import Currency, Partner
 
 
-class JournalSerializer(serializers.HyperlinkedModelSerializer):
-    """Serializer for the ``Journal`` model."""
+class JournalSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the ``Journal`` model.
+    """
     price = serializers.DecimalField(
         decimal_places=Journal.PRICE_FIELD_CONFIG['decimal_places'],
         max_digits=Journal.PRICE_FIELD_CONFIG['max_digits']
     )
-    currency = serializers.SlugRelatedField(read_only=True, slug_field='code')
-    sku = serializers.CharField()
-    expires = serializers.DateTimeField()
-
-    # @classmethod
-    #def prefetch_queryset(cls):
-    #   return Journal.objects.all().select_related('currency')
+    partner = serializers.SlugRelatedField(slug_field='name', queryset=Partner.objects.all())
+    currency = serializers.SlugRelatedField(slug_field='code', queryset=Currency.objects.all())
 
     class Meta(object):
         model = Journal
-        # TODO - add partner
-        fields = ('uuid', 'title', 'price', 'currency', 'sku', 'expires')
+        fields = ('id', 'uuid', 'partner', 'title', 'price', 'currency', 'sku', 'expires')
 
 
 class JournalBundleSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the ``JournalBundle`` model.
+    """
     courses = MinimalCourseSerializer(many=True, read_only=True)
     journals = JournalSerializer(many=True, read_only=True)
     partner = serializers.SlugRelatedField(slug_field='name', read_only=True)
