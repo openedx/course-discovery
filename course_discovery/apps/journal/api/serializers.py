1ea7a213ac284bb9660aab120b93b1b0d418b8d6
Bill Filler <bfiller@edx.org>
2018-04-03 15:32:08 -0400

    make uuid writeable and fix serializer

--- course_discovery/apps/journal/api/serializers.py
+++ course_discovery/apps/journal/api/serializers.py
@@ -4,6 +4,7 @@ from rest_framework import serializers
 from course_discovery.apps.api.serializers import MinimalCourseSerializer
 from course_discovery.apps.journal.models import Journal, JournalBundle
 from course_discovery.apps.core.models import Currency, Partner
+from course_discovery.apps.course_metadata.models import Organization
 
 
 class JournalSerializer(serializers.ModelSerializer):
@@ -14,12 +15,25 @@ class JournalSerializer(serializers.ModelSerializer):
         decimal_places=Journal.PRICE_FIELD_CONFIG['decimal_places'],
         max_digits=Journal.PRICE_FIELD_CONFIG['max_digits']
     )
-    partner = serializers.SlugRelatedField(slug_field='name', queryset=Partner.objects.all())
+    partner = serializers.SlugRelatedField(slug_field='short_code', queryset=Partner.objects.all())
+    organization = serializers.SlugRelatedField(slug_field='key', queryset=Organization.objects.all())
     currency = serializers.SlugRelatedField(slug_field='code', queryset=Currency.objects.all())
 
     class Meta(object):
         model = Journal
-        fields = ('uuid', 'partner', 'title', 'price', 'currency', 'sku', 'card_image_url', 'short_description', 'full_description', 'access_length')
+        fields = (
+            'uuid',
+            'partner',
+            'organization',
+            'title',
+            'price',
+            'currency',
+            'sku',
+            'card_image_url',
+            'short_description',
+            'full_description',
+            'access_length'
+        )
 
 
 class JournalBundleSerializer(serializers.ModelSerializer):
