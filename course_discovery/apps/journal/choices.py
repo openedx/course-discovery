db3bdc832ea0338e820553a02ce0d058d8446d26
Matt Tuchfarber <mtuchfarber@edx.org>
2018-04-11 16:04:14 -0400

    Add status to Journal

---
+++ course_discovery/apps/journal/choices.py
@@ -0,0 +1,6 @@
+from django.utils.translation import ugettext_lazy as _
+from djchoices import ChoiceItem, DjangoChoices
+
+class JournalStatus(DjangoChoices):
+    Active = ChoiceItem('active', _('Active'))
+    Inactive = ChoiceItem('inactive', _('Inactive'))
