984cdd4e9f2e5d57f5085f789ff87bb5f02a0792
attiyaishaque <atiya.ishaq@arbisoft.com>
2018-07-03 11:26:22 +0500

    Add publisher user permission on discovery people API.

---
+++ course_discovery/apps/api/permissions.py
@@ -0,0 +1,11 @@
+from rest_framework.permissions import BasePermission
+
+
+class ReadByPublisherUserOnly(BasePermission):
+    """
+    Custom Permission class to check user is a publisher user..
+    """
+    def has_permission(self, request, view):
+        if request.method == 'GET':
+                return request.user.groups.exists()
+        return True
