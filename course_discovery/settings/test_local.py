32669e99295a858ffc37e7ad6a35d607d178d8f7
Alex Dusenbery <adusenbery@edx.org>
2018-08-22 16:41:09 -0400

    Add a test_local.py settings file to speed up local test setup.

---
+++ course_discovery/settings/test_local.py
@@ -0,0 +1,11 @@
+import sys
+
+from course_discovery.settings.test import *
+
+
+# This allows you to use the --reuse-db option, even with in-memory SQLite databases,
+# since /dev/shm is a filesystem on the machine's RAM, and should persist between
+# processes until you reboot.  Don't use this in travis.
+# Source: http://www.obeythetestinggoat.com/speeding-up-django-unit-tests-with-sqlite-keepdb-and-devshm.html
+DATABASES['default']['NAME'] = '/dev/shm/course_discovery.test.db.sqlite3'
+DATABASES['default']['TEST'] = {'NAME': DATABASES['default']['NAME']}
