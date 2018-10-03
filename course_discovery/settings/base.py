5a7286199b89fc0eea4c874e23a50219fa535f7d
Robert Raposa <rraposa@edx.org>
2018-10-03 16:26:05 -0400

    Add JWT Auth Middleware.
    From edx-drf-extensions:
    1. EnsureJWTAuthSettingsMiddleware: Ensures proper JWT auth settings
       for endpoints.
    2. JwtAuthCookieMiddleware: Combines the JWT auth cookie parts into a
       JWT auth cookie.
    
    ARCH-233

--- course_discovery/settings/base.py
+++ course_discovery/settings/base.py
@@ -93,6 +93,8 @@ MIDDLEWARE_CLASSES = (
     'simple_history.middleware.HistoryRequestMiddleware',
     'edx_django_utils.cache.middleware.TieredCacheMiddleware',
     'edx_rest_framework_extensions.middleware.RequestMetricsMiddleware',
+    'edx_rest_framework_extensions.auth.jwt.middleware.EnsureJWTAuthSettingsMiddleware',
+    'edx_rest_framework_extensions.auth.jwt.middleware.JwtAuthCookieMiddleware',
 )
 
 ROOT_URLCONF = 'course_discovery.urls'
