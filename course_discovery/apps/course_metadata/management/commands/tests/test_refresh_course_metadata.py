00af9dffb5a6318680a2d0b9b5eadb457a07343c
Michael Terry <mterry@edx.org>
2018-12-13 12:53:19 -0500

    Stop pulling in Person slugs from marketing site
    This also means we can drop the entire PersonMarketingSiteDataLoader
    class.

--- course_discovery/apps/course_metadata/management/commands/tests/test_refresh_course_metadata.py
+++ course_discovery/apps/course_metadata/management/commands/tests/test_refresh_course_metadata.py
@@ -13,8 +13,8 @@ from course_discovery.apps.course_metadata.data_loaders.api import (
     CoursesApiDataLoader, EcommerceApiDataLoader, OrganizationsApiDataLoader, ProgramsApiDataLoader
 )
 from course_discovery.apps.course_metadata.data_loaders.marketing_site import (
-    CourseMarketingSiteDataLoader, PersonMarketingSiteDataLoader, SchoolMarketingSiteDataLoader,
-    SponsorMarketingSiteDataLoader, SubjectMarketingSiteDataLoader
+    CourseMarketingSiteDataLoader, SchoolMarketingSiteDataLoader, SponsorMarketingSiteDataLoader,
+    SubjectMarketingSiteDataLoader
 )
 from course_discovery.apps.course_metadata.data_loaders.tests import mock_data
 from course_discovery.apps.course_metadata.management.commands.refresh_course_metadata import execute_parallel_loader
@@ -35,7 +35,6 @@ class RefreshCourseMetadataCommandTests(TransactionTestCase):
             (SubjectMarketingSiteDataLoader, partner.marketing_site_url_root, None),
             (SchoolMarketingSiteDataLoader, partner.marketing_site_url_root, None),
             (SponsorMarketingSiteDataLoader, partner.marketing_site_url_root, None),
-            (PersonMarketingSiteDataLoader, partner.marketing_site_url_root, None),
             (CourseMarketingSiteDataLoader, partner.marketing_site_url_root, None),
             (OrganizationsApiDataLoader, partner.organizations_api_url, None),
             (CoursesApiDataLoader, partner.courses_api_url, None),
@@ -206,7 +205,6 @@ class RefreshCourseMetadataCommandTests(TransactionTestCase):
                     SubjectMarketingSiteDataLoader,
                     SchoolMarketingSiteDataLoader,
                     SponsorMarketingSiteDataLoader,
-                    PersonMarketingSiteDataLoader,
                     CourseMarketingSiteDataLoader,
                     OrganizationsApiDataLoader,
                     CoursesApiDataLoader,
