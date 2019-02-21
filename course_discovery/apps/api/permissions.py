23c58a5de89a13471cc87e9de23e0aa954d22648
Michael Terry <mterry@edx.org>
2019-02-21 12:06:55 -0500

    Allow filtering courses by editable
    If you pass editable=1 to the courses list endpoint, you'll now only
    see courses that you will be able to edit.
    
    (Which means staff or editors for that course.)
    
    DISCO-539

--- course_discovery/apps/api/permissions.py
+++ course_discovery/apps/api/permissions.py
@@ -1,5 +1,7 @@
 from rest_framework.permissions import SAFE_METHODS, BasePermission
 
+from course_discovery.apps.course_metadata.models import CourseEditor
+
 
 class ReadOnlyByPublisherUser(BasePermission):
     """
@@ -17,30 +19,13 @@ class IsCourseEditorOrReadOnly(BasePermission):
     """
     def has_permission(self, request, view):
         if request.method == 'POST':
-            # You must be a member of the organization within which you are creating a course
             org = request.data.get('org')
-            return org and (
-                request.user.is_staff or
-                request.user.groups.filter(organization_extension__organization__key=org).exists()
-            )
+            return org and CourseEditor.can_create_course(request.user, org)
         else:
             return True  # other write access attempts will be caught by object permissions below
 
     def has_object_permission(self, request, view, obj):
         if request.method in SAFE_METHODS:
             return True
-        elif request.user.is_staff:  # staff users are always editors
-            return True
-
-        authoring_orgs = obj.authoring_organizations.all()
-
-        # No matter what, if an editor or their organization has been removed from the course, they can't be an editor
-        # for it. This handles cases of being dropped from an org... But might be too restrictive in case we want
-        # to allow outside guest editors on a course? Let's try this for now and see how it goes.
-        valid_editors = obj.editors.filter(user__groups__organization_extension__organization__in=authoring_orgs)
-
-        if not valid_editors.exists():
-            # No valid editors - this is an edge case where we just grant anyone in an authoring org access
-            return request.user.groups.filter(organization_extension__organization__in=authoring_orgs).exists()
         else:
-            return request.user in {x.user for x in valid_editors}
+            return CourseEditor.is_course_editable(request.user, obj)
