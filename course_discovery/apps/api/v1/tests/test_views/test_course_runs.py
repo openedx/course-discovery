8dded92dc267702af7336043c7b2915ea825af8a
Jeff LaJoie <jlajoie@edx.org>
2019-05-07 15:56:23 -0400

    DISCO-904: Swaps to set comparison for elements on transcript languages

--- course_discovery/apps/api/v1/tests/test_views/test_course_runs.py
+++ course_discovery/apps/api/v1/tests/test_views/test_course_runs.py
@@ -481,6 +481,27 @@ class CourseRunViewSetTests(SerializationMixin, ElasticsearchTestMixin, OAuth2Mi
         assert draft_course_run.status == CourseRunStatus.Unpublished
         assert draft_course_run.official_version.status == CourseRunStatus.Unpublished
 
+    @responses.activate
+    def test_patch_put_reset_status_transcript_languages(self):
+        self.mock_patch_to_studio(self.draft_course_run.key)
+        self.draft_course_run.status = CourseRunStatus.Reviewed
+        self.draft_course_run.save()
+        official_course_run = CourseRun.everything.get(key=self.draft_course_run.key, draft=False)
+        assert official_course_run.status == CourseRunStatus.Reviewed
+
+        url = reverse('api:v1:course_run-detail', kwargs={'key': self.draft_course_run.key})
+        response = self.client.put(url, {
+            'course': self.draft_course_run.course.key,  # required, so we need for a put
+            'start': self.draft_course_run.start,  # required, so we need for a put
+            'end': self.draft_course_run.end,  # required, so we need for a put
+            # Have the same number of elements, as the original array
+            'transcript_languages': ['en-us'],  # just any language tag to force a difference
+        }, format='json')
+        assert response.status_code == 200, "Status {}: {}".format(response.status_code, response.content)
+        draft_course_run = CourseRun.everything.get(key=self.draft_course_run.key, draft=True)
+        assert draft_course_run.status == CourseRunStatus.Unpublished
+        assert draft_course_run.official_version.status == CourseRunStatus.Unpublished
+
     @ddt.data(
         CourseRunStatus.Unpublished,
         CourseRunStatus.Reviewed,
