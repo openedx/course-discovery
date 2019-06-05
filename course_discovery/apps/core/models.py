ffa061f59dd5fda49e9bb6ea829cf5f44122206c
bmedx <bmesick@edx.org>
2019-06-05 13:59:27 -0400

    DE-1526 Adding history models to CourseEntitlement, Seat, Partner

--- course_discovery/apps/core/models.py
+++ course_discovery/apps/core/models.py
@@ -10,6 +10,7 @@ from django.utils.translation import ugettext_lazy as _
 from django_extensions.db.models import TimeStampedModel
 from edx_rest_api_client.client import EdxRestApiClient, OAuthAPIClient
 from guardian.mixins import GuardianUserMixin
+from simple_history.models import HistoricalRecords
 
 
 class User(GuardianUserMixin, AbstractUser):
@@ -94,6 +95,8 @@ class Partner(TimeStampedModel):
     lms_url = models.URLField(max_length=255, null=True, blank=True, verbose_name=_('LMS URL'))
     analytics_url = models.URLField(max_length=255, blank=True, verbose_name=_('Analytics API URL'), default='')
     analytics_token = models.CharField(max_length=255, blank=True, verbose_name=_('Analytics Access Token'), default='')
+
+    history = HistoricalRecords()
 
     def __str__(self):
         return self.name
