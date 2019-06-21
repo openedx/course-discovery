449c9cd9b5f4ddb5c37865898af12b15d87c8f8c
Brandon Baker <bbaker@edx.org>
2019-06-21 10:16:56 -0400

    Add CompressCacheResponseMixin
    Mixin compresses response content going into the cache in order to
    reduce the size of the objects that we're caching, which will hopefully
    fix the memcached performance issues.

--- course_discovery/settings/local.py
+++ course_discovery/settings/local.py
@@ -8,7 +8,8 @@ ALLOWED_HOSTS = ['*']
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#caches
 CACHES = {
     'default': {
-        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211',
     }
 }
 # END CACHE CONFIGURATION
