b289ae77b2e28209d66d6ae0fb689ec15490fde0
Brandon Baker <bbaker@edx.org>
2019-06-25 11:15:46 -0400

    Adds in revised CompressedCacheResponseMixin
    The mixin will eventually compress response date before going into the
    cache, in an effort to try and fix the memcached issues we are seeing
    with large items. This first commit does not have compression, but
    only attempts to safely decompress items from the cache, to avoid
    issues with blue/green deployments. A follow up will be to actually add
    compression once the safe decompression is deployed.
    
    Also, only caches JSON responses to prevent users from seeing other
    usernames in the cached browsable api responses.

--- course_discovery/apps/api/cache.py
+++ course_discovery/apps/api/cache.py
@@ -1,8 +1,11 @@
 import logging
 import time
+import zlib
 
+from django.conf import settings
 from django.core.cache import cache
-from rest_framework_extensions.key_constructor.bits import KeyBitBase, QueryParamsKeyBit
+from rest_framework_extensions.cache.decorators import CacheResponse
+from rest_framework_extensions.key_constructor.bits import KeyBitBase, QueryParamsKeyBit, UserKeyBit
 from rest_framework_extensions.key_constructor.constructors import (
     DefaultListKeyConstructor, DefaultObjectKeyConstructor
 )
@@ -25,6 +28,10 @@ class TimestampedListKeyConstructor(DefaultListKeyConstructor):
     # cache collisions when other query params are involved. For more, see:
     # https://github.com/chibisov/drf-extensions/blob/master/rest_framework_extensions/key_constructor/bits.py#L48-L49
     querystring = QueryParamsKeyBit()
+    # The UserKeyBit ensures that responses are only cached on a per-user basis,
+    # to avoid the issue where another user's username will appear in the api view
+    # rendered response, even though you are not actually the other user.
+    user = UserKeyBit()
 
 
 class TimestampedObjectKeyConstructor(DefaultObjectKeyConstructor):
@@ -32,6 +39,10 @@ class TimestampedObjectKeyConstructor(DefaultObjectKeyConstructor):
     # The DefaultObjectKeyConstructor doesn't include querystring parameters
     # in its cache key.
     querystring = QueryParamsKeyBit()
+    # The UserKeyBit ensures that responses are only cached on a per-user basis,
+    # to avoid the issue where another user's username will appear in the api view
+    # rendered response, even though you are not actually the other user.
+    user = UserKeyBit()
 
 
 def timestamped_list_key_constructor(*args, **kwargs):  # pylint: disable=unused-argument
@@ -61,3 +72,61 @@ def api_change_receiver(sender, **kwargs):  # pylint: disable=unused-argument
     )
 
     set_api_timestamp(timestamp)
+
+
+class CompressedCacheResponse(CacheResponse):
+    """
+    Subclasses CacheResponse to allow for compression of content going into the cache
+    See https://github.com/chibisov/drf-extensions/blob/0.3.1/rest_framework_extensions/cache/decorators.py#L52
+    for the implementation of process_cache_response without compression
+    """
+    def process_cache_response(self, view_instance, view_method, request, args, kwargs):
+        key = self.calculate_key(
+            view_instance=view_instance,
+            view_method=view_method,
+            request=request,
+            args=args,
+            kwargs=kwargs
+        )
+        response = self.cache.get(key)
+
+        if not response:
+            response = view_method(view_instance, request, *args, **kwargs)
+            response = view_instance.finalize_response(request, response, *args, **kwargs)
+            response.render()  # should be rendered, before pickling while storing to cache
+
+            if not response.status_code >= 400 or self.cache_errors:
+                self.cache.set(key, response, self.timeout)
+
+        if not hasattr(response, '_closable_objects'):
+            response._closable_objects = []  # pylint: disable=protected-access
+
+        try:
+            response.data = zlib.decompress(response.data)
+        except TypeError:
+            # If we get a type error, the response data was never compressed
+            pass
+
+        return response
+
+
+# Decorator for mixin
+compressed_cache_response = CompressedCacheResponse
+
+
+class CompressedCacheResponseMixin():
+    """
+    Acts like drf-extensions CacheResponseMixin, but with compression into the cache and decompression out of it
+    """
+    object_cache_key_func = timestamped_object_key_constructor
+    list_cache_key_func = timestamped_list_key_constructor
+    object_cache_timeout = settings.REST_FRAMEWORK_EXTENSIONS['DEFAULT_CACHE_RESPONSE_TIMEOUT']
+    list_cache_timeout = settings.REST_FRAMEWORK_EXTENSIONS['DEFAULT_CACHE_RESPONSE_TIMEOUT']
+
+    @compressed_cache_response(key_func=list_cache_key_func, timeout=list_cache_timeout)
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+    @compressed_cache_response(key_func=object_cache_key_func, timeout=object_cache_timeout)
+    def retrieve(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
---
+++ course_discovery/apps/api/v1/tests/test_cache.py
@@ -0,0 +1,68 @@
+import zlib
+
+from django.core.cache import cache
+from django.test import TestCase
+from rest_framework import permissions, views
+from rest_framework.response import Response
+from rest_framework_extensions.test import APIRequestFactory
+
+from course_discovery.apps.api.cache import compressed_cache_response
+
+factory = APIRequestFactory()
+
+
+class CompressedCacheResponseTest(TestCase):
+    def setUp(self):
+        super(CompressedCacheResponseTest, self).setUp()
+        self.request = factory.get('')
+        self.cache_response_key = 'cache_response_key'
+
+    def test_should_handle_getting_uncompressed_response_from_cache(self):
+        """ Verify that the decorator correctly returns uncompressed responses """
+        def key_func(**kwargs):  # pylint: disable=unused-argument
+            return self.cache_response_key
+
+        class TestView(views.APIView):
+            permission_classes = [permissions.AllowAny]
+
+            @compressed_cache_response(key_func=key_func)
+            def get(self, request, *args, **kwargs):
+                return Response('test response')
+
+        view_instance = TestView()
+        view_instance.headers = {}  # pylint: disable=attribute-defined-outside-init
+        uncompressed_cached_response = Response('cached test response')
+        view_instance.finalize_response(request=self.request, response=uncompressed_cached_response)
+        uncompressed_cached_response.render()
+        cache.set(self.cache_response_key, uncompressed_cached_response)
+
+        response = view_instance.dispatch(request=self.request)
+        self.assertEqual(response.content.decode('utf-8'), '"cached test response"')
+
+    def test_should_handle_getting_compressed_response_from_cache(self):
+        """ Verify that the decorator correctly returns compressed responses """
+        def key_func(**kwargs):  # pylint: disable=unused-argument
+            return self.cache_response_key
+
+        class TestView(views.APIView):
+            permission_classes = [permissions.AllowAny]
+
+            @compressed_cache_response(key_func=key_func)
+            def get(self, request, *args, **kwargs):
+                return Response('test response')
+
+        view_instance = TestView()
+        view_instance.headers = {}  # pylint: disable=attribute-defined-outside-init
+        compressed_cached_response = Response('compressed cached test response')
+        view_instance.finalize_response(request=self.request, response=compressed_cached_response)
+        compressed_cached_response.render()
+
+        # Data is encoded and compressed before response goes into the cache
+        compressed_cached_response.data = zlib.compress(compressed_cached_response.data.encode('utf-8'))
+        cache.set(
+            self.cache_response_key,
+            compressed_cached_response,
+        )
+
+        response = view_instance.dispatch(request=self.request)
+        self.assertEqual(response.content.decode('utf-8'), '"compressed cached test response"')
