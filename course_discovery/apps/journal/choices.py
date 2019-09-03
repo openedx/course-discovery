4ad04cf60cff41609f0f0ed2b4d9a2eb1c5eab73
Matt Tuchfarber <mtuchfarber@edx.org>
2019-09-03 15:22:21 -0400

    Migration to delete Journals' models

--- course_discovery/apps/journal/choices.py
+++
@@ -1,8 +0,0 @@
-from django.utils.translation import ugettext_lazy as _
-from djchoices import ChoiceItem, DjangoChoices
-
-
-class JournalStatus(DjangoChoices):
-    Active = ChoiceItem('active', _('Active'))
-    Inactive = ChoiceItem('inactive', _('Inactive'))
-    Retired = ChoiceItem('retired', _('Retired'))
