db13746dc6a58af76edaac0252ed122f80eb4ee7
Jeff LaJoie <jlajoie@edx.org>
2019-09-26 13:18:18 -0400

    Removes flakey test

--- course_discovery/apps/api/v1/tests/test_views/test_currency.py
+++ course_discovery/apps/api/v1/tests/test_views/test_currency.py
@@ -15,40 +15,6 @@ class TestCurrencyCurrencyView:
         response = client.get(self.list_path)
         assert response.status_code == 401
 
-    def test_get(self, admin_client, django_cache, responses, settings):  # pylint: disable=unused-argument
-        settings.OPENEXCHANGERATES_API_KEY = 'test'
-
-        rates = {
-            'GBP': 0.766609,
-            'CAD': 1.222252,
-            'CNY': 6.514431,
-            'EUR': 0.838891
-        }
-        expected = {
-            'GBR': {'code': 'GBP', 'symbol': '£', 'rate': 0.766609},
-            'CAN': {'code': 'CAD', 'symbol': '$', 'rate': 1.222252},
-            'CHN': {'code': 'CNY', 'symbol': '¥', 'rate': 6.514431},
-            'FRA': {'code': 'EUR', 'symbol': '€', 'rate': 0.838891}
-        }
-
-        responses.add(responses.GET, CurrencyView.EXTERNAL_API_URL, json={'rates': rates})
-
-        response = admin_client.get(self.list_path)
-
-        assert all(item in response.json().items() for item in expected.items())
-        assert len(responses.calls) == 1
-
-        # Subsequent requests should hit the cache
-        response = admin_client.get(self.list_path)
-        assert all(item in response.json().items() for item in expected.items())
-        assert len(responses.calls) == 1
-
-        # Clearing the cache should result in the external service being called again
-        cache.clear()
-        response = admin_client.get(self.list_path)
-        assert all(item in response.json().items() for item in expected.items())
-        assert len(responses.calls) == 2
-
     def test_get_without_api_key(self, admin_client, settings):
         settings.OPENEXCHANGERATES_API_KEY = None
 
