c0e5cb600eaebf6391c7882373450584f69b3c51
zamanafzal <zamanafzal@gmail.com>
2019-10-09 12:34:31 +0500

    Add extra test case which will check both current and expired case scenario

--- course_discovery/apps/api/tests/test_serializers.py
+++ course_discovery/apps/api/tests/test_serializers.py
@@ -1645,6 +1645,22 @@ class CourseSearchSerializerTests(TestCase, CourseSearchSerializerMixin):
         serializer = self.serialize_course(course, request)
         assert serializer.data == self.get_expected_data(course, course_run, seat)
 
+    def test_expired_and_current_course_run(self):
+        request = make_request()
+        organization = OrganizationFactory()
+        course = CourseFactory(
+            subjects=SubjectFactory.create_batch(3),
+            authoring_organizations=[organization],
+            sponsoring_organizations=[organization],
+        )
+        course_run = CourseRunFactory(course=course, end=datetime.datetime.now(UTC) + datetime.timedelta(days=10))
+        course_run_expired = CourseRunFactory(course=course)
+        course.course_runs.add(course_run, course_run_expired)
+        course.save()
+        seat = SeatFactory(course_run=course_run)
+        serializer = self.serialize_course(course, request)
+        assert serializer.data["course_runs"] == self.get_expected_data(course, course_run, seat)["course_runs"]
+
     def test_exclude_expired_course_run(self):
         request = make_request()
         organization = OrganizationFactory()
