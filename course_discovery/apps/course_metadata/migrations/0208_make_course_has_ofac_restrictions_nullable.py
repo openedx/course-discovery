ee99c7939a93f33d7d6640ce22a48b3b771ffaa3
Nicholas D'Alfonso <ndalfonso@edx.org>
2019-11-01 14:50:27 -0400

    make course has_ofac_restrictions a nullable column

---
+++ course_discovery/apps/course_metadata/migrations/0208_make_course_has_ofac_restrictions_nullable.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('course_metadata', '0207_auto_20191025_1939'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='course',
+            name='has_ofac_restrictions',
+            field=models.NullBooleanField(verbose_name='Course Has OFAC Restrictions'),
+        ),
+        migrations.AlterField(
+            model_name='historicalcourse',
+            name='has_ofac_restrictions',
+            field=models.NullBooleanField(verbose_name='Course Has OFAC Restrictions'),
+        ),
+    ]
