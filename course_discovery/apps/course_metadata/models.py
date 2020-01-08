df11db2029d0a45549b90d45be0a6912f365850b
Nick <ndalfonso@edx.org>
2020-01-08 14:22:17 -0500

    DISCO-1414 org source of truth 3 (#2355)
    - remove download org image command
    - remove usages in org model to image url fields

--- course_discovery/apps/course_metadata/models.py
+++ course_discovery/apps/course_metadata/models.py
@@ -168,12 +168,6 @@ class Organization(CachedMixin, TimeStampedModel):
     slug = AutoSlugField(populate_from='key', editable=False, slugify_function=uslugify)
     description = models.TextField(null=True, blank=True)
     homepage_url = models.URLField(max_length=255, null=True, blank=True)
-    logo_image_url = models.URLField(null=True, blank=True)
-    banner_image_url = models.URLField(null=True, blank=True)
-    certificate_logo_image_url = models.URLField(
-        null=True, blank=True, help_text=_('Logo to be displayed on certificates. If this logo is the same as '
-                                           'logo_image_url, copy and paste the same value to both fields.')
-    )
     logo_image = models.ImageField(
         upload_to=UploadToFieldNamePath(populate_from='uuid', path='organization/logos'),
         blank=True,
