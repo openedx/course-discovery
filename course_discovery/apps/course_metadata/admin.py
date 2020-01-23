d0bda280d62297435a5b318cf413d2be387aaebe
Justin Hynes <jhynes@edx.org>
2020-01-23 13:56:40 -0500

    MICROBA-84 | Add 'credit_value' to Program model
    - Add 'credit_value' field to Program model. Represents number of credits a learner will earn upon successful completion of the program
    - Add 'credit_value' to ProgramAdmin
    - Add 'credit_value' to ProgramSerializer

--- course_discovery/apps/course_metadata/admin.py
+++ course_discovery/apps/course_metadata/admin.py
@@ -187,7 +187,7 @@ class ProgramAdmin(admin.ModelAdmin):
         'order_courses_by_start_date', 'custom_course_runs_display', 'excluded_course_runs', 'authoring_organizations',
         'credit_backing_organizations', 'one_click_purchase_enabled', 'hidden', 'corporate_endorsements', 'faq',
         'individual_endorsements', 'job_outlook_items', 'expected_learning_items', 'instructor_ordering',
-        'enrollment_count', 'recent_enrollment_count',
+        'enrollment_count', 'recent_enrollment_count', 'credit_value',
     )
 
     save_error = False
