04d049f4d6ac5411839f8c19790a953bdb94a114
Rebecca Graber <rgraber@edx.org>
2020-02-07 11:16:08 -0500

    WS-549 make sure url_slug in discovery matches active url_slug, make read-only (#2412)

--- course_discovery/apps/course_metadata/admin.py
+++ course_discovery/apps/course_metadata/admin.py
@@ -76,7 +76,7 @@ class CourseAdmin(admin.ModelAdmin):
     list_display = ('uuid', 'key', 'key_for_reruns', 'title', 'draft',)
     list_filter = ('partner',)
     ordering = ('key', 'title',)
-    readonly_fields = ('uuid', 'enrollment_count', 'recent_enrollment_count',)
+    readonly_fields = ('uuid', 'enrollment_count', 'recent_enrollment_count', 'active_url_slug',)
     search_fields = ('uuid', 'key', 'key_for_reruns', 'title',)
     raw_id_fields = ('canonical_course_run', 'draft_version',)
 
