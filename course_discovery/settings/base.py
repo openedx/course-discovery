021608fa445953d250483118efc1f852bd98c13e
Zulqarnain <muhammad.zulqarnain@arbisoft.com>
2020-03-12 14:43:04 +0500

    Added Travis workers

--- course_discovery/settings/base.py
+++ course_discovery/settings/base.py
@@ -99,7 +99,6 @@ MIDDLEWARE = (
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.contrib.sites.middleware.CurrentSiteMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
