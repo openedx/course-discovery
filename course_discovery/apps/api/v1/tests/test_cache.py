8774303319be4792677d6a883149bfe7f4010199
Calen Pennington <cale@edx.org>
2020-05-05 16:01:59 -0400

    Allow pages to be opted out of page-level caching using a waffle flag

--- course_discovery/apps/api/v1/tests/test_cache.py
+++ course_discovery/apps/api/v1/tests/test_cache.py
@@ -1,11 +1,13 @@
 import zlib
 
+import ddt
 from django.core.cache import cache
 from django.test import TestCase, override_settings
 from rest_framework import permissions, views
 from rest_framework.renderers import BrowsableAPIRenderer, JSONRenderer
 from rest_framework.response import Response
 from rest_framework_extensions.test import APIRequestFactory
+from waffle.testutils import override_flag
 
 from course_discovery.apps.api.cache import compressed_cache_response
 
@@ -13,6 +15,7 @@ factory = APIRequestFactory()
 
 
 @override_settings(USE_API_CACHING=True)
+@ddt.ddt
 class CompressedCacheResponseTest(TestCase):
     def setUp(self):
         super(CompressedCacheResponseTest, self).setUp()
@@ -97,3 +100,29 @@ class CompressedCacheResponseTest(TestCase):
 
         # Verify nothing was cached
         self.assertEqual(cache.get('non_json_cache_key'), None)
+
+    @ddt.data(True, False)
+    def test_should_not_cache_if_waffled(self, waffle_active):
+        """ Verify that the decorator does not cache the waffle flag is turned off """
+        def key_func(**kwargs):  # pylint: disable=unused-argument
+            return self.cache_response_key
+
+        class TestView(views.APIView):
+            permission_classes = [permissions.AllowAny]
+            renderer_classes = [JSONRenderer]
+
+            @compressed_cache_response(key_func=key_func)
+            def get(self, request, *args, **kwargs):
+                return Response('test response')
+
+        with override_flag('compressed_cache.TestView.get', active=waffle_active):
+
+            view_instance = TestView()
+            view_instance.headers = {}  # pylint: disable=attribute-defined-outside-init
+            view_instance.dispatch(request=self.request)
+
+        # Verify nothing was cached
+        if waffle_active:
+            self.assertIsNot(cache.get(self.cache_response_key), None)
+        else:
+            self.assertIs(cache.get(self.cache_response_key), None)
