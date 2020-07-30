2fdb19ff6eba426a72babbd50efb3ccaff161537
nikolay.borovenskiy <nikolay.borovenskiy@gmail.com>
2020-07-30 17:29:28 +0300

    OeX_ES-33: determs Elasticsearch documents package

---
+++ course_discovery/apps/course_metadata/search_indexes/documents/__init__.py
@@ -0,0 +1,6 @@
+from .course_run import CourseRunDocument
+from .course import CourseDocument
+from .person import PersonDocument
+from .program import ProgramDocument
+
+__all__ = ('CourseRunDocument', 'CourseDocument', 'PersonDocument', 'ProgramDocument')
