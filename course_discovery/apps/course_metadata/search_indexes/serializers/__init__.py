41fa8eb34fa223d478b504724f2e206deb381960
nikolay.borovenskiy <nikolay.borovenskiy@gmail.com>
2020-08-03 19:28:41 +0300

    OeX_ES-33: Refactores search indexes module serializers.

--- course_discovery/apps/course_metadata/search_indexes/serializers/__init__.py
+++ course_discovery/apps/course_metadata/search_indexes/serializers/__init__.py
@@ -1,14 +1,32 @@
+from .aggregation import (
+    AggregateFacetSearchSerializer,
+    AggregateSearchModelSerializer,
+    AggregateSearchSerializer,
+    LimitedAggregateSearchSerializer,
+)
 from .common import DocumentDSLSerializerMixin, ModelObjectDocumentSerializerMixin
-from .course import CourseSearchDocumentSerializer
-from .course_run import CourseRunSearchDocumentSerializer
-from .person import PersonSearchDocumentSerializer
-from .program import ProgramSearchDocumentSerializer
+from .course import CourseFacetSerializer, CourseSearchDocumentSerializer, CourseSearchModelSerializer
+from .course_run import CourseRunFacetSerializer, CourseRunSearchDocumentSerializer, CourseRunSearchModelSerializer
+from .person import PersonFacetSerializer, PersonSearchDocumentSerializer, PersonSearchModelSerializer
+from .program import ProgramFacetSerializer, ProgramSearchDocumentSerializer, ProgramSearchModelSerializer
 
 __all__ = (
+    'AggregateSearchModelSerializer',
+    'AggregateFacetSearchSerializer',
+    'LimitedAggregateSearchSerializer',
+    'AggregateSearchSerializer',
     'CourseSearchDocumentSerializer',
+    'CourseFacetSerializer',
+    'CourseSearchModelSerializer',
     'CourseRunSearchDocumentSerializer',
+    'CourseRunFacetSerializer',
+    'CourseRunSearchModelSerializer',
     'DocumentDSLSerializerMixin',
     'ModelObjectDocumentSerializerMixin',
     'PersonSearchDocumentSerializer',
+    'PersonFacetSerializer',
+    'PersonSearchModelSerializer',
     'ProgramSearchDocumentSerializer',
+    'ProgramFacetSerializer',
+    'ProgramSearchModelSerializer',
 )
