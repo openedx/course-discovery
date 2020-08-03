ccc106d9b6f525f2d76ac3df697017d328409c92
nikolay.borovenskiy <nikolay.borovenskiy@gmail.com>
2020-08-03 11:47:55 +0300

    OeX_ES-33: Creates generic serializers mixins to handle es response. ModelObjectDocumentSerializerMixin and DocumentDSLSerializerMixin.

---
+++ course_discovery/apps/course_metadata/search_indexes/serializers/common.py
@@ -0,0 +1,62 @@
+import logging
+
+from django.core.exceptions import ObjectDoesNotExist
+from django_elasticsearch_dsl.registries import registry
+
+log = logging.getLogger(__name__)
+
+
+class ModelObjectDocumentSerializerMixin:
+    """
+    Model object document serializer mixin.
+
+    This mixin can be added to provide db model interface for elasticsearch response.
+    There is method `get_model_object_by_instance` to fetch Model object.
+    """
+
+    def get_model_object_by_instance(self, instance):
+        """
+        Provide Model object by elasticsearch response instance.
+        """
+        document = None
+        _object = None
+        index_name = instance.meta.index
+        for doc in registry.get_documents():
+            if doc._index._name == index_name:
+                document = doc
+                break
+        hit = self._build_hit(instance)
+        es_pk = hit['_source'].get('pk')
+        if document and es_pk:
+            try:
+                _object = document(hit).get_queryset().get(pk=es_pk)
+            except ObjectDoesNotExist:
+                log.error("Object could not be found in database for SearchResult '%r'.", self)
+
+        return _object
+
+    @staticmethod
+    def _build_hit(instance):
+        return {
+            '_id': instance.meta.id,
+            '_index': instance.meta.index,
+            '_score': instance.meta.score,
+            '_source': instance.to_dict(),
+            '_type': instance.meta.doc_type,
+        }
+
+
+class DocumentDSLSerializerMixin(ModelObjectDocumentSerializerMixin):
+    """
+    Document elasticsearch dsl serializer mixin.
+
+    This mixin can be added to a serializer to use the actual object
+    as the data source for serialization rather
+    than the data stored in the search index fields.
+    This makes it easy to return data from search results in
+    the same format as elsewhere in your API and reuse your existing serializers
+    """
+
+    def to_representation(self, instance):
+        _object = self.get_model_object_by_instance(instance)
+        return super(DocumentDSLSerializerMixin, self).to_representation(_object)
