acd93cfd73d2e0c1237c48ca172e657845231be7
nikolay.borovenskiy <nikolay.borovenskiy@gmail.com>
2020-08-03 16:37:49 +0300

    OeX_ES-33: Renames elasticsearch dsl default connection fixture.

--- course_discovery/apps/api/tests/test_serializers.py
+++ course_discovery/apps/api/tests/test_serializers.py
@@ -2292,7 +2292,7 @@ class PersonSearchModelSerializerTests(PersonSearchSerializerTest):
 
 
 @pytest.mark.django_db
-@pytest.mark.usefixtures('haystack_default_connection')
+@pytest.mark.usefixtures('elasticsearch_dsl_default_connection')
 class TestProgramSearchSerializer(TestCase):
     serializer_class = ProgramSearchDocumentSerializer
 
@@ -2383,7 +2383,7 @@ class ProgramSearchModelSerializerTest(TestProgramSearchSerializer):
 
 
 @pytest.mark.django_db
-@pytest.mark.usefixtures('haystack_default_connection')
+@pytest.mark.usefixtures('elasticsearch_dsl_default_connection')
 class TestTypeaheadCourseRunSearchSerializer:
     serializer_class = TypeaheadCourseRunSearchSerializer
 
@@ -2410,7 +2410,7 @@ class TestTypeaheadCourseRunSearchSerializer:
 
 
 @pytest.mark.django_db
-@pytest.mark.usefixtures('haystack_default_connection')
+@pytest.mark.usefixtures('elasticsearch_dsl_default_connection')
 class TestTypeaheadProgramSearchSerializer:
     serializer_class = TypeaheadProgramSearchSerializer
 
