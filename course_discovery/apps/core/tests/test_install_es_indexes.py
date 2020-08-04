16084d58a33ca966b6fb5cfd92b41223f37c0e2b
nikolay.borovenskiy <nikolay.borovenskiy@gmail.com>
2020-08-04 15:22:31 +0300

    OeX_ES-33: Updates test create index. Deletes test alias exists cause django es dsl has own manage.py commands to manage indexes.

--- course_discovery/apps/core/tests/test_install_es_indexes.py
+++ course_discovery/apps/core/tests/test_install_es_indexes.py
@@ -10,25 +10,14 @@ LOGGER_NAME = 'courses.management.commands.install_es_indexes'
 class InstallEsIndexesCommandTests(ElasticsearchTestMixin, TestCase):
     def test_create_index(self):
         """ Verify the app sets the alias and creates a new index. """
-        index = settings.HAYSTACK_CONNECTIONS['default']['INDEX_NAME']
+        index_names = settings.ELASTICSEARCH_INDEX_NAMES
+        for __, index_name in index_names.items():
+            # Delete the index
+            self.es.indices.delete(index=index_name, ignore=404)
+            self.assertFalse(self.es.indices.exists(index=index_name))
 
-        # Delete the index
-        self.es.indices.delete(index=index, ignore=404)
-        self.assertFalse(self.es.indices.exists(index=index))
-
-        call_command('install_es_indexes')
+        call_command('search_index', '--create')
 
         # Verify the index was created
-        self.assertTrue(self.es.indices.exists(index=index))
-
-    def test_alias_exists(self):
-        """ Verify the app does not setup a new Elasticsearch index if the alias is already set. """
-        index = settings.HAYSTACK_CONNECTIONS['default']['INDEX_NAME']
-
-        # Verify the index exists
-        self.assertTrue(self.es.indices.exists(index=index))
-
-        call_command('install_es_indexes')
-
-        # Verify the index still exists
-        self.assertTrue(self.es.indices.exists(index=index))
+        for __, index_name in index_names.items():
+            self.assertTrue(self.es.indices.exists(index=index_name))
