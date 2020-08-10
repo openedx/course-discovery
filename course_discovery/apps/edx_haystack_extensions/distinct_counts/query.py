63042d789d5f5adc7e7fbffed6ec269b71a867b0
nikolay.borovenskiy <nikolay.borovenskiy@gmail.com>
2020-08-10 17:44:13 +0300

    OeX_ES-33: Modifies distinct count module. Due to architectural aspects of elasticsearch dsl removed old haystack QueryBackend and moved the logic to the customized Search class.

--- course_discovery/apps/edx_haystack_extensions/distinct_counts/query.py
+++ course_discovery/apps/edx_haystack_extensions/distinct_counts/query.py
@@ -1,30 +1,41 @@
 from django.conf import settings
-from haystack.query import SearchQuerySet
+from elasticsearch_dsl.connections import get_connection
 
-from course_discovery.apps.edx_haystack_extensions.distinct_counts.backends import DistinctCountsSearchQuery
+from course_discovery.apps.edx_haystack_extensions.viewsets import DistinctDSLResponse, FacetedSearch
 
 
-class DistinctCountsSearchQuerySet(SearchQuerySet):
-    """Custom SearchQuerySet class that can compute and cache distinct hit and facet counts for a query."""
-
-    @staticmethod
-    def from_queryset(queryset):
-        """ Builds a DistinctCountsSearchQuerySet from an existing SearchQuerySet."""
-        return queryset._clone(klass=DistinctCountsSearchQuerySet)  # pylint: disable=protected-access
+class DistinctCountsSearchQuerySet(FacetedSearch):
+    """Custom `FacetedSearch` class that can compute and cache distinct hit and facet counts for a query."""
 
     def __init__(self, **kwargs):
         """
         Initialize a new instance of the DistinctCountsSearchQuerySet.
-
-        Overrides SearchQuerySet.__init__ from:
-        https://github.com/django-haystack/django-haystack/blob/v2.5.0/haystack/query.py#L24
         """
         super(DistinctCountsSearchQuerySet, self).__init__(**kwargs)
+        self.aggregation_key = None
         self._distinct_result_count = None
+
+    def _clone(self, klass=None):
+        """
+        Create and return a new DistinctCountsSearchQuery with fields set to match those on the original object.
+        """
+        clone = super()._clone(klass=klass)
+        if isinstance(clone, DistinctCountsSearchQuerySet):
+            clone.aggregation_key = self.aggregation_key
+            clone._distinct_result_count = self._distinct_result_count  # pylint: disable=protected-access
+
+        return clone
+
+    @staticmethod
+    def from_queryset(queryset):
+        """ Builds a DistinctCountsSearchQuerySet from an existing `Search` queryset."""
+        queryset.__class__.with_distinct_counts = DistinctCountsSearchQuerySet.with_distinct_counts
+        clone = queryset._clone()  # pylint: disable=protected-access
+        return clone
 
     def with_distinct_counts(self, aggregation_key):
         """
-        Adds distinct_count aggregations to the Query.
+        Adds distinct_count aggregations to the `Search`.
 
         Arguments:
             aggregation_key (str): The field that should be used to group records when computing distinct counts.
@@ -32,44 +43,194 @@ class DistinctCountsSearchQuerySet(SearchQuerySet):
                 Using a field that is analyzed will result in inaccurate counts, as analyzed fields are broken down by
                 the search backend and will result in records being grouped by substrings of the aggregation_key field.
         """
-        clone = self._clone()
-        clone.query = clone.query._clone(DistinctCountsSearchQuery)  # pylint: disable=protected-access
-        clone.query.aggregation_key = aggregation_key
-        clone.query.validate()
+        clone = self._clone(klass=DistinctCountsSearchQuerySet)
+        clone.aggregation_key = aggregation_key
+        clone.validate()
         return clone
+
+    def execute(self, ignore_cache=False):
+        """
+        Execute the query and cache the results.
+        """
+        # Make sure that the Query is valid before running it.
+        self.validate()
+        if ignore_cache or not hasattr(self, '_response'):
+            search_query = self.to_dict()
+            backend = DistinctCountsElasticsearchQueryWrapper(self, self.aggregation_key)
+            self._response = backend.search(search_query)
+            # Use the DistinctCountsElasticsearchQueryWrapper to execute the query so that distinct hit and query
+            # counts may be computed.
+
+            self._distinct_result_count = getattr(self._response, 'distinct_hits', 0)
+
+        return self._response
+
+    def validate(self):
+        """ Verify that all `FacetedSearch` options are valid and supported by this custom `FacetedSearch` class."""
+        dicted_aggs = self.aggs.to_dict().get('aggs')
+        if dicted_aggs:
+            for agg_name, options in dicted_aggs.items():
+                aggs = options.get('aggs')
+                if aggs:
+                    for field, agg_options in aggs.items():
+                        if 'date_histogram' in agg_options.keys():
+                            raise RuntimeError('DistinctCountsSearchQuerySet does not support date facets.')
+                        self._validate_field_facet_options(field, agg_options)
+
+        if self.aggregation_key is None:
+            raise RuntimeError('aggregation_key is required.')
+
+    def _validate_field_facet_options(self, field, options):
+        """ Verify that the provided field facet options are valid and can be converted to an aggregation."""
+        supported_options = DistinctCountsElasticsearchQueryWrapper.SUPPORTED_FIELD_FACET_OPTIONS
+        options_ = list(options.values())[0]
+        for option, __ in options_.items():
+            if option not in supported_options:
+                msg = (
+                    'DistinctCountsSearchQuerySet only supports a limited set of field facet options.'
+                    'Field: {field}, Supported Options: ({supported}), Provided Options: ({provided})'
+                ).format(field=field, supported=','.join(supported_options), provided=','.join(options.keys()))
+                raise RuntimeError(msg)
+
+    @classmethod
+    def from_dict(cls, *args, **kwargs):  # pylint: disable=unused-argument
+        """ Raise an exception since we do not currently want/need to support raw queries."""
+        raise RuntimeError('DistinctCountsSearchQuerySet does not support raw queries.')
+
+    def update_from_dict(self, *args, **kwargs):  # pylint: disable=unused-argument
+        """ Raise an exception since we do not currently want/need to support raw queries."""
+        raise RuntimeError('DistinctCountsSearchQuerySet does not support raw queries.')
 
     def distinct_count(self):
         """
         Return the distinct hit count.
-
-        Note: This will raise an error if the SearchQuerySet has not been configured to compute distinct counts. It
-        will also force the query to run if it hasn't already.
         """
-        if not isinstance(self.query, DistinctCountsSearchQuery):
-            raise RuntimeError('This SearchQuerySet has not been configured to compute distinct counts.')
 
         if self._distinct_result_count is None:
-            self._distinct_result_count = self.query.get_distinct_count()
+            self.execute()
         return self._distinct_result_count
 
     def facet_counts(self):
         """
-        Return the facet counts. Note: this will cause the query to run if it hasn't already.
-
-        Override the original implementation so that if we're forced to run the query, we can
-        cache the results that come back with it and avoid having to make another request to get
-        them later. Original implementation:
-        https://github.com/django-haystack/django-haystack/blob/master/haystack/query.py#L532
+        Return the facet counts.
         """
-        if self.query.has_run():
-            return self.query.get_facet_counts()
-        else:
-            # Force the query to run and fill the cache with the first page of results.
-            # This will cause the facet_counts to be cached along with the rest of the results
-            # and could potentially reduce the number of queries required to complete a search
-            # request.
-            #
-            # Note: If there are fewer than count results for the query, ES will simply return what it
-            # has found without raising an exception.
-            self._fill_cache(0, settings.DISTINCT_COUNTS_QUERY_CACHE_WARMING_COUNT)
-            return self.query.get_facet_counts()
+        response = self.execute()
+
+        return response.facets
+
+
+class DistinctCountsElasticsearchQueryWrapper:
+    """
+    Custom search-like class that enables the computation of distinct hit and facet counts during search queries.
+    This class is not meant to be a true Search. It is meant to wrap an existing
+    Search instance and expose a very limited subset of search functionality.
+    """
+
+    # The options that are supported for building field facet aggregations.
+    SUPPORTED_FIELD_FACET_OPTIONS = {'field', 'size'}
+
+    # The default size for field facet aggregations.
+    DEFAULT_FIELD_FACET_SIZE = 100
+
+    def __init__(self, search_instance, aggregation_key):
+        """
+        Initialize a new instance of the DistinctCountsElasticsearchQueryWrapper.
+
+        Arguments:
+            search_instance (Search)
+            aggregation_key (str): The field that should be used to group records when computing distinct counts.
+                It should be a field that is NOT analyzed by the index (like one of the faceted _exact fields).
+                Using a field that is analyzed will result in inaccurate counts, as analyzed fields are broken down by
+                the search instance and will result in records being grouped by substrings of the aggregation_key field.
+        """
+        self.search_instance = search_instance
+        self.aggregation_key = aggregation_key
+        self.aggregation_name = 'distinct_{}'.format(aggregation_key)
+
+    def search(self, search_query):
+        """
+        Run a search query and return the results.
+        """
+        if not search_query:
+            return {'results': [], 'hits': 0, 'distinct_hits': 0}
+
+        self.search_instance.validate()
+        search_kwargs = self._build_search_kwargs(**search_query)
+        es = get_connection(self.search_instance._using)
+        raw_results = es.search(index=self.search_instance._index, body=search_kwargs, **self.search_instance._params)
+
+        return self._process_results(raw_results)
+
+    def _build_search_kwargs(self, *args, **kwargs):
+        """ Build and return the arguments for the elasticsearch query."""
+        aggregations = self._build_cardinality_aggregation(precision=settings.DISTINCT_COUNTS_HIT_PRECISION)
+        facets = kwargs.get('aggs', {})
+        field_facets = {key: value for key, value in facets.items() if key.startswith('_filter')}
+        query_facets = {key: value for key, value in facets.items() if key.startswith('_query')}
+        unhandled_facets = set(facets.keys()) - set(field_facets.keys()) - set(query_facets.keys())
+        if field_facets:
+            aggregations.update(
+                self._build_field_facet_aggregations(
+                    facet_dict=field_facets, precision=settings.DISTINCT_COUNTS_FACET_PRECISION
+                )
+            )
+
+        if query_facets:
+            aggregations.update(
+                self._build_query_facet_aggregations(
+                    facet_dict=query_facets, precision=settings.DISTINCT_COUNTS_FACET_PRECISION
+                )
+            )
+
+        if unhandled_facets:
+            raise RuntimeError(
+                'DistinctCountsElasticsearchQueryWrapper does not support {} facets.'.format(unhandled_facets)
+            )
+        kwargs['aggs'] = aggregations
+
+        return kwargs
+
+    def _build_cardinality_aggregation(self, precision=None):
+        """
+        Build and return a cardinality aggregation using the configured aggregation_key.
+        The elasticsearch cardinality aggregation does not guarantee accurate results. Accuracy
+        is configurable via an optional precision_threshold argument. See
+        https://www.elastic.co/guide/en/elasticsearch/reference/current/search-aggregations-metrics-cardinality-aggregation.html
+
+        Arguments:
+            precision (str): a numeric value below which counts computed by the cardinality aggregation can
+                be expected to be close to accurate. Setting this value requires a memory tradeoff of
+                about (precision * 8) bytes.
+        """
+        aggregation = {self.aggregation_name: {'cardinality': {'field': self.aggregation_key}}}
+        if precision is not None:
+            aggregation[self.aggregation_name]['cardinality']['precision_threshold'] = precision
+        return aggregation
+
+    def _build_field_facet_aggregations(self, facet_dict, precision=None):
+        """ Build and return a dictionary of aggregations for field facets."""
+        aggregations = {}
+        for facet_field_name, opts in facet_dict.items():
+            aggregations[facet_field_name] = {'aggregations': self._build_cardinality_aggregation(precision=precision)}
+            aggregations[facet_field_name].update(list(opts.get('aggs').values())[0])
+        return aggregations
+
+    def _build_query_facet_aggregations(self, facet_dict, precision=None):
+        """ Build and return a dictionary of aggregations for query facets."""
+        aggregations = {}
+        for facet_field_name, value in facet_dict.items():
+            aggregations[facet_field_name] = {
+                'filter': value.get('filter'),
+                'aggs': self._build_cardinality_aggregation(precision=precision),
+            }
+        return aggregations
+
+    def _process_results(self, raw_results, **kwargs):
+        """ Process the query results into a form that is more easily consumable by the client."""
+        raw_results['aggregations']['aggregation_name'] = self.aggregation_name
+        results = DistinctDSLResponse(self.search_instance, raw_results)
+        aggregations = raw_results['aggregations']
+        # Process the distinct hit count
+        setattr(results, 'distinct_hits', aggregations[self.aggregation_name]['value'])
+
+        return results
