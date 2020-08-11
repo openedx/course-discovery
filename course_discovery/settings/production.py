3e2b12b6eb2e595f7557f477df76220acfc0709f
nikolay.borovenskiy <nikolay.borovenskiy@gmail.com>
2020-08-11 18:48:43 +0300

    OeX_ES-33: Updates course-discovery settings.

--- course_discovery/settings/production.py
+++ course_discovery/settings/production.py
@@ -62,13 +62,10 @@ DB_OVERRIDES = dict(
     PORT=environ.get('DB_MIGRATION_PORT', DATABASES['default']['PORT']),
 )
 
-HAYSTACK_CONNECTIONS['default'].update({
-    'URL': ELASTICSEARCH_URL,
-    'INDEX_NAME': ELASTICSEARCH_INDEX_NAME,
-    'KWARGS': {
-        'verify_certs': True,
-        'ca_certs': certifi.where(),
-    },
+# To attach certifi elasticsearch host you should  do pip install certifi
+# should do the trick. elasticsearch-py will automatically look it up.
+ELASTICSEARCH_DSL['default'].update({
+    'hosts': ELASTICSEARCH_URL,
 })
 
 for override, value in DB_OVERRIDES.items():
