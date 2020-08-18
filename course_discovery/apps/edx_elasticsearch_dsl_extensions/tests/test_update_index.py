6270f48ede612033d5df94c5bfc44582be9e660d
nikolay.borovenskiy <nikolay.borovenskiy@gmail.com>
2020-08-18 12:29:21 +0300

    OeX_ES-33: Updates tests for  command.

--- course_discovery/apps/edx_elasticsearch_dsl_extensions/tests/test_update_index.py
+++ course_discovery/apps/edx_elasticsearch_dsl_extensions/tests/test_update_index.py
@@ -1,13 +1,12 @@
 import mock
 import pytest
 from django.conf import settings
-from django.core.management import CommandError, call_command
-from django.test import TestCase, override_settings
-from elasticsearch import Elasticsearch
+from django.core.management import call_command, CommandError
+from django.test import override_settings, TestCase
 from freezegun import freeze_time
 
 from course_discovery.apps.core.tests.mixins import ElasticsearchTestMixin
-from course_discovery.apps.course_metadata.tests.factories import CourseRunFactory
+from course_discovery.apps.course_metadata.tests.factories import CourseRunFactory, PersonFactory, ProgramFactory
 from course_discovery.apps.edx_elasticsearch_dsl_extensions.tests.mixins import SearchIndexTestMixin
 
 
@@ -17,23 +16,20 @@ class UpdateIndexTests(ElasticsearchTestMixin, SearchIndexTestMixin, TestCase):
     def test_handle(self):
         """ Verify the command creates a timestamped index and repoints the alias. """
         with mock.patch('course_discovery.apps.edx_elasticsearch_dsl_extensions.management.commands.'
-                        'update_index.Command.sanity_check_new_index', return_value=(True, '')):
-            call_command('update_index')
+                        'search_index.Command.sanity_check_new_index', return_value=(True, '')):
+            call_command('search_index', '--update')
 
-        alias = settings.HAYSTACK_CONNECTIONS['default']['INDEX_NAME']
-        index = '{alias}_20160621_000000'.format(alias=alias)
-
-        host = settings.HAYSTACK_CONNECTIONS['default']['URL']
-        connection = Elasticsearch(host)
-        response = connection.indices.get_alias(name=alias)
-        expected = {
-            index: {
-                'aliases': {
-                    alias: {}
+        for alias in settings.ELASTICSEARCH_INDEX_NAMES.values():
+            index = '{alias}_20160621_000000'.format(alias=alias)
+            response = self.conn.indices.get_alias(name=alias)
+            expected = {
+                index: {
+                    'aliases': {
+                        alias: {}
+                    }
                 }
             }
-        }
-        self.assertDictEqual(response, expected)
+            self.assertDictEqual(response, expected)
 
     def test_sanity_check_error(self):
         """ Verify the command raises a CommandError if new index fails the sanity check. """
@@ -45,29 +41,29 @@ class UpdateIndexTests(ElasticsearchTestMixin, SearchIndexTestMixin, TestCase):
         # Ensure that an error is raised if the sanity check does not pass
         with pytest.raises(CommandError):
             with mock.patch('course_discovery.apps.edx_elasticsearch_dsl_extensions.management.commands.'
-                            'update_index.Command.set_alias', return_value=True):
+                            'search_index.Command.set_alias', return_value=True):
                 with mock.patch('course_discovery.apps.edx_elasticsearch_dsl_extensions.management.commands.'
-                                'update_index.Command.get_record_count', return_value=record_count):
-                    call_command('update_index')
+                                'search_index.Command.get_record_count', return_value=record_count):
+                    call_command('search_index', '--update')
 
     def test_sanity_check_success(self):
         """ Verify the command does not raise a CommandError error if the new index passes the sanity check. """
-        CourseRunFactory.create_batch(30)
+        CourseRunFactory.create_batch(59)
+        ProgramFactory.create_batch(59)
+        PersonFactory.create_batch(59)
         record_count = 60
-        additional_runs = int(10 * settings.INDEX_SIZE_CHANGE_THRESHOLD - 1)
-        CourseRunFactory.create_batch(additional_runs)
 
         # Ensure that no error is raised and the sanity check passes the second time
         with mock.patch('course_discovery.apps.edx_elasticsearch_dsl_extensions.management.commands.'
-                        'update_index.Command.set_alias', return_value=True):
+                        'search_index.Command.set_alias', return_value=True):
             with mock.patch('course_discovery.apps.edx_elasticsearch_dsl_extensions.management.commands.'
-                            'update_index.Command.get_record_count', return_value=record_count):
-                call_command('update_index')
+                            'search_index.Command.get_record_count', return_value=record_count):
+                call_command('search_index', '--update')
 
     @freeze_time('2016-06-21')
     def test_sanity_check_disabled(self):
         """ Verify the sanity check can be disabled. """
         with mock.patch('course_discovery.apps.edx_elasticsearch_dsl_extensions.management.commands.'
-                        'update_index.Command.sanity_check_new_index') as mock_sanity_check_new_index:
-            call_command('update_index', disable_change_limit=True)
+                        'search_index.Command.sanity_check_new_index') as mock_sanity_check_new_index:
+            call_command('search_index', '--update', disable_change_limit=True)
             self.assertFalse(mock_sanity_check_new_index.called)
