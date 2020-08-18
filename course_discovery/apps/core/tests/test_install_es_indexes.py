4e9b2ee7bdd75fed5f848614cb4180986e0077a7
nikolay.borovenskiy <nikolay.borovenskiy@gmail.com>
2020-08-18 11:42:36 +0300

    OeX_ES-33: Updates tests for the command .

--- course_discovery/apps/core/tests/test_install_es_indexes.py
+++ course_discovery/apps/core/tests/test_install_es_indexes.py
@@ -1,6 +1,6 @@
-from django.conf import settings
 from django.core.management import call_command
 from django.test import TestCase
+from django_elasticsearch_dsl.registries import registry
 
 from course_discovery.apps.core.tests.mixins import ElasticsearchTestMixin
 
@@ -8,20 +8,28 @@ LOGGER_NAME = 'courses.management.commands.install_es_indexes'
 
 
 class InstallEsIndexesCommandTests(ElasticsearchTestMixin, TestCase):
-    def setUp(self):
-        super().setUp()
-        call_command('search_index', '--delete', '-f')
 
     def test_create_index(self):
         """ Verify the app sets the alias and creates a new index. """
-        index_names = settings.ELASTICSEARCH_INDEX_NAMES
-        for __, index_name in index_names.items():
+        for index in registry.get_indices():
             # Delete the index
-            self.es.indices.delete(index=index_name, ignore=404)
-            self.assertFalse(self.es.indices.exists(index=index_name))
+            self.es.indices.delete(index=index._name, ignore=404)
+            self.assertFalse(self.es.indices.exists(index=index._name))
 
-        call_command('search_index', '--create')
+        call_command('install_es_indexes')
 
         # Verify the index was created
-        for __, index_name in index_names.items():
+        for index in registry.get_indices():
+            self.assertTrue(self.es.indices.exists(index=index._name))
+
+    def test_alias_exists(self):
+        """ Verify the app does not setup a new Elasticsearch index if the alias is already set. """
+        index_names = [index._name for index in registry.get_indices()]
+        for index_name in index_names:
+            # Verify the index exists
+            self.assertTrue(self.es.indices.exists(index=index_name))
+
+        call_command('install_es_indexes')
+        for index_name in index_names:
+            # Verify the index still exists
             self.assertTrue(self.es.indices.exists(index=index_name))
