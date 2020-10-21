d05f99a3440adb213fe8b9f6ea661a735f2e6f54
nikolay.borovenskiy <nikolay.borovenskiy@gmail.com>
2020-10-21 18:19:09 +0300

    OeX_ES-57: Changes facet field for 'org' on 'org.raw'

--- course_discovery/apps/api/v1/views/search.py
+++ course_discovery/apps/api/v1/views/search.py
@@ -175,7 +175,7 @@ class BaseAggregateSearchViewSet(FacetQueryFieldsMixin, BaseElasticsearchDocumen
         'content_type': {'field': 'content_type', 'lookups': [LOOKUP_FILTER_TERM, LOOKUP_FILTER_TERMS]},
         'aggregation_key': {'field': 'aggregation_key', 'lookups': [LOOKUP_FILTER_TERM, LOOKUP_FILTER_TERMS]},
         'key': {'field': 'key', 'lookups': [LOOKUP_FILTER_TERM, LOOKUP_FILTER_TERMS]},
-        'org': {'field': 'org', 'lookups': [LOOKUP_FILTER_TERM, LOOKUP_FILTER_TERMS]},
+        'org': {'field': 'org.raw', 'lookups': [LOOKUP_FILTER_TERM, LOOKUP_FILTER_TERMS]},
         'authoring_organization_uuids': {
             'field': 'authoring_organization_uuids',
             'lookups': [LOOKUP_FILTER_TERM, LOOKUP_FILTER_TERMS]
