928eb5723e7275f795ff115bac149af2b8bb1773
nikolay.borovenskiy <nikolay.borovenskiy@gmail.com>
2020-10-21 18:36:13 +0300

    OeX_ES-57: Defines a module for specific custom exceptions for Elasticsearch.

---
+++ course_discovery/apps/edx_elasticsearch_dsl_extensions/exceptions.py
@@ -0,0 +1,19 @@
+from django.utils.translation import ugettext_lazy as _
+from rest_framework import status
+from rest_framework.exceptions import APIException
+
+
+class InvalidQuery(APIException):
+    """
+    API exception.
+
+    Raised when, when executing a request to fetch data from Elasticsearch,
+    there were used incorrect parameters.
+    In this case, the Elasticsearch instance itself responds with an exception
+    `search_phase_execution_exception`.
+    """
+    status_code = status.HTTP_400_BAD_REQUEST
+    default_detail = _(
+        "Request could not be completed due to an incorrect ElasticSearch query parameters"
+    )
+    default_code = "invalid_query"
