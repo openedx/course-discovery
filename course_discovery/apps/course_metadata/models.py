509086c179aa999162fb57a0cfc15bd2606e2b7b
Michael Terry <mike@mterry.name>
2020-12-01 15:22:32 -0500

    Revert "Switch to elasticsearch7 (#2846)"
    This reverts commit a769714aa420ebd170c9daeeebb1dbd6dc75fe1b.

--- course_discovery/apps/course_metadata/models.py
+++ course_discovery/apps/course_metadata/models.py
@@ -16,11 +16,9 @@ from django.db import models, transaction
 from django.db.models import F, Q
 from django.utils.functional import cached_property
 from django.utils.translation import ugettext_lazy as _
-from django_elasticsearch_dsl.registries import registry
 from django_extensions.db.fields import AutoSlugField
 from django_extensions.db.models import TimeStampedModel
-from elasticsearch.exceptions import RequestError
-from elasticsearch_dsl.query import Q as ESDSLQ
+from haystack.query import SearchQuerySet
 from parler.models import TranslatableModel, TranslatedFieldsModel
 from simple_history.models import HistoricalRecords
 from solo.models import SingletonModel
@@ -720,13 +718,7 @@ class PkSearchableMixin:
             # want everything, we don't need to actually query elasticsearch at all.
             return queryset
 
-        es_document, *_ = registry.get_documents(models=(cls,))
-        dsl_query = ESDSLQ('query_string', query=query, analyze_wildcard=True)
-        try:
-            results = es_document.search().query(dsl_query).execute()
-        except RequestError as exp:
-            logger.warning('Elasticsearch request is failed. Got exception: %r', exp)
-            results = []
+        results = SearchQuerySet().models(cls).raw_search(query)
         ids = {result.pk for result in results}
 
         return queryset.filter(pk__in=ids)
@@ -781,7 +773,7 @@ class Course(DraftModelMixin, PkSearchableMixin, CachedMixin, TimeStampedModel):
     )
     authoring_organizations = SortedManyToManyField(Organization, blank=True, related_name='authored_courses')
     sponsoring_organizations = SortedManyToManyField(Organization, blank=True, related_name='sponsored_courses')
-    collaborators = SortedManyToManyField(Collaborator, null=True, blank=True, related_name='courses_collaborated')
+    collaborators = SortedManyToManyField(Collaborator, blank=True, related_name='courses_collaborated')
     subjects = SortedManyToManyField(Subject, blank=True)
     prerequisites = models.ManyToManyField(Prerequisite, blank=True)
     level_type = models.ForeignKey(LevelType, models.CASCADE, default=None, null=True, blank=True)
@@ -1629,17 +1621,17 @@ class CourseRun(DraftModelMixin, CachedMixin, TimeStampedModel):
         Args:
             query (str) -- Elasticsearch querystring (e.g. `title:intro*`)
         Returns:
-            Search object
+            SearchQuerySet
         """
         query = clean_query(query)
-        es_document, *_ = registry.get_documents(models=(cls,))
-        queryset = es_document.search()
+        queryset = SearchQuerySet().models(cls)
+
         if query == '(*)':
             # Early-exit optimization. Wildcard searching is very expensive in elasticsearch. And since we just
-            # want everything, we don't need to actually query elasticsearch at all
-            return queryset.query(ESDSLQ('match_all'))
-        dsl_query = ESDSLQ('query_string', query=query, analyze_wildcard=True)
-        return queryset.query(dsl_query)
+            # want everything, we don't need to actually query elasticsearch at all.
+            return queryset.load_all()
+
+        return queryset.raw_search(query).load_all()
 
     def __str__(self):
         return '{key}: {title}'.format(key=self.key, title=self.title)
@@ -2123,10 +2115,6 @@ class Program(PkSearchableMixin, TimeStampedModel):
     objects = ProgramQuerySet.as_manager()
 
     history = HistoricalRecords()
-
-    class Meta:
-        ordering = ['created']
-        get_latest_by = 'created'
 
     def __str__(self):
         return self.title
