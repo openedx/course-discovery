032073f996c42754cc971b91247228ec74cfbc05
Awais Qureshi <awais.qureshi@arbisoft.com>
2021-06-17 16:12:11 +0500

    temp: testing django3 behaviour. Temp change.

--- course_discovery/apps/course_metadata/tests/test_admin.py
+++ course_discovery/apps/course_metadata/tests/test_admin.py
@@ -501,4 +501,4 @@ class PathwayAdminTest(TestCase):
         """
         import django   # pylint: disable=import-outside-toplevel
         django.get_version()
-        self.assertEqual(django.get_version(), (2, 2, 20, 'final', 0))
+        self.assertEqual(django.get_version(), (2, 2, 24, 'final', 0))
