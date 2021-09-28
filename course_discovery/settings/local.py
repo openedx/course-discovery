c3c67bceda2a92ad64b26105e3f8a43530e13865
Awais Qureshi <awais.qureshi@arbisoft.com>
2021-09-28 15:26:40 +0500

    fix: removing `algoliasearch-django` pin to get

--- course_discovery/settings/local.py
+++ course_discovery/settings/local.py
@@ -60,4 +60,3 @@ JWT_AUTH.update({
 # Lastly, see if the developer has any local overrides.
 if os.path.isfile(join(dirname(abspath(__file__)), 'private.py')):
     from .private import *  # pylint: disable=import-error
-
