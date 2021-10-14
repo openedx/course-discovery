782ab812f804a27ad07e18aa637f42cf61a47eba
Waheed Ahmed <waheed.ahmed@arbisoft.com>
2021-10-14 16:33:12 +0500

    fix: cached response headers issue (#3197)

--- course_discovery/apps/api/cache.py
+++ course_discovery/apps/api/cache.py
@@ -108,7 +108,7 @@ class CompressedCacheResponse(CacheResponse):
                 if hasattr(response, '_headers'):
                     headers = response._headers.copy()  # pylint: disable=protected-access
                 else:
-                    headers = {k: (k, v) for k, v in response.items()}
+                    headers = response.headers.copy()
 
                 response_triple = (
                     zlib.compress(response.rendered_content),
@@ -129,7 +129,7 @@ class CompressedCacheResponse(CacheResponse):
                 decompressed_content = compressed_content
 
             response = HttpResponse(content=decompressed_content, status=status)
-            response._headers = headers  # pylint: disable=protected-access
+            response.headers = headers  # pylint: disable=protected-access
 
         if not hasattr(response, '_closable_objects'):
             response._closable_objects = []  # pylint: disable=protected-access
