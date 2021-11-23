ee9caa82a31426a8658ba8c57271cf1c2ee39e21
Muneeb Ur Rehman <muneeb.github@gmail.com>
2021-11-23 17:51:19 +0500

    feat: add model for LearnerPathwayStep and few functions in LearnerPathwayNode

--- course_discovery/apps/learner_pathway/models.py
+++ course_discovery/apps/learner_pathway/models.py
@@ -7,7 +7,6 @@ from uuid import uuid4
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
-
 class AbstractModelMeta(ABCMeta, type(models.Model)):
     pass
 
@@ -16,6 +15,7 @@ class LearnerPathwayNode(models.Model, metaclass=AbstractModelMeta):
     """
     Abstract model for learner pathway related models.
     """
+    step = models.ForeignKey('LearnerPathwayStep', on_delete=models.CASCADE)
     uuid = models.UUIDField(default=uuid4, editable=False, unique=True, verbose_name=_('UUID'))
 
     class Meta:
@@ -32,3 +32,49 @@ class LearnerPathwayNode(models.Model, metaclass=AbstractModelMeta):
         """
         Subclasses must implement this method to calculate and return the list of aggregated skills.
         """
+    @classmethod
+    def get_nodes(cls, step):
+        nodes = []
+        for node_class in cls.get_subclasses():
+            nodes += node_class.objects.filter(step=step).all()
+        return nodes
+
+    @classmethod
+    def get_subclasses(cls):
+        return cls.__subclasses__()
+
+    @classmethod
+    def get_node(cls, uuid):
+        for node_class in cls.get_subclasses():
+            node = node_class.objects.filter(uuid=uuid).first()
+            if node:
+                return node
+        return None
+
+
+class LearnerPathwayStep(models.Model):
+    uuid = models.UUIDField(default=uuid4, editable=False, unique=True, verbose_name=_('UUID'))
+
+
+    def get_nodes(self):
+        return LearnerPathwayNode.get_nodes(self)
+
+    def get_node(self, uuid):
+        return LearnerPathwayNode.get_node(self,uuid)
+
+    def remove_node(self,uuid):
+        node = self.get_node(uuid)
+        if node:
+            node.delete()
+
+
+    def get_estimated_time_of_completion(self):
+        return sum([node.get_estimated_time_of_completion() for node in self.get_nodes()])
+
+    def get_skills(self):
+        skills = set()
+        for node in self.get_nodes():
+            skills.update(node.get_skills())
+        return list(skills)
+
+
