a61774080a9e28cb56258440a723dda27b8de9ec
Saleem Latif <saleem.latif.ee@gmail.com>
2021-11-29 13:26:59 +0500

    feat: added REST API endpoints for learner pathways.

---
+++ course_discovery/apps/learner_pathway/api/v1/urls.py
@@ -0,0 +1,12 @@
+""" API v1 URLs. """
+from rest_framework import routers
+
+from course_discovery.apps.learner_pathway.api.v1 import views
+
+router = routers.SimpleRouter()
+router.register(r'learner-pathway', views.LearnerPathwayViewSet)
+router.register(r'learner-pathway-step', views.LearnerPathwayStepViewSet)
+router.register(r'learner-pathway-course', views.LearnerPathwayCourseViewSet)
+router.register(r'learner-pathway-program', views.LearnerPathwayProgramViewSet)
+
+urlpatterns = router.urls
