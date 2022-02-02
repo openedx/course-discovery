e476460b68ceb2d0c9a0b36d3c5c0c10f14f01e2
muhammad-ammar <mammar@gmail.com>
2022-02-03 00:25:10 +0500

    feat: update learner pathway api with related data

--- course_discovery/apps/learner_pathway/tests/factories.py
+++ course_discovery/apps/learner_pathway/tests/factories.py
@@ -1,7 +1,10 @@
 import factory
 from factory.fuzzy import FuzzyText
 
-from course_discovery.apps.learner_pathway.models import LearnerPathway, LearnerPathwayStep
+from course_discovery.apps.course_metadata.tests.factories import CourseFactory, ProgramFactory
+from course_discovery.apps.learner_pathway.models import (
+    LearnerPathway, LearnerPathwayCourse, LearnerPathwayProgram, LearnerPathwayStep
+)
 
 
 class LearnerPathwayFactory(factory.django.DjangoModelFactory):
@@ -10,6 +13,8 @@ class LearnerPathwayFactory(factory.django.DjangoModelFactory):
 
     name = FuzzyText(prefix='learner-pathway-name-')
     uuid = factory.Faker('uuid4')
+    banner_image = factory.django.ImageField()
+    overview = FuzzyText(prefix='learner-pathway-overview-')
 
 
 class LearnerPathwayStepFactory(factory.django.DjangoModelFactory):
@@ -18,3 +23,21 @@ class LearnerPathwayStepFactory(factory.django.DjangoModelFactory):
 
     uuid = factory.Faker('uuid4')
     pathway = factory.SubFactory(LearnerPathwayFactory)
+
+
+class LearnerPathwayCourseFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = LearnerPathwayCourse
+
+    uuid = factory.Faker('uuid4')
+    step = factory.SubFactory(LearnerPathwayStepFactory)
+    course = factory.SubFactory(CourseFactory)
+
+
+class LearnerPathwayProgramFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = LearnerPathwayProgram
+
+    uuid = factory.Faker('uuid4')
+    step = factory.SubFactory(LearnerPathwayStepFactory)
+    program = factory.SubFactory(ProgramFactory)
