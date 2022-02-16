9ccd289d6d105629c2be0922cd0bd4407306a0a7
HammadAhmadWaqas <hammadahmadwaqas1@gmail.com>
2022-02-16 13:13:46 +0500

    feat: Added learner_pathway index in Algolia search.
    1. Created LearnerPathwayDocument to handle new pathways index.
    2. Added partner field in LearnerPathway model.

--- course_discovery/apps/course_metadata/search_indexes/documents/__init__.py
+++ course_discovery/apps/course_metadata/search_indexes/documents/__init__.py
@@ -1,6 +1,7 @@
 from .course import CourseDocument
 from .course_run import CourseRunDocument
+from .learner_pathway import LearnerPathwayDocument
 from .person import PersonDocument
 from .program import ProgramDocument
 
-__all__ = ('CourseRunDocument', 'CourseDocument', 'PersonDocument', 'ProgramDocument')
+__all__ = ('CourseRunDocument', 'CourseDocument', 'LearnerPathwayDocument', 'PersonDocument', 'ProgramDocument')
