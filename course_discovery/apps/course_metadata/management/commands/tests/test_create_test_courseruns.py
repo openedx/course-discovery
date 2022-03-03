ee86fcd1332fc21c513d3d026d79b4ba82621463
John Nagro <jnagro@edx.org>
2022-03-03 11:54:57 -0500

    feat: ✅ generate test data for devstack
    - fixing tests
    
    ENT-5355

--- course_discovery/apps/course_metadata/management/commands/tests/test_create_test_courseruns.py
+++ course_discovery/apps/course_metadata/management/commands/tests/test_create_test_courseruns.py
@@ -14,12 +14,10 @@ class CreateTestCourseRunsCommandTests(TestCase):
     def test_create_course_runs_command(self):
         course_run_count_before = CourseRun.objects.count()
         parnter_course_url_before = self.partner.courses_api_url
-        partner_marketing_url_before = self.partner.marketing_site_url_root
         call_command('create_test_courseruns', *self.command_args)
         course_run_count_after = CourseRun.objects.count()
         # Check that new course runs were created
         assert course_run_count_after > course_run_count_before
-        # Check that the course api and marketing URLs were updated
+        # Check that the course api URLs were updated
         self.partner.refresh_from_db()
         assert parnter_course_url_before != self.partner.courses_api_url
-        assert partner_marketing_url_before != self.partner.marketing_site_url_root
