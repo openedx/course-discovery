77f8f23e33f1309e91f36164e4122b5af24494b1
Usama Sadiq <usama.sadiq@arbisoft.com>
2022-03-25 12:38:13 +0500

    fix: fix django40 warnings (#3329)

--- course_discovery/apps/ietf_language_tags/urls.py
+++ course_discovery/apps/ietf_language_tags/urls.py
@@ -1,12 +1,13 @@
 """
 URLs for autocomplete lookups.
 """
-from django.conf.urls import url
+
+from django.urls import path
 
 from course_discovery.apps.ietf_language_tags.lookups import LanguageTagAutocomplete
 
 app_name = 'language_tags'
 
 urlpatterns = [
-    url(r'^language-tag-autocomplete/$', LanguageTagAutocomplete.as_view(), name='language-tag-autocomplete',),
+    path('language-tag-autocomplete/', LanguageTagAutocomplete.as_view(), name='language-tag-autocomplete',),
 ]
--- course_discovery/apps/learner_pathway/api/urls.py
+++ course_discovery/apps/learner_pathway/api/urls.py
@@ -4,10 +4,11 @@ Root API URLs.
 All API URLs should be versioned, so urlpatterns should only
 contain namespaces for the active versions of the API.
 """
-from django.conf.urls import include, url
+from django.conf.urls import include
+from django.urls import path
 
 app_name = 'learner_pathway'
 
 urlpatterns = [
-    url(r'^v1/', include('course_discovery.apps.learner_pathway.api.v1.urls')),
+    path('v1/', include('course_discovery.apps.learner_pathway.api.v1.urls')),
 ]
