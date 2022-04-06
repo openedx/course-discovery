770edf8fd23437478f93d1808b58dec62869b811
srwang <srwang@users.noreply.github.com>
2022-04-06 10:29:42 -0400

    fix: add back check for advertised_course_run
    Fix accidental change

--- course_discovery/apps/course_metadata/algolia_models.py
+++ course_discovery/apps/course_metadata/algolia_models.py
@@ -241,7 +241,7 @@ class AlgoliaProxyCourse(Course, AlgoliaBasicModelFieldsMixin):
                 self.partner.name == 'edX' and
                 self.availability_level and
                 bool(self.advertised_course_run) and
-                not self.hidden and
+                not self.advertised_course_run.hidden and
                 self.course_type.slug != CourseType.EXECUTIVE_EDUCATION_2U)
 
     @property
