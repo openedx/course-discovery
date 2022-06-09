238fb48df1714a408adb3bc325d3071bceca4a2f
srwang <srwang@users.noreply.github.com>
2022-06-09 15:43:13 -0400

    feat: add management command
    starting work on management command - pulls data from prod algolia

---
+++ course_discovery/apps/course_metadata/management/commands/populate_algolia_stage_with_prod_data.py
@@ -0,0 +1,25 @@
+import json
+from algoliasearch.search_client import SearchClient
+from django.core.management import BaseCommand, CommandError
+
+# write script to grab data from prod
+# write script to push data to stage
+# delete data file
+# add arguments
+
+applicationID = 'meowmeow'
+adminAPIKey = 'meowmeow'
+indexName = 'meowmeow'
+
+class Command(BaseCommand):
+    client = SearchClient.create(applicationID, adminAPIKey)
+    index = client.init_index(indexName)
+
+    hits = []
+
+    for hit in index.browse_objects({'query': ''}):
+        hits.append(hit)
+
+    with open('algolia_prod_data', 'w') as f:
+        json.dump(hits, f)
+
