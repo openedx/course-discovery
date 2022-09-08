5858361fc21a1e76c569512865f2d591123a614b
aliadnan <ali.adnan@arbisoft.com>
2022-09-09 00:26:05 +0500

    feat: add gspread client to communicate with google sheets and images

---
+++ course_discovery/apps/course_metadata/tests/test_gspread.py
@@ -0,0 +1,31 @@
+from unittest import mock
+
+from django.test import TestCase
+
+from course_discovery.apps.course_metadata.gspread_client import GspreadClient
+
+
+class GspreadClientTests(TestCase):
+    @mock.patch('course_discovery.apps.course_metadata.gspread_client.logger')
+    @mock.patch('course_discovery.apps.course_metadata.gspread_client.gspread.service_account_from_dict')
+    def test_connection_with_google(self, _mock_gspread_connection, mock_logger):
+        GspreadClient()
+        mock_logger.info.assert_called_with(
+            '[Connection Successful]: Successful connection with google service account'
+        )
+
+    @mock.patch('course_discovery.apps.course_metadata.gspread_client.logger')
+    @mock.patch('course_discovery.apps.course_metadata.gspread_client.gspread.service_account_from_dict')
+    def test_get_spread_sheet_by_url(self, _mock_gspread_connection, mock_logger):
+        client = GspreadClient()
+        client.get_spread_sheet_by_url('https://www.testsheet.com')
+        mock_logger.info.assert_called_with('[Spread Sheet Found]: Opening google sheet')
+
+    @mock.patch('course_discovery.apps.course_metadata.gspread_client.logger')
+    @mock.patch('course_discovery.apps.course_metadata.gspread_client.gspread.service_account_from_dict')
+    def test_get_worksheet_data_by_tab_id(self, _mock_gspread_connection, mock_logger):
+        client = GspreadClient()
+        spread_sheet = mock.Mock()
+        spread_sheet.worksheets.return_value = []
+        client.get_worksheet_data_by_tab_id(spread_sheet, '123456')
+        mock_logger.error.assert_called_with('[Worksheet Not Found]: No worksheet found with id: 123456')
