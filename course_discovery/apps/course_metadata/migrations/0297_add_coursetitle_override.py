574bb9dbb13edeea3741bed469d3a46b2dcdec7f
Yuchen Tu <yuchentu@192.168.1.5>
2022-09-21 08:47:47 -0300

    feat: WS-3010 update migration.

--- course_discovery/apps/course_metadata/migrations/0297_add_coursetitle_override.py
+++ course_discovery/apps/course_metadata/migrations/0297_add_coursetitle_override.py
@@ -12,5 +12,10 @@ class Migration(migrations.Migration):
             model_name='course',
             name='course_title_override',
             field=models.CharField(max_length=20, verbose_name='Course override', help_text='This field allows for override the default course to program/programme or other term you need', blank=True),
+        ),
+        migrations.AddField(
+            model_name='historicalcourse',
+            name='course_title_override',
+            field=models.CharField(max_length=20, verbose_name='Course override', help_text='This field allows for override the default course to program/programme or other term you need', blank=True),
         )
     ]
