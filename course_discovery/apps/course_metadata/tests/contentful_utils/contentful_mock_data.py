21ee16c41cedbd2a334b809a15b040fc7e4d1567
ansabgillani <syedansab500@gmail.com>
2022-12-21 23:40:49 +0500

    refactor: refactoring bootcamp contentful code for consistency

--- course_discovery/apps/course_metadata/tests/contentful_utils/contentful_mock_data.py
+++ course_discovery/apps/course_metadata/tests/contentful_utils/contentful_mock_data.py
@@ -104,11 +104,11 @@ class MockContenfulDegreeResponse:
             'test-uuid': {
                 'page_title': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
                 'subheading': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-                'about_program_heading': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-                'about_program_content': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-                'about_program_checkmarked_items': [
-                    'Lorem ipsum: dolor sit amet, consectetur adipiscing elit'
-                ],
+                'about_the_program': {
+                    'heading': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+                    'content': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+                    'checkmarked_items': ['Lorem ipsum: dolor sit amet, consectetur adipiscing elit']
+                },
                 'faq_items': [
                     {
                         'question': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
@@ -132,5 +132,154 @@ class MockContenfulDegreeResponse:
                     "heading": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
                     "body_text": "Lorem ipsum: dolor sit amet, consectetur adipiscing elit",
                 }
+            }
+        }
+
+
+def create_bootcamp_mock_response_data():
+
+    seo_entry = create_contentful_entry('seo', {
+        'pageTitle': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+        'pageDescription': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+        'languageCode': 'en'
+    })
+
+    hero_entry = create_contentful_entry('hero', {
+        'internalName': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+        'subheading': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+        'textList': {'nodeType': 'document', 'data': {}, 'content': [{'nodeType': 'unordered-list', 'data': {}, 'content': [{'nodeType': 'list-item', 'data': {}, 'content': [{'nodeType': 'paragraph', 'data': {}, 'content': [{'value': 'Lorem ipsum:', 'nodeType': 'text', 'marks': [{'type': 'bold'}], 'data': {}}, {'value': ' dolor sit amet, consectetur adipiscing elit', 'nodeType': 'text', 'marks': [], 'data': {}}]}]}, {'nodeType': 'list-item', 'data': {}, 'content': [{'nodeType': 'paragraph', 'data': {}, 'content': [{'value': 'Lorem ipsum:', 'nodeType': 'text', 'marks': [{'type': 'bold'}], 'data': {}}, {'value': ' dolor sit amet, consectetur adipiscing elit', 'nodeType': 'text', 'marks': [], 'data': {}}]}]}, {'nodeType': 'list-item', 'data': {}, 'content': [{'nodeType': 'paragraph', 'data': {}, 'content': [{'value': 'Lorem ipsum:', 'nodeType': 'text', 'marks': [{'type': 'bold'}], 'data': {}}, {'value': ' dolor sit amet, consectetur adipiscing elit', 'nodeType': 'text', 'marks': [], 'data': {}}]}]}, {'nodeType': 'list-item', 'data': {}, 'content': [{'nodeType': 'paragraph', 'data': {}, 'content': [{'value': 'Lorem ipsum:', 'nodeType': 'text', 'marks': [{'type': 'bold'}], 'data': {}}, {'value': ' dolor sit amet, consectetur adipiscing elit', 'nodeType': 'text', 'marks': [], 'data': {}}]}]}]}]},
+    })
+
+    about_the_program_entry = create_contentful_entry('aboutTheProgramModule', {
+        'internalName': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+        'heading': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+        'content': {'nodeType': 'document', 'data': {}, 'content': [{'nodeType': 'paragraph', 'data': {}, 'content': [{'value': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', 'nodeType': 'text', 'marks': [], 'data': {}}]}]},
+        'checkmarkedItems': create_contentful_entry('textListModule', {
+            'title': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+            'theme': 'background-white',
+            'textListItems': [create_contentful_entry('textListItem', {
+                   'header': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+                   'description': {'nodeType': 'document', 'data': {}, 'content': [{'nodeType': 'paragraph', 'data': {}, 'content': [{'value': 'Lorem ipsum:', 'nodeType': 'text', 'marks': [{'type': 'bold'}], 'data': {}}, {'value': ' dolor sit amet, consectetur adipiscing elit', 'nodeType': 'text', 'marks': [], 'data': {}}]}]}
+            })]
+        }),
+    })
+
+    blurb1_entry = create_contentful_entry('blurbModule', {
+        'internalName': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+        'blurbHeading': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+        'blurbBody': {'nodeType': 'document', 'data': {}, 'content': [{'nodeType': 'heading-3', 'data': {}, 'content': [{'value': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', 'nodeType': 'text', 'marks': [], 'data': {}}]}, {'nodeType': 'paragraph', 'data': {}, 'content': [{'value': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', 'nodeType': 'text', 'marks': [], 'data': {}}]}, {'nodeType': 'heading-3', 'data': {}, 'content': [{'value': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', 'nodeType': 'text', 'marks': [], 'data': {}}]}, {'nodeType': 'paragraph', 'data': {}, 'content': [{'value': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', 'nodeType': 'text', 'marks': [], 'data': {}}, {'nodeType': 'embedded-entry-inline', 'content': []}]}, {'nodeType': 'paragraph', 'data': {}, 'content': [{'value': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', 'nodeType': 'text', 'marks': [], 'data': {}}, {'nodeType': 'embedded-entry-inline', 'content': []}]}, {'nodeType': 'heading-3', 'data': {}, 'content': [{'value': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', 'nodeType': 'text', 'marks': [], 'data': {}}]}, {'nodeType': 'paragraph', 'data': {}, 'content': [{'value': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', 'nodeType': 'text', 'marks': [], 'data': {}}]}]}
+    })
+
+    bootcamp_curriculum_entry = create_contentful_entry('bootCampCurriculumModule', {
+        'internalName': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+        'subheading': {'nodeType': 'document', 'data': {}, 'content': [{'nodeType': 'heading-3', 'data': {}, 'content': [{'value': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', 'nodeType': 'text', 'marks': [], 'data': {}}]}, {'nodeType': 'paragraph', 'data': {}, 'content': [{'value': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', 'nodeType': 'text', 'marks': [], 'data': {}}, {'value': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', 'nodeType': 'text', 'marks': [{'type': 'bold'}], 'data': {}}, {'value': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', 'nodeType': 'text', 'marks': [], 'data': {}}, {'value': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', 'nodeType': 'text', 'marks': [], 'data': {}}, {'value': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', 'nodeType': 'text', 'marks': [{'type': 'bold'}], 'data': {}}, {'value': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', 'nodeType': 'text', 'marks': [], 'data': {}}]}]},
+        'items': [create_contentful_entry('faq', {
+            'name': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+            'question': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+            'answerRichText': {'nodeType': 'document', 'data': {}, 'content': [{'nodeType': 'paragraph', 'data': {}, 'content': [{'value': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', 'nodeType': 'text', 'marks': [], 'data': {}}]}, {'nodeType': 'paragraph', 'data': {}, 'content': [{'value': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', 'nodeType': 'text', 'marks': [{'type': 'bold'}], 'data': {}}]}, {'nodeType': 'unordered-list', 'data': {}, 'content': [{'nodeType': 'list-item', 'data': {}, 'content': [{'nodeType': 'paragraph', 'data': {}, 'content': [{'value': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', 'nodeType': 'text', 'marks': [], 'data': {}}]}]}, {'nodeType': 'list-item', 'data': {}, 'content': [{'nodeType': 'paragraph', 'data': {}, 'content': [{'value': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', 'nodeType': 'text', 'marks': [], 'data': {}}]}]}, {'nodeType': 'list-item', 'data': {}, 'content': [{'nodeType': 'paragraph', 'data': {}, 'content': [{'value': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', 'nodeType': 'text', 'marks': [], 'data': {}}]}]}, {'nodeType': 'list-item', 'data': {}, 'content': [{'nodeType': 'paragraph', 'data': {}, 'content': [{'value': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', 'nodeType': 'text', 'marks': [], 'data': {}}]}]}]}]}
+        })]
+    })
+
+    partnership_entry = create_contentful_entry('partnershipModule', {
+        'internalName': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+        'headingText': {'nodeType': 'document', 'data': {}, 'content': [{'nodeType': 'paragraph', 'data': {}, 'content': [{'value': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', 'nodeType': 'text', 'marks': [], 'data': {}}]}]},
+        'bodyText': {'nodeType': 'document', 'data': {}, 'content': [{'nodeType': 'paragraph', 'data': {}, 'content': [{'value': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', 'nodeType': 'text', 'marks': [], 'data': {}}, {'value': 'Lorem', 'nodeType': 'text', 'marks': [{'type': 'bold'}], 'data': {}}, {'value': ', ', 'nodeType': 'text', 'marks': [], 'data': {}}, {'value': 'ipsum', 'nodeType': 'text', 'marks': [{'type': 'bold'}], 'data': {}}, {'value': ', or ', 'nodeType': 'text', 'marks': [], 'data': {}}, {'value': 'dolor', 'nodeType': 'text', 'marks': [{'type': 'bold'}], 'data': {}}, {'value': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', 'nodeType': 'text', 'marks': [], 'data': {}}]}]},
+    })
+
+    blurb2_entry = create_contentful_entry('blurbModule', {
+        'internalName': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+        'blurbHeading': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+        'blurbBody': {'nodeType': 'document', 'data': {}, 'content': [{'nodeType': 'heading-3', 'data': {}, 'content': [{'value': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', 'nodeType': 'text', 'marks': [], 'data': {}}]}, {'nodeType': 'paragraph', 'data': {}, 'content': [{'value': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', 'nodeType': 'text', 'marks': [], 'data': {}}]}, {'nodeType': 'paragraph', 'data': {}, 'content': [{'value': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', 'nodeType': 'text', 'marks': [], 'data': {}}]}]}
+    })
+
+    faq_entry = create_contentful_entry('faqModule', {
+        'name': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+        'faqs': [
+            create_contentful_entry('faq', {
+                'name': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+                'question': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+                'answerRichText': {'nodeType': 'document', 'data': {}, 'content': [{'nodeType': 'paragraph', 'data': {}, 'content': [{'value': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', 'nodeType': 'text', 'marks': [], 'data': {}}]}, {'nodeType': 'paragraph', 'data': {}, 'content': [{'value': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', 'nodeType': 'text', 'marks': [{'type': 'bold'}], 'data': {}}]}, {'nodeType': 'unordered-list', 'data': {}, 'content': [{'nodeType': 'list-item', 'data': {}, 'content': [{'nodeType': 'paragraph', 'data': {}, 'content': [{'value': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', 'nodeType': 'text', 'marks': [], 'data': {}}]}]}, {'nodeType': 'list-item', 'data': {}, 'content': [{'nodeType': 'paragraph', 'data': {}, 'content': [{'value': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', 'nodeType': 'text', 'marks': [], 'data': {}}]}]}, {'nodeType': 'list-item', 'data': {}, 'content': [{'nodeType': 'paragraph', 'data': {}, 'content': [{'value': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', 'nodeType': 'text', 'marks': [], 'data': {}}]}]}, {'nodeType': 'list-item', 'data': {}, 'content': [{'nodeType': 'paragraph', 'data': {}, 'content': [{'value': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', 'nodeType': 'text', 'marks': [], 'data': {}}]}]}]}]}
+            })
+        ]
+    })
+
+    return create_contentful_entry('bootCampPage', {
+        'internalName': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+        'uuid': 'test-uuid',
+        'seo': seo_entry,
+        'hero': hero_entry,
+        'modules': [
+            about_the_program_entry,
+            blurb1_entry,
+            bootcamp_curriculum_entry,
+            partnership_entry,
+            blurb2_entry,
+            faq_entry,
+        ]
+    })
+
+
+class MockContentfulBootcampResponse:
+    """
+    Mock response received from Contentful for bootcamp
+    """
+    total = 15
+    items = [create_bootcamp_mock_response_data()]
+
+    def __init__(self):
+        self.mock_contentful_bootcamp_entry = self.items[0]
+        self.bootcamp_transformed_data = {
+            'test-uuid': {
+                'page_title': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+                'subheading': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+                'hero_text_list': 'Lorem ipsum: dolor sit amet, consectetur adipiscing elitLorem ipsum: dolor sit amet,'
+                ' consectetur adipiscing elitLorem ipsum: dolor sit amet, consectetur adipiscing elitLorem'
+                ' ipsum: dolor sit amet, consectetur adipiscing elit',
+                'about_the_program': {
+                    'heading': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+                    'content': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+                    'checkmarked_items': ['Lorem ipsum: dolor sit amet, consectetur adipiscing elit']
+                },
+                'blurb_1': {
+                    'heading': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+                    'body': 'Lorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet,'
+                            ' consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elit'
+                            'Lorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet,'
+                            ' consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elit'
+                            'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+                },
+                "blurb_2": {
+                    "heading": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+                    "body": 'Lorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur'
+                            ' adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elit'
+                },
+                'bootcamp_curriculum': {
+                    'heading': 'Lorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit '
+                    'amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur '
+                    'adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem '
+                    'ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet,'
+                    ' consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elit'
+                },
+                'partnerships': {
+                    'heading_text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+                    'body_text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elitLorem, ipsum, or dolorLorem '
+                                    'ipsum dolor sit amet, consectetur adipiscing elit',
+                },
+                'faq_items': [
+                    {
+                        'question': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+                        'answer': 'Lorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet,'
+                                    ' consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem'
+                                    ' ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, '
+                                    'consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elit'
+                    },
+                    {
+                        'question': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+                        'answer': 'Lorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, '
+                                    'consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem'
+                                    ' ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur'
+                                    ' adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elit'
+                    }
+                ]
             }
         }
--- course_discovery/apps/course_metadata/tests/test_contentful_utils.py
+++ course_discovery/apps/course_metadata/tests/test_contentful_utils.py
@@ -14,11 +14,8 @@ from course_discovery.apps.course_metadata.contentful_utils import (
     get_aggregated_data_from_contentful_data, get_contentful_cache_key, get_data_from_contentful,
     rich_text_to_plain_text
 )
-from course_discovery.apps.course_metadata.tests.contentful_bootcamp_mock_data import (
-    MockContentBootcampResponse, about_the_program_entry, bootcamp_transformed_data, mock_contentful_bootcamp_entry
-)
 from course_discovery.apps.course_metadata.tests.contentful_utils.contentful_mock_data import (
-    MockContenfulDegreeResponse
+    MockContenfulDegreeResponse, MockContentfulBootcampResponse, create_contentful_entry
 )
 
 LOGGER_NAME = 'course_discovery.apps.course_metadata.contentful_utils'
@@ -35,26 +32,32 @@ class TestContentfulUtils(TestCase):
         """
         Test get_data_from_contentful utility with mock data.
         """
-        mock_client.return_value.entries.return_value = MockContentBootcampResponse
-        contentful_data = get_data_from_contentful(settings.BOOTCAMP_CONTENTFUL_CONTENT_TYPE)
+        mock_response = MockContentfulBootcampResponse()
+        mock_client.return_value.entries.return_value = MockContentfulBootcampResponse
+        contentful_data = get_data_from_contentful(
+            settings.BOOTCAMP_CONTENTFUL_CONTENT_TYPE)
+
         assert len(contentful_data) == 2
-        assert contentful_data[0] == mock_contentful_bootcamp_entry
-        self.assertDictEqual(contentful_data[0].raw, mock_contentful_bootcamp_entry.raw)
+        assert contentful_data[0] == mock_response.mock_contentful_bootcamp_entry
+        self.assertDictEqual(
+            contentful_data[0].raw, mock_response.mock_contentful_bootcamp_entry.raw)
 
     @mock.patch('course_discovery.apps.course_metadata.contentful_utils.Client')
     def test_get_cached_data_from_contentful(self, mock_client):
         """
         Test get_data_from_contentful utility with mock data.
         """
-        self.maxDiff = None
-        mock_client.return_value.entries.return_value = MockContentBootcampResponse
-        cache_key = get_contentful_cache_key(settings.BOOTCAMP_CONTENTFUL_CONTENT_TYPE)
+        mock_response = MockContentfulBootcampResponse()
+        mock_client.return_value.entries.return_value = MockContentfulBootcampResponse
+        cache_key = get_contentful_cache_key(
+            settings.BOOTCAMP_CONTENTFUL_CONTENT_TYPE)
         assert cache.get(cache_key) is None
         _ = get_data_from_contentful(settings.BOOTCAMP_CONTENTFUL_CONTENT_TYPE)
         assert cache.get(cache_key) is not None
 
         with LogCapture(LOGGER_NAME) as log_capture:
-            contentful_data = get_data_from_contentful(settings.BOOTCAMP_CONTENTFUL_CONTENT_TYPE)
+            contentful_data = get_data_from_contentful(
+                settings.BOOTCAMP_CONTENTFUL_CONTENT_TYPE)
 
             log_capture.check(
                 (
@@ -65,26 +68,64 @@ class TestContentfulUtils(TestCase):
                 )
             )
             assert len(contentful_data) == 2
-            assert contentful_data[0].uuid == mock_contentful_bootcamp_entry.uuid
+            assert contentful_data[0].uuid == mock_response.mock_contentful_bootcamp_entry.uuid
 
     def test_rich_text_to_plain_text(self):
         """
         Test rich_text_to_plain_text utility which transforms rich text to plain text.
         """
+        about_the_program_entry = create_contentful_entry('aboutTheProgramModule', {
+            'internalName': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+            'heading': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+            'content': {'nodeType': 'document', 'data': {}, 'content': [
+                {
+                    'nodeType': 'paragraph', 'data': {}, 'content': [
+                        {
+                            'value': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+                            'nodeType': 'text', 'marks': [], 'data': {}
+                        }
+                    ]
+                }
+            ]},
+            'checkmarkedItems': create_contentful_entry('textListModule', {
+                'title': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+                'theme': 'background-white',
+                'textListItems': [create_contentful_entry('textListItem', {
+                    'header': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+                    'description': {'nodeType': 'document', 'data': {}, 'content': [
+                        {
+                            'nodeType': 'paragraph', 'data': {}, 'content': [
+                                {
+                                    'value': 'Lorem ipsum:',
+                                    'nodeType': 'text', 'marks': [{'type': 'bold'}], 'data': {}
+                                },
+                                {
+                                    'value': ' dolor sit amet, consectetur adipiscing elit',
+                                    'nodeType': 'text', 'marks': [], 'data': {}
+                                }
+                            ]
+                        }
+                    ]}
+                })]
+            }),
+        })
         plain_text = rich_text_to_plain_text(about_the_program_entry.content)
         expected_text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
         assert plain_text == expected_text
 
     @mock.patch('course_discovery.apps.course_metadata.contentful_utils.get_data_from_contentful',
-                return_value=[mock_contentful_bootcamp_entry])
+                return_value=[MockContentfulBootcampResponse().mock_contentful_bootcamp_entry])
     def test_transform_bootcamp_contentful_data(self, *args):
         """
         Test transform_bootcamp_contentful_data given a mocked entry from contentful.
         """
+        mock_bootcamp_response = MockContentfulBootcampResponse()
         transformed_data = fetch_and_transform_bootcamp_contentful_data()
-        self.assertDictEqual(transformed_data, bootcamp_transformed_data)
+        self.assertDictEqual(
+            transformed_data, mock_bootcamp_response.bootcamp_transformed_data)
 
     def test_get_aggregated_data_from_contentful_data(self):
+        mock_bootcamp_response = MockContentfulBootcampResponse()
         expected_data = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, ' \
                         'consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ' \
                         'ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur ' \
@@ -99,8 +140,10 @@ class TestContentfulUtils(TestCase):
                         'consectetur adipiscing elit'
 
         assert get_aggregated_data_from_contentful_data({}, 'uuid_123') is None
-        assert get_aggregated_data_from_contentful_data(bootcamp_transformed_data, 'no_uuid') is None
-        assert get_aggregated_data_from_contentful_data(bootcamp_transformed_data, 'test-uuid') == expected_data
+        assert get_aggregated_data_from_contentful_data(
+            mock_bootcamp_response.bootcamp_transformed_data, 'no_uuid') is None
+        assert get_aggregated_data_from_contentful_data(
+            mock_bootcamp_response.bootcamp_transformed_data, 'test-uuid') == expected_data
 
     @mock.patch('course_discovery.apps.course_metadata.contentful_utils.get_data_from_contentful',
                 return_value=[MockContenfulDegreeResponse().mock_contentful_degree_entry])
