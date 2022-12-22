350645bdc83ea7ac316c509116f7ab81650f2cc2
arslan.abdulrauf@arbisoft.com <arslan.abdulrauf@arbisoft.com>
2022-12-22 17:29:33 +0500

    dataloader API added Loader API updated to work with single course_id with its partner

--- course_discovery/apps/edly_discovery_app/api/v1/urls.py
+++ course_discovery/apps/edly_discovery_app/api/v1/urls.py
@@ -1,9 +1,10 @@
-from django.conf.urls import include, url
+from django.conf.urls import url
 
-from edly_discovery_app.api.v1 import views
+from edly_discovery_app.api.v1.views import dataloader_api, edly_sites
 
 
 app_name = 'v1'
 urlpatterns = [
-    url(r'^edly_sites/', views.EdlySiteViewSet.as_view(), name='edly_sites'),
+    url(r'^edly_sites/', edly_sites.EdlySiteViewSet.as_view(), name='edly_sites'),
+    url(r'^dataloader/', dataloader_api.EdlyDataLoaderView.as_view(), name='edly_dataloader'),
 ]
