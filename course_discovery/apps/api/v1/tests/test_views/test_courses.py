26382454b0c0b72d383817f5466071d03291c1f8
Eugene Dyudyunov <evgen.dyudyunov@raccoongang.com>
2022-12-28 09:24:01 +0200

    fix: add verified only course type
    Discovery fires the error when the `refresh_course_metadata` management
    command works if ecommerce has the `verified only` course set up.
    This PR introduces the migration to add missing course/courserun type.

--- course_discovery/apps/api/v1/tests/test_views/test_courses.py
+++ course_discovery/apps/api/v1/tests/test_views/test_courses.py
@@ -1896,7 +1896,7 @@ class CourseViewSetTests(OAuth2Mixin, SerializationMixin, APITestCase):
         CourseEntitlementFactory(course=self.course, mode=SeatTypeFactory.verified())
 
         url = reverse('api:v1:course-detail', kwargs={'key': self.course.uuid})
-        with self.assertNumQueries(40, threshold=0):
+        with self.assertNumQueries(41, threshold=0):
             response = self.client.options(url)
         assert response.status_code == 200
 
