7a14e2aa2bf78e924ff31494f8975cd7a90241f6
aliadnan <ali.adnan@arbisoft.com>
2023-03-16 19:32:16 +0500

    feat: update populate_default_product_source to populate program's value as well

--- course_discovery/apps/course_metadata/management/commands/populate_default_product_source.py
+++ course_discovery/apps/course_metadata/management/commands/populate_default_product_source.py
@@ -1,5 +1,5 @@
 """
-Management command to populate the default identifier to the courses having no identifier
+Management command to populate the default identifier to the products having no identifier
 """
 import logging
 
@@ -7,30 +7,59 @@ from django.conf import settings
 from django.core.management import BaseCommand, CommandError
 from django.db.transaction import atomic
 
-from course_discovery.apps.course_metadata.models import Course, Source
+from course_discovery.apps.course_metadata.models import Course, Program, Source
 
 logger = logging.getLogger(__name__)
 
 
+def get_products_with_no_product_source(product_type):
+    """
+    If product type is course then filtering the courses with no product_source and no additional_metadata because
+    legacy edX courses do not have additional_metadata, only the courses from external sources have additional_metadata
+    Else filtering the programs with no product_source and are not external degree programs
+    """
+    if product_type == 'course':
+        return Course.everything.filter(product_source=None, additional_metadata=None)
+    products = [
+        program.id for program in Program.objects.filter(product_source=None)
+        if not program.is_2u_degree_program
+    ]
+    return Program.objects.filter(id__in=products)
+
+
 class Command(BaseCommand):
-    help = 'Populate the default product_source to the courses having no product_source'
+    help = 'Populate the default product_source to the products having no product_source'
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--product_type',
+            help='The product type we want to populate with default product source.'
+                 'It can only be either course or program',
+            default='course',
+            required=False,
+            type=str,
+        )
 
     def handle(self, *args, **kwargs):
+        product_type = kwargs.get('product_type', 'course').lower()
         try:
+            if product_type not in ['course', 'program']:
+                raise Exception(f"Product Type {product_type} is invalid")  # pylint: disable=broad-exception-raised
             default_product_source = Source.objects.get(slug=settings.DEFAULT_PRODUCT_SOURCE_SLUG)
             with atomic():
-                # filtering the courses with no product_source and no additional_metadata because legacy edX courses
-                # do not have additional_metadata, only the courses from external sources have additional_metadata
-                courses = Course.everything.filter(product_source=None, additional_metadata=None)
-                # tranforming the queryset to list because after updating the courses, the queryset becomes empty
-                courses_list = list(courses)
-                courses.update(product_source=default_product_source)
-                logger.info(f'Updated {len(courses_list)} courses with default product_source')
-                if len(courses_list) > 0:
+                products = get_products_with_no_product_source(product_type)
+                # tranforming the queryset to list because after updating the products, the queryset becomes empty
+                product_list = list(products)
+                products.update(product_source=default_product_source)
+                logger.info(f'Updated {len(product_list)} {product_type}s with default product_source')
+                if len(product_list) > 0:
                     # pylint: disable=logging-not-lazy
-                    logger.info('Updated courses:\n' +
-                                '\n'.join(f"{course.key} - {'draft' if course.draft else 'non-draft'}"
-                                          for course in courses_list))
+                    if product_type == 'course':
+                        logger.info('Updated courses:\n' +
+                                    '\n'.join(f"{course.key} - {'draft' if course.draft else 'non-draft'}"
+                                              for course in product_list))
+                    else:
+                        logger.info('Updated programs:\n' + '\n'.join(program.title for program in product_list))
 
         except Source.DoesNotExist as ex:
             logging.exception(f'Default product_source {settings.DEFAULT_PRODUCT_SOURCE_SLUG} does not exist')
--- course_discovery/apps/course_metadata/management/commands/tests/test_populate_default_product_source.py
+++ course_discovery/apps/course_metadata/management/commands/tests/test_populate_default_product_source.py
@@ -4,8 +4,11 @@ from django.conf import settings
 from django.core.management import call_command
 from django.test import TestCase
 
-from course_discovery.apps.course_metadata.models import Course, Source
-from course_discovery.apps.course_metadata.tests.factories import CourseFactory, SourceFactory
+from course_discovery.apps.course_metadata.management.commands.populate_default_product_source import (
+    get_products_with_no_product_source
+)
+from course_discovery.apps.course_metadata.models import Course, Program, Source
+from course_discovery.apps.course_metadata.tests.factories import CourseFactory, ProgramFactory, SourceFactory
 
 LOGGER_PATH = 'course_discovery.apps.course_metadata.management.commands.populate_default_product_source.logger'
 
@@ -31,7 +34,7 @@ class PopulateDefaultProductSourceTests(TestCase):
         self.default_product_source.delete()
         self.external_test_product_source.delete()
 
-    def test_populate_default_product_source_with_no_existing_product_source(self):
+    def test_populate_default_product_source___courses_with_no_existing_product_source(self):
         """
         Verify that the command updates the courses with no product_source with the default product_source.
         """
@@ -50,7 +53,24 @@ class PopulateDefaultProductSourceTests(TestCase):
                 )
             ])
 
-    def test_populate_default_product_source_with_existing_product_source(self):
+    def test_populate_default_product_source__programs_with_no_existing_product_source(self):
+        """
+        Verify that the command updates the programs with no product_source with the default product_source.
+        """
+        programs = ProgramFactory.create_batch(50, product_source=None)
+        with mock.patch(LOGGER_PATH) as mock_logger:
+            call_command('populate_default_product_source', '--product_type', 'program')
+            for program in programs:
+                program.refresh_from_db()
+                assert program.product_source.name == self.default_product_source.name
+            assert len(programs) == Program.objects.filter(product_source=self.default_product_source).count()
+            mock_logger.info.assert_has_calls([
+                mock.call(f'Updated {len(programs)} programs with default product_source'),
+                mock.call(
+                    'Updated programs:\n' + '\n'.join(program.title for program in programs))
+            ])
+
+    def test_populate_default_product_source__with_existing_product_source(self):
         """
         Verify that the command does not update the courses with existing product_source.
         """
@@ -65,6 +85,27 @@ class PopulateDefaultProductSourceTests(TestCase):
                 assert Course.everything.filter(product_source=self.default_product_source).count() == 0
                 mock_logger.info.assert_called_once_with('Updated 0 courses with default product_source')
 
+    def test_populate_default_product_source__programs_with_existing_product_source(self):
+        """
+        Verify that the command does not update the programs with existing product_source.
+        """
+        test_programs = ProgramFactory.create_batch(5, product_source=self.external_test_product_source)
+        with mock.patch(LOGGER_PATH) as mock_logger:
+            call_command('populate_default_product_source', '--product_type', 'program')
+            for program in test_programs:
+                program.refresh_from_db()
+                assert program.product_source not in (None, self.default_product_source)
+                assert Program.objects.filter(product_source=self.default_product_source).count() == 0
+                mock_logger.info.assert_called_once_with('Updated 0 programs with default product_source')
+
+    def test_invalid_product_type(self):
+        """
+        Verify that the command raises an exception if the product_type is not valid.
+        """
+        with self.assertRaises(Exception) as context:
+            call_command('populate_default_product_source', '--product_type', 'test_type')
+        assert 'Product Type test_type is invalid' in str(context.exception)
+
     def test_source_does_not_exist(self):
         """
         Verify that the command raises an exception if the default product_source does not exist.
@@ -73,3 +114,9 @@ class PopulateDefaultProductSourceTests(TestCase):
         with self.assertRaises(Exception) as context:
             call_command('populate_default_product_source')
         assert f'Default product_source {self.default_product_source_slug} does not exist' in str(context.exception)
+
+    def test_get_products_with_no_product_source(self):
+        programs = ProgramFactory.create_batch(50, product_source=None)
+        courses = CourseFactory.create_batch(50, product_source=None, additional_metadata=None)
+        assert len(get_products_with_no_product_source('course')) == len(courses)
+        assert len(get_products_with_no_product_source('program')) == len(programs)
