2e2582ce6ace54da35ee87b1047b8aad06328604
Muhammad Arslan <arslan.abdulrauf@arbisoft.com>
2023-04-05 12:57:14 +0500

    Run Management command in shell environment (#53)

--- course_discovery/apps/edly_discovery_app/tasks.py
+++ course_discovery/apps/edly_discovery_app/tasks.py
@@ -1,6 +1,7 @@
+import subprocess
+
 from celery.task import task
 from celery.utils.log import get_task_logger
-from django.core import management
 
 
 from course_discovery.apps.course_metadata.data_loaders.api import (
@@ -34,5 +35,12 @@ def run_dataloader(partner, course_id, service):
     ).ingest()
 
     if service == 'wordpress':
-        management.call_command('update_index', '--disable-change-limit')
-        management.call_command('remove_unused_indexes')
+        update_index_cmd = "/edx/app/discovery/venvs/discovery/bin/python /edx/app/discovery/discovery/manage.py update_index --disable-change-limit"
+        remove_unused_index_cmd = "/edx/app/discovery/venvs/discovery/bin/python /edx/app/discovery/discovery/manage.py remove_unused_indexes"
+        LOGGER.info('Runing update_index command ...')
+        with subprocess.Popen(update_index_cmd, stdout=subprocess.PIPE, shell=True) as proc:
+            LOGGER.info(proc.stdout.read())
+
+        LOGGER.info('Runing remove_unused indexes command ...')
+        with subprocess.Popen(remove_unused_index_cmd, stdout=subprocess.PIPE, shell=True) as proc:
+            LOGGER.info(proc.stdout.read())
