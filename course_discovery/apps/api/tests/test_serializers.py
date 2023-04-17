fe1607d59b6bb926f07127ea6572e89c89f5a4af
zamanafzal <zamanafzal@gmail.com>
2023-04-18 03:29:09 +0500

    feat: Added Course review Api

--- course_discovery/apps/api/tests/test_serializers.py
+++ course_discovery/apps/api/tests/test_serializers.py
@@ -20,7 +20,7 @@ from course_discovery.apps.api.serializers import (
     AdditionalMetadataSerializer, AdditionalPromoAreaSerializer, AffiliateWindowSerializer, CatalogSerializer,
     CertificateInfoSerializer, CollaboratorSerializer, ContainedCourseRunsSerializer, ContainedCoursesSerializer,
     ContentTypeSerializer, CorporateEndorsementSerializer, CourseEditorSerializer, CourseEntitlementSerializer,
-    CourseLocationRestrictionSerializer, CourseRecommendationSerializer, CourseRunSerializer,
+    CourseLocationRestrictionSerializer, CourseRecommendationSerializer, CourseReviewSerializer, CourseRunSerializer,
     CourseRunWithProgramsSerializer, CourseSerializer, CourseWithProgramsSerializer,
     CourseWithRecommendationsSerializer, CurriculumSerializer, DegreeAdditionalMetadataSerializer, DegreeCostSerializer,
     DegreeDeadlineSerializer, EndorsementSerializer, FactSerializer, FAQSerializer,
@@ -43,7 +43,7 @@ from course_discovery.apps.core.tests.helpers import make_image_file
 from course_discovery.apps.core.tests.mixins import ElasticsearchTestMixin, LMSAPIClientMixin
 from course_discovery.apps.core.utils import serialize_datetime
 from course_discovery.apps.course_metadata.choices import CourseRunStatus, ProgramStatus
-from course_discovery.apps.course_metadata.models import AbstractLocationRestrictionModel
+from course_discovery.apps.course_metadata.models import AbstractLocationRestrictionModel, CourseReview
 from course_discovery.apps.course_metadata.search_indexes.documents import (
     CourseDocument, CourseRunDocument, LearnerPathwayDocument, PersonDocument, ProgramDocument
 )
@@ -3102,3 +3102,45 @@ class LocationRestrictionSerializerTests(TestCase):
         serializer.is_valid()
 
         assert serializer.errors['location_restriction']
+
+
+class CourseReviewSerializerTests(TestCase):
+    def setUp(self):
+        self.data = {
+            'course_key': 'CS101',
+            'reviews_count': 10,
+            'avg_course_rating': 4.500001,
+            'confident_learners_percentage': 80.012345,
+            'most_common_goal': CourseReview.CHANGE_CAREERS,
+            'most_common_goal_learners_percentage': 50.012345,
+            'total_enrollments': 1000,
+        }
+
+    def test_valid_data(self):
+        serializer = CourseReviewSerializer(data=self.data)
+        self.assertTrue(serializer.is_valid())
+        self.assertEqual(serializer.data['course_key'], 'CS101')
+        self.assertEqual(serializer.data['reviews_count'], 10)
+        self.assertEqual(serializer.data['avg_course_rating'], '4.500001')
+        self.assertEqual(serializer.data['confident_learners_percentage'], '80.012345')
+        self.assertEqual(serializer.data['most_common_goal'], CourseReview.CHANGE_CAREERS)
+        self.assertEqual(serializer.data['most_common_goal_learners_percentage'], '50.012345')
+        self.assertEqual(serializer.data['total_enrollments'], 1000)
+
+    def test_required_fields(self):
+        required_fields = ['course_key', 'most_common_goal']
+        for field in required_fields:
+            data = self.data.copy()
+            data.pop(field)
+            serializer = CourseReviewSerializer(data=data)
+            self.assertFalse(serializer.is_valid())
+            self.assertIn(field, serializer.errors)
+
+    def test_decimal_fields(self):
+        decimal_fields = ['avg_course_rating', 'confident_learners_percentage', 'most_common_goal_learners_percentage']
+        for field in decimal_fields:
+            data = self.data.copy()
+            data[field] = 'invalid'
+            serializer = CourseReviewSerializer(data=data)
+            self.assertFalse(serializer.is_valid())
+            self.assertIn(field, serializer.errors)
