1faa3c071a386285dc788498c0e505586b617471
Navin Karkera <navin@disroot.org>
2023-04-24 16:00:59 +0530

    feat!: update event_bus_kafka and add event_bus_redis
    feat: add event bus redis as dependency
    
    refactor: add openedx_events in installed apps to use consume_events command
    
    chore: upgrade dependencies

--- course_discovery/apps/course_metadata/management/commands/consume_events.py
+++
@@ -1,8 +0,0 @@
-# Import the consumer Command from event-bus-kafka in order to expose it here.
-#
-# FIXME: This currently hardcodes our use of the Kafka implementation.
-#   https://github.com/openedx/openedx-events/issues/147 discusses ways
-#   we might make this configurable.
-
-# pylint: disable=unused-import
-from edx_event_bus_kafka.management.commands.consume_events import Command
