499198bb8bc19eca552fe062f0150dfd5ea058eb
AfaqShuaib09 <afaq.shoaib09@gmail.com>
2023-05-04 02:04:19 +0500

    feat: modify bulk_upload_tags command to add tags to all product types

--- course_discovery/apps/course_metadata/management/commands/bulk_upload_tags.py
+++ course_discovery/apps/course_metadata/management/commands/bulk_upload_tags.py
@@ -6,13 +6,21 @@ import uuid
 
 from django.core.management import BaseCommand, CommandError
 
-from course_discovery.apps.course_metadata.models import BulkUploadTagsConfig, Course
+from course_discovery.apps.course_metadata.models import BulkUploadTagsConfig, Course, Degree, Program
 
 logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
-    help = 'Add tags to courses from a csv file'
+    help = 'Add SEO tags to products parsed from a CSV file'
+
+    # Mapping of SEO tags field names to the corresponding product type
+    PRODUCT_TYPE_TO_TAGS_FIELD_MAPPING = {
+        'course': {'model': Course, 'field': 'topics'},
+        'program': {'model': Program, 'field': 'labels'},
+        'degree': {'model': Degree, 'field': 'labels'},
+    }
+    PRODUCT_TYPES = ['course', 'program', 'degree']
 
     def add_arguments(self, parser):
         parser.add_argument(
@@ -36,37 +44,70 @@ class Command(BaseCommand):
                 "Error occured while opening the tags csv.\n{}".format(exc)
             )
 
-        for line in file_handle:
-            if line:
-                course_uuid, tags = self.parse_line(line)
-                self.set_course_tags(course_uuid, tags)
+        for row in file_handle:
+            if row:
+                product_uuid, product_type, tags = self.parse_row(row)
+                if product_type not in self.PRODUCT_TYPES:
+                    logger.info(f'Invalid product type: {product_type}')
+                    continue
+                self.set_product_tags(product_uuid, tags, product_type)
 
         file_handle.close()
 
-    def parse_line(self, line):
+    def parse_row(self, row):
         """
+        Parse a line from the csv file and return the product uuid and list of tags
         uuid,tag1,tag2,tag3 -> uuid, [tag1, tag2, tag3]
         """
-        course_id, *tags = line.split(',')
-        return course_id.strip(), [tag.strip() for tag in tags]
+        product_uuid, product_type, *tags = row.split(',')
+        return product_uuid.strip().lower(), product_type.strip(), [tag.strip() for tag in tags]
 
-    def set_course_tags(self, course_uuid, tags):
-        if self.is_valid_uuid(course_uuid):
-            tags = [tag for tag in tags if tag]  # remove empty strings from tags
-            logger.info(f'Adding tags for {course_uuid}')
-            qs = Course.everything.filter(uuid=course_uuid)
-            if not qs:
-                logger.info(f'No course found with uuid: {course_uuid}')
-            for course in qs:
-                course.topics.set(tags)
-                course.save()
+    def set_product_tags(self, product_uuid, tags, product_type):
+        """
+        Set the tags for a product of the given type and UUID.
+
+        Args:
+            product_uuid (str): The UUID of the product to set tags for.
+            tags (List[str]): A list of tags to set for the product. Empty strings are ignored.
+            product_type (str): The type of product to set tags for. Must be one of 'course', 'program', or 'degree'.
+
+        Example usage:
+
+        ```
+        set_product_tags('dummy-uuid', ['tag1', 'tag2'], 'course')
+        ```
+        """
+        if not self.is_valid_uuid(product_uuid):
+            logger.info(f'Invalid uuid: {product_uuid}')
+            return
+        tags = [tag for tag in tags if tag]
+
+        mapping = self.PRODUCT_TYPE_TO_TAGS_FIELD_MAPPING.get(product_type)
+        if not mapping:
+            logger.info(f'Invalid product type: {product_type}')
+            return
+
+        model = mapping['model']
+        field = mapping['field']
+
+        if model == Course:
+            query_set = Course.everything.filter(uuid=product_uuid)
         else:
-            logger.info(f'{course_uuid} is not a valid uuid')
+            query_set = model.objects.filter(uuid=product_uuid)
+        if not query_set:
+            logger.info(f'No {product_type} found with uuid: {product_uuid}')
+            return
 
-    def is_valid_uuid(self, course_uuid):
-        """Check if course_uuid is a valid uuid"""
+        for obj in query_set:
+            # pylint: disable=logging-not-lazy
+            logger.info(f'Setting tags for {product_type} with uuid -{product_uuid}: {tags}')
+            getattr(obj, field).set(tags)
+            obj.save()
+
+    def is_valid_uuid(self, product_uuid):
+        """ Check if the product uuid is valid """
         try:
-            uuid.UUID(str(course_uuid))
+            uuid.UUID(str(product_uuid))
             return True
         except ValueError:
             return False
