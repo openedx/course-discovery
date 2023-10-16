5bcdce55bebbcbf941614aad4f512998222bee65
Awais Qureshi <awais.qureshi@arbisoft.com>
2023-10-16 18:22:32 +0500

    Revert "feat: upgrade to Django 4.2 (#4088)"
    This reverts commit 3c075d4b5f6d2dd82df8464cbc9afaddb3f536fe.

--- .ci/run-tests.sh
+++ .ci/run-tests.sh
@@ -5,6 +5,10 @@ set -e
 
 apt-get update
 apt-get install -y --no-install-recommends firefox gettext
-make requirements
-
+if [ "$TOXENV" == "py38-django42" ]
+then
+    make requirements_dj42
+else
+    make requirements
+fi
 make test
