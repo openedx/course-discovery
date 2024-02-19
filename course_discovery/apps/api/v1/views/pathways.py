251d16120a094ab73c590951493e6cee258ef204
Syed Muhammad Dawoud Sheraz Ali <40599381+DawoudSheraz@users.noreply.github.com>
2024-02-19 14:17:48 +0500

    temp: add function_trace temporarily to breakdown queryset fetch in pathways api (#4274)

--- course_discovery/apps/api/v1/views/pathways.py
+++ course_discovery/apps/api/v1/views/pathways.py
@@ -1,4 +1,5 @@
 """ Views for accessing Pathway data """
+from edx_django_utils.monitoring import function_trace
 from rest_framework import viewsets
 
 from course_discovery.apps.api import serializers
@@ -10,6 +11,7 @@ class PathwayViewSet(CompressedCacheResponseMixin, viewsets.ReadOnlyModelViewSet
     permission_classes = (ReadOnlyByPublisherUser,)
     serializer_class = serializers.PathwaySerializer
 
+    @function_trace('pathways_api_queryset')
     def get_queryset(self):
         queryset = self.get_serializer_class().prefetch_queryset(partner=self.request.site.partner)
         return queryset.order_by('created')
