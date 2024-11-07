5b5447770d2b927fbf0fd6831bb711952927fccf
Ali Nawaz <alinawaz2000@hotmail.com>
2024-11-08 00:23:59 +0500

    test: some fixups and new ones

--- course_discovery/apps/course_metadata/management/commands/populate_executive_education_data_csv.py
+++ course_discovery/apps/course_metadata/management/commands/populate_executive_education_data_csv.py
@@ -183,8 +183,8 @@ class Command(BaseCommand):
         variants = (
             [product['variant']] if product['variant'] else []
         )
-        variants.extend(product['customPresentations'])
-        variants.extend([{**v, 'isFutureVariant': True} for v in product['futureVariants']])
+        variants.extend(product.get('customPresentations', []))
+        variants.extend([{**v, 'isFutureVariant': True} for v in product.get('futureVariants', [])])
 
         return variants
 
