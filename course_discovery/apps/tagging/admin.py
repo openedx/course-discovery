14ed4c1f5a2fb567d2e7c7dfe604f549cba93e30
Muhammad Afaq Shuaib <afaq.shoaib09@gmail.com>
2024-12-24 00:55:44 +0500

    chore: clean bit of admin code

--- course_discovery/apps/tagging/admin.py
+++ course_discovery/apps/tagging/admin.py
@@ -13,11 +13,17 @@ from course_discovery.apps.tagging.models import (
 
 @admin.register(VerticalFilter)
 class VerticalFilterAdmin(admin.ModelAdmin):
+    """
+    Admin class for VerticalFilter model admin.
+    """
     list_display = ('name', 'is_active', 'description', 'slug',)
     search_fields = ('name',)
 
 @admin.register(SubVericalFilter)
 class SubVericalFilterAdmin(admin.ModelAdmin):
+    """
+    Admin class for SubVerticalFilter model admin.
+    """
     list_display = ('name', 'is_active', 'slug', 'description', 'vertical_filters')
     list_filter = ('vertical_filters', )
     search_fields = ('name',)
@@ -30,18 +36,17 @@ class CourseVerticalFiltersAdmin(admin.ModelAdmin):
     list_filter = ('vertical', 'sub_vertical')
     search_fields = ('course__title', 'vertical__name', 'sub_vertical__name')
     ordering = ('course__title',)
-    
+
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == 'course':
             kwargs['queryset'] = Course.objects.filter(draft=False)
         return super().formfield_for_foreignkey(db_field, request, **kwargs)
-    
+
     def changelist_view(self, request, extra_context=None):
         extra_context = extra_context or {}
         extra_context['upload_csv_url'] = reverse('admin:courseverticalfilters_upload_csv')
         return super().changelist_view(request, extra_context=extra_context)
 
-    
     def get_urls(self):
         from django.urls import path
         urls = super().get_urls()
@@ -50,29 +55,26 @@ class CourseVerticalFiltersAdmin(admin.ModelAdmin):
         ]
         return custom_urls + urls
 
-    # Handle the CSV upload
     def upload_csv(self, request):
         if request.method == 'POST' and request.FILES.get('csv_file'):
             form = CSVUploadForm(request.POST, request.FILES)
             if form.is_valid():
-                # Process the CSV file
                 csv_file = form.cleaned_data['csv_file']
                 try:
                     decoded_file = csv_file.read().decode('utf-8').splitlines()
                     reader = csv.reader(decoded_file)
                     next(reader)  # Skip the header row
                     for row in reader:
-                        course_uuid = row[0]  # Assuming the first column is the course title
-                        vertical_name = row[1]  # Assuming the second column is the vertical name
-                        sub_vertical_name = row[2]  # Assuming the third column is the sub-vertical name
+                        course_uuid = row[0]
+                        vertical_name = row[1]
+                        sub_vertical_name = row[2]
 
-                        # Get the corresponding objects from the database
-                        import pdb; pdb.set_trace();
                         course = Course.objects.get(uuid=course_uuid)
                         vertical, _ = VerticalFilter.objects.get_or_create(name=vertical_name)
-                        sub_vertical, _ = SubVericalFilter.objects.get_or_create(name=sub_vertical_name, vertical_filters=vertical)
+                        sub_vertical, _ = SubVericalFilter.objects.get_or_create(
+                            name=sub_vertical_name, vertical_filters=vertical
+                        )
 
-                        # Create or update the CourseVerticalFilters instance
                         CourseVerticalFilters.objects.update_or_create(
                             course=course,
                             defaults={'vertical': vertical, 'sub_vertical': sub_vertical}
@@ -87,7 +89,7 @@ class CourseVerticalFiltersAdmin(admin.ModelAdmin):
 
         return render(
             request,
-            'admin/tagging/courseverticalfilters/upload_csv_form.html',  # You can create a custom template for the upload form
+            'admin/tagging/courseverticalfilters/upload_csv_form.html',
             context={'form': form}
         )
 
