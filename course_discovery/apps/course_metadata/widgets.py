714163c4e92eb561cee2b013c313efa2538b6e24
Ali Nawaz <alinawaz2000@hotmail.com>
2025-01-10 12:06:17 +0500

    perf: relegate optgroup calculation to superclass

--- course_discovery/apps/course_metadata/widgets.py
+++ course_discovery/apps/course_metadata/widgets.py
@@ -11,13 +11,7 @@ class SortedModelSelect2Multiple(autocomplete.ModelSelect2Multiple):
         This is a simplified version of Django's version. The big difference is that we keep the results sorted and
         only support one main group (because that's all we need right now).
         """
-        selected = []
-        for index, (option_value, option_label) in enumerate(chain(self.choices)):
-            is_selected = str(option_value) in value
-            if is_selected:
-                subgroup = [self.create_option(name, option_value, option_label, is_selected, index, attrs=attrs)]
-                item = (None, subgroup, index)
-                selected.append(item)
+        selected = super().optgroups(name, value, attrs)
 
         ordered = []
         for value_id in value:
