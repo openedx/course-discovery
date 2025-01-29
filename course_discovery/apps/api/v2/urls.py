63a1ecd26fee107aa859d62c46d1aa40f68134d4
Ali Akbar <52413434+Ali-D-Akbar@users.noreply.github.com>
2025-01-29 15:00:56 +0500

    feat: add SearchAfterMixin for ES search_after capability (#4536)

--- course_discovery/apps/api/v2/urls.py
+++ course_discovery/apps/api/v2/urls.py
@@ -1,11 +1,17 @@
 """API v2 URLs."""
 
+from django.urls import re_path
 from rest_framework import routers
 
 from course_discovery.apps.api.v2.views import search as search_views
+from course_discovery.apps.api.v2.views.catalog_queries import CatalogQueryContainsViewSet
 
 app_name = 'v2'
 
+urlpatterns = [
+    re_path(r'^catalog/query_contains/?', CatalogQueryContainsViewSet.as_view(), name='catalog-query_contains'),
+]
+
 router = routers.SimpleRouter()
 router.register(r'search/all', search_views.AggregateSearchViewSet, basename='search-all')
-urlpatterns = router.urls
+urlpatterns += router.urls
