154eaddcffcfe0d611b82432e339f238140bbc14
Muhammad Afaq Shuaib <afaq.shoaib09@gmail.com>
2025-01-29 18:16:55 +0500

    style: add bootstrap styling to tagging app

--- course_discovery/apps/tagging/mixins.py
+++ course_discovery/apps/tagging/mixins.py
@@ -1,23 +1,18 @@
 from django.conf import settings
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.core.exceptions import PermissionDenied
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 
-class VerticalTaggingAdministratorPermissionRequiredMixin(LoginRequiredMixin):
+class VerticalTaggingAdministratorPermissionRequiredMixin(
+    LoginRequiredMixin, UserPassesTestMixin
+):
     """
     A mixin to enforce permission on VERTICALS_MANAGEMENT_GROUPS for class-based views.
     """
 
-    def dispatch(self, request, *args, **kwargs):
-        response = super().dispatch(request, *args, **kwargs)
-        if response.status_code == 403:
-            return response
-
-        in_vertical_management_group = request.user.groups.filter(
+    def test_func(self):
+        """
+        Check if the user is in the VERTICALS_MANAGEMENT_GROUPS group or is a superuser.
+        """
+        return self.request.user.is_superuser or self.request.user.groups.filter(
             name__in=settings.VERTICALS_MANAGEMENT_GROUPS
         ).exists()
-
-        if not request.user.is_superuser and not in_vertical_management_group:
-            raise PermissionDenied("You do not have permission to access this page.")
-
-        return response
