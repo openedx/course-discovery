844e13e214a4c15da0397cad33329c03efce4ce1
Hamza Shafique <hamzashafiquehere@gmail.com>
2025-02-14 14:54:26 +0500

    chore: enable tracing for management commands

--- manage.py
+++ manage.py
@@ -11,5 +11,7 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "course_discovery.settings.local")
 
     from django.core.management import execute_from_command_line
+    from edx_django_utils.monitoring import monitor_django_management_command
 
-    execute_from_command_line(sys.argv)
+    with monitor_django_management_command(sys.argv[1]):
+        execute_from_command_line(sys.argv)
