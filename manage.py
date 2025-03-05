02ce38a3937add780f7f4bd5fe5160a46a27c900
Hamza Shafique <hamzashafiquehere@gmail.com>
2025-03-06 02:17:03 +0500

    chore: monitor django management commands

--- manage.py
+++ manage.py
@@ -11,5 +11,14 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "course_discovery.settings.local")
 
     from django.core.management import execute_from_command_line
+    from course_discovery.apps.core.utils import _get_enable_custom_management_command_monitoring
 
-    execute_from_command_line(sys.argv)
+    monitoring_enabled = _get_enable_custom_management_command_monitoring()
+
+    if monitoring_enabled and len(sys.argv) > 1:
+        from course_discovery.apps.core.utils import monitor_django_management_command
+
+        with monitor_django_management_command(sys.argv[1]):
+            execute_from_command_line(sys.argv)
+    else:
+        execute_from_command_line(sys.argv)
