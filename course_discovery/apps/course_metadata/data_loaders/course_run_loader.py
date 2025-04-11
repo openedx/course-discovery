8712609e8efa5b8a3ef80289433da006245d5f80
Ali Akbar <aliakbarit019@gmail.com>
2025-04-11 15:02:49 +0500

    updates

--- course_discovery/apps/course_metadata/data_loaders/course_run_loader.py
+++ course_discovery/apps/course_metadata/data_loaders/course_run_loader.py
@@ -1,11 +1,19 @@
 import logging
 from django.conf import settings
+from django.urls import reverse
+from functools import cache
 
 from course_discovery.apps.course_metadata.data_loaders import AbstractDataLoader
-from course_discovery.apps.course_metadata.data_loaders.utils import initialize_csv_reader, transform_dict_keys, get_formatted_datetime_string
-from course_discovery.apps.course_metadata.models import CourseRun
+from course_discovery.apps.course_metadata.data_loaders.utils import (
+    initialize_csv_reader,
+    transform_dict_keys,
+    get_formatted_datetime_string,
+    extract_seat_prices
+)
+from course_discovery.apps.course_metadata.models import CourseRun, CourseRunType, CourseRunPacing
 
 logger = logging.getLogger(__name__)
+
 
 class CourseRunDataLoader(AbstractDataLoader):
     """
@@ -28,7 +36,7 @@ class CourseRunDataLoader(AbstractDataLoader):
             api_url: The base URL for the course run API.
             csv_path: Path to the CSV file.
             csv_file: A file object for the CSV file.
-            use_gspread_client: If True, use the gspr`ead client to read data.
+            use_gspread_client: If True, use the gspread client to read data.
         """
         super(CourseRunDataLoader, self).__init__(partner, api_url)
         self.reader = initialize_csv_reader(csv_path, csv_file, use_gspread_client)
@@ -71,33 +79,29 @@ class CourseRunDataLoader(AbstractDataLoader):
                 start_datetime = get_formatted_datetime_string(f"{start_date} {start_time}")
                 end_datetime = get_formatted_datetime_string(f"{end_date} {end_time}")
 
-                # Prepare the API request payload.
-                payload = {
-                    'course': course_run.course.key,
+                course = course_run.course
+                course_type = getattr(course, 'course_type', None)
+                course_run_type_uuid = self.get_course_run_type(run_type)
+
+                data = {
+                    'prices': extract_seat_prices(course_run),
                     'start': start_datetime,
                     'end': end_datetime,
-                    'run_type': run_type,
                     'pacing_type': pacing_type,
                 }
 
-                # Create URL for the API endpoint. (Assumes reverse lookup)
-                url = f"{settings.DISCOVERY_BASE_URL}{reverse('api:v1:course_run-list')}"
+                _ = self._create_course_run(
+                    data,
+                    course,
+                    course_type,
+                    course_run_type_uuid,
+                    course_run_key
+                )
 
-                # Make the API request to create a course run.
-                response = call_course_api('POST', url, payload)
-                if response.status_code in (200, 201):
-                    logger.info(f"Successfully created course run for course: {course_key}")
-                    self.ingestion_summary['success_count'] += 1
-                else:
-                    error_message = (
-                        f"Failed to create course run for course '{course_key}'. "
-                        f"API responded with status {response.status_code} and content: {response.content}"
-                    )
-                    logger.error(error_message)
-                    self.ingestion_summary['failure_count'] += 1
-                    self.ingestion_summary['errors'].append(error_message)
+                logger.info(f"Successfully created course run for course run key: {course_run_key}")
+                self.ingestion_summary['success_count'] += 1
             except Exception as e:
-                error_message = f"Exception processing course key '{course_key}': {e}"
+                error_message = f"Exception processing course run key '{course_run_key}': {e}"
                 logger.exception(error_message)
                 self.ingestion_summary['failure_count'] += 1
                 self.ingestion_summary['errors'].append(error_message)
@@ -105,3 +109,87 @@ class CourseRunDataLoader(AbstractDataLoader):
         logger.info("Course run ingestion complete.")
         logger.info(f"Ingestion Summary: {self.ingestion_summary}")
         return self.ingestion_summary
+
+    def _create_course_run(self, data, course, course_type, course_run_type_uuid, rerun=None):
+        """
+        Make a course run entry through course run API.
+        """
+        url = f"{settings.DISCOVERY_BASE_URL}{reverse('api:v1:course_run-list')}"
+        request_data = self._create_course_run_api_request_data(data, course, course_type, course_run_type_uuid, rerun)
+        response = self._call_course_api('POST', url, request_data)
+        if response.status_code not in (200, 201):
+            logger.info(f"Course run creation response: {response.content}")
+        return response.json()
+
+    def _create_course_run_api_request_data(self, data, course, course_type, course_run_type_uuid, rerun=None):
+        """
+        Given a data dictionary, return a reduced data representation which will be used
+        as input for course run creation via the course run API.
+        """
+        pricing = self.get_pricing_representation(data['prices'], course_type)
+        course_run_creation_fields = {
+            'pacing_type': self.get_pacing_type(data['pacing_type']),
+            'start': data['start'],
+            'end': data['end'],
+            'run_type': str(course_run_type_uuid),
+            'prices': pricing,
+            'course': course.key,
+        }
+
+        if rerun:
+            course_run_creation_fields['rerun'] = rerun
+        return course_run_creation_fields
+
+    def _call_course_api(self, method, url, data):
+        """
+        Helper method to make course and course run API calls.
+        """
+        response = self.api_client.request(
+            method,
+            url,
+            json=data,
+            headers={'content-type': 'application/json'}
+        )
+        if not response.ok:
+            logger.info("API request failed for url {} with response: {}".format(url, response.content.decode('utf-8')))
+        response.raise_for_status()
+        return response
+
+    def get_pricing_representation(self, price, course_type):
+        """
+        Return dict representation of prices for a given course type.
+        """
+        prices = {}
+        entitlement_types = course_type.entitlement_types.all()
+        for entitlement_type in entitlement_types:
+            prices.update({entitlement_type.slug: price})
+        return prices
+
+    def get_pacing_type(self, pacing):
+        """
+        Return appropriate pacing selection against a provided pacing string.
+        """
+        if pacing:
+            pacing = pacing.lower()
+
+        if pacing == 'instructor-paced':
+            return CourseRunPacing.Instructor.value
+        elif pacing == 'self-paced':
+            return CourseRunPacing.Self.value
+        else:
+            return None
+
+    @staticmethod
+    @cache
+    def get_course_run_type(course_run_type_name):
+        """
+        Retrieve a CourseRunType object, using a cache to avoid redundant queries.
+
+        Args:
+            course_run_type_name (str): Course run type name
+        """
+        try:
+            return CourseRunType.objects.get(name=course_run_type_name)
+        except CourseRunType.DoesNotExist:
+            return None
+
--- course_discovery/apps/course_metadata/data_loaders/utils.py
+++ course_discovery/apps/course_metadata/data_loaders/utils.py
@@ -10,6 +10,7 @@ from lxml.html import clean
 from django.conf import settings
 import unicodecsv
 from dateutil.parser import parse
+from course_discovery.apps.course_metadata.data_loaders.constants import CSVIngestionErrorMessages, CSVIngestionErrors
 
 from course_discovery.apps.core.utils import serialize_datetime
 from course_discovery.apps.course_metadata.gspread_client import GspreadClient
@@ -186,3 +187,77 @@ def get_formatted_datetime_string(date_string):
     Return the datetime string into the desired format %Y-%m-%dT%H:%M:%SZ
     """
     return serialize_datetime(parse(date_string))
+
+def _validate_and_process_row(self, row, course_title, org_key=None):
+    """
+    Validate the row data and process the row if it is valid.
+
+    Args:
+        row (dict): course data row
+        course_title (str): Course title
+        org_key (str): Organization key
+
+    Returns:
+        bool: True if the row is valid, False otherwise
+        CourseType: CourseType object
+        CourseRunType: CourseRunType object
+    """
+    if org_key and not self.validate_organization(org_key, course_title):
+        return False, None, None
+
+    def validate_course_and_course_run_types(row, course_title):
+        """
+        Helper method to validate course and course run types.
+
+        Args:
+            row (dict): Course data row
+            course_title (str): Course title
+
+        Returns:
+            bool: True if course and course run types are valid, False otherwise
+            CourseType: CourseType object
+            CourseRunType: CourseRunType object
+        """
+        course_type = self.get_course_type(row["course_enrollment_track"])
+        if not course_type:
+            self._log_ingestion_error(
+                CSVIngestionErrors.MISSING_COURSE_TYPE,
+                CSVIngestionErrorMessages.MISSING_COURSE_TYPE.format(
+                    course_title=course_title, course_type=row["course_enrollment_track"]
+                ),
+            )
+            return False, None, None
+
+        course_run_type = self.get_course_run_type(row["course_run_enrollment_track"])
+        if not course_run_type:
+            self._log_ingestion_error(
+                CSVIngestionErrors.MISSING_COURSE_RUN_TYPE,
+                CSVIngestionErrorMessages.MISSING_COURSE_RUN_TYPE.format(
+                    course_title=course_title, course_run_type=row["course_run_enrollment_track"]
+                ),
+            )
+            return False, None, None
+
+        return True, course_type, course_run_type
+
+    is_valid, course_type, course_run_type = validate_course_and_course_run_types(row, course_title)
+    if not is_valid:
+        return False, course_type, course_run_type
+
+    missing_fields = self.validate_course_data(course_type, row)
+    if missing_fields:
+        self._log_ingestion_error(
+            CSVIngestionErrors.MISSING_REQUIRED_DATA,
+            CSVIngestionErrorMessages.MISSING_REQUIRED_DATA.format(
+                course_title=course_title, missing_data=missing_fields
+            )
+        )
+        return False, course_type, course_run_type
+
+    return True, course_type, course_run_type
+
+def extract_seat_prices(course_run):
+    prices = {}
+    for seat in course_run.seats:
+        prices[seat.type] = seat.price
+    return prices
