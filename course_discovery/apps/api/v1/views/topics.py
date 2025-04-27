412abc22496e770e7d124f37840f6dd2fef34991
Barry Bao <barrybao@Barrys-Mac-mini.local>
2025-04-27 15:32:18 +0800

    removed: file

--- course_discovery/apps/api/v1/views/topics.py
+++
@@ -1,7 +0,0 @@
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
-
-from course_discovery.apps.api import filters, serializers
-from course_discovery.apps.api.pagination import ProxiedPagination
-
