fbcf9afe8461e88b9ac13388acc1783a60401edf
Barry Bao <barrybao@Barrys-Mac-mini.local>
2025-05-27 18:07:31 +0800

    added

--- course_discovery/apps/course_metadata/views.py
+++ course_discovery/apps/course_metadata/views.py
@@ -34,38 +34,34 @@ class CourseMetadataRefresher(View):
         if not course_id:
             return JsonResponse({'error': 'Invalid Course Key : {}'.format(course_id)}, status=503)
 
-        partner_code = data.get('partner_code', None)
-        # If a specific partner was indicated, filter down the set
-        partners = Partner.filter(short_code=partner_code) if partner_code else Partner.objects.all()
-        if not partners:
-            return JsonResponse({'error': 'No partners available!'}, status=503)
+        try:
+            # Get `class Partner` instance by request.site.domain (e.g., 0.0.0.0:18000)
+            course_partner = Partner.objects.get(site__domain=request.site.domain)
+            access_token, __ = EdxRestApiClient.get_oauth_access_token(
+                '{root}/access_token'.format(root=course_partner.oidc_url_root.strip('/')),
+                course_partner.oidc_key, course_partner.oidc_secret, token_type='JWT'
+            )
+            kwargs = {
+                'partner': course_partner, 'api_url': course_partner.courses_api_url,
+                'access_token': access_token, 'token_type': 'JWT', 'max_workers': 1, 'is_threadsafe': True,
+                'course_key': course_id
+            }
+            username = jwt.decode(access_token, verify=False)['preferred_username']
+            if username:
+                kwargs['username'] = username
 
-        for partner in partners:
-            try:
-                access_token, __ = EdxRestApiClient.get_oauth_access_token(
-                    '{root}/access_token'.format(root=partner.oidc_url_root.strip('/')),
-                    partner.oidc_key, partner.oidc_secret, token_type='JWT'
-                )
-                kwargs = {
-                    'partner': partner, 'api_url': partner.courses_api_url, 'access_token': access_token,
-                    'token_type': 'JWT', 'max_workers': 1, 'is_threadsafe': True, 'course_key': course_id
-                }
-                username = jwt.decode(access_token, verify=False)['preferred_username']
-                if username:
-                    kwargs['username'] = username
+            CoursesApiDataLoader(**kwargs).ingest()
 
-                CoursesApiDataLoader(**kwargs).ingest()
+            response[course_partner.short_code] = course_id
 
-                response[partner.short_code] = course_id
+        except Exception as e:
+            error_message = 'domain: {} | course_key: {} | error: {}'.format(
+                request.site.domain, course_id, str(e)
+            )
 
-            except Exception as e:
-                error_message = 'code: {} | course_key: {} | error: {}'.format(
-                    partner.short_code, course_id, str(e)
-                )
-
-                if 'errors' in response:
-                    response['errors'].append(error_message)
-                else:
-                    response['errors'] = [error_message]
+            if 'errors' in response:
+                response['errors'].append(error_message)
+            else:
+                response['errors'] = [error_message]
 
         return JsonResponse(response)
