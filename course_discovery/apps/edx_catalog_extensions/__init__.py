33ee4263ba825fc7167f4991468e3f46681bd197
Barry Bao <barrybao@Barrys-Mac-mini.local>
2025-05-28 17:49:28 +0800

    removed: 'edx_catalog_extensions' + 'edx_haystack_extensions'

--- course_discovery/apps/edx_catalog_extensions/__init__.py
+++
@@ -1,4 +0,0 @@
-""" edX extensions for the Catalog (Course Discovery) Service.
-
-This Django app is intended for usage by edX. Its usage is NOT supported for OpenEdX.
-"""
--- course_discovery/apps/edx_catalog_extensions/urls.py
+++
@@ -1,5 +0,0 @@
-from django.conf.urls import include, url
-
-urlpatterns = [
-    url(r'^api/', include('course_discovery.apps.edx_catalog_extensions.api.urls', namespace='api')),
-]
